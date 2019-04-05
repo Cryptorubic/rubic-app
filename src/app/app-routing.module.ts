a92c158d4c4f565493582290c173c38c8776f997
Dmitriy Kovalyov <dmitriy.kovalyov@ddgcorp.ru>
2019-04-05 17:09:15 +0300

    FAQ, header links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -47,7 +47,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import PROJECTS from "./projects-resourses";
+import {HttpService} from '../services/http/http.service';
+
+const STAT_URL = 'get_statistics_landing/';
 
 @Component({
   selector: 'app-index',
@@ -9,9 +12,13 @@ import PROJECTS from "./projects-resourses";
 export class IndexComponent implements OnInit {
 
   public projects = PROJECTS;
-  constructor() { }
+  public stat;
+  constructor(
+    private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+    this.httpService.get(STAT_URL).subscribe(res => this.stat = res);
   }
 
 }
