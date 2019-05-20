e6172054078ce9622df2e970e56288e0ab056fc3
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-05-20 21:12:05 +0300

    chore(team.ts): add advisers, team data arrays

--- src/app/team-component/team.component.ts
+++ src/app/team-component/team.component.ts
@@ -14,12 +14,13 @@ export interface IPerson {
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit {
-  public people: Array<IPerson>;
+  public team: Array<IPerson>;
+  public advisers: Array<IPerson>;
 
   constructor() {}
 
   public ngOnInit(): void {
-    this.people = [
+    this.team = [
       {
         name: 'Vladimir Tikhomirov',
         position: 'CEO',
@@ -42,6 +43,51 @@ export class TeamComponent implements OnInit {
       {
         name: 'Alexandra Korneva',
         position: 'PR and Marketing',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      },
+      {
+        name: 'Max Strenk',
+        position: 'Backend Developer',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/',
+        gitHubUrl: 'https://github.com/'
+      },
+      {
+        name: 'Alexander Boyarshenchok',
+        position: 'Designer',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg'
+      },
+      {
+        name: 'Olga Kulakova',
+        position: 'SMM',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      }
+    ];
+
+    this.advisers = [
+      {
+        name: 'Pavel Shterlyaev',
+        position: 'Adviser / Partner - Founder BestRate.org',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      },
+      {
+        name: 'Hugo Hellebuyck',
+        position: 'Adviser / Partner at Tangem',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      },
+      {
+        name: 'Eric Benz',
+        position: 'CEO of Changelly',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      },
+      {
+        name: 'Dmitry Machikhin',
+        position: 'Adviser / Head GMTLegal',
         avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
         linkedInUrl: 'https://ru.linkedin.com/'
       }
