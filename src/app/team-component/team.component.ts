99b6cda3233556dcbe520e48890559928ac5d9c8
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-05-20 21:01:04 +0300

    chore(team.ts): add people in people data array

--- src/app/team-component/team.component.ts
+++ src/app/team-component/team.component.ts
@@ -19,12 +19,33 @@ export class TeamComponent implements OnInit {
   constructor() {}
 
   public ngOnInit(): void {
-    this.people = [{
-      name: 'Vladimir Tikhomirov',
-      position: 'CEO',
-      avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
-      linkedInUrl: 'https://ru.linkedin.com/'
-    }];
+    this.people = [
+      {
+        name: 'Vladimir Tikhomirov',
+        position: 'CEO',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      },
+      {
+        name: 'Veronika Trunina',
+        position: 'Business Development',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      },
+      {
+        name: 'Nina Lukina',
+        position: 'Backend Developer',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/',
+        gitHubUrl: 'https://github.com/'
+      },
+      {
+        name: 'Alexandra Korneva',
+        position: 'PR and Marketing',
+        avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+        linkedInUrl: 'https://ru.linkedin.com/'
+      }
+    ];
   }
 
 }
