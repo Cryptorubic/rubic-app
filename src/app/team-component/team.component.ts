03a815b77000f82fa328bd0e145071a5d6b73af3
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-05-20 20:39:09 +0300

    chore: add ngFor for rendering people data

--- src/app/team-component/team.component.ts
+++ src/app/team-component/team.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IPerson {
+  name: string;
+  position: string;
+  avatarPath: string;
+  linkedInUrl: string;
+}
+
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit {
+  public people: Array<IPerson>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.people = [{
+      name: 'Vladimir Tikhomirov',
+      position: 'CEO',
+      avatarPath: 'https://pbs.twimg.com/profile_images/685347483430486016/tr4jiGUK_400x400.jpg',
+      linkedInUrl: 'https://ru.linkedin.com/'
+    }];
   }
 
 }
