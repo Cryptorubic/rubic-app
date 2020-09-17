b6d7426e87dda57b5242de6650dc67ab9322f38f
Amary <laumiqv@gmail.com>
2020-09-17 15:33:41 +0300

    addmodal and fix content

--- src/app/token-sale/token-sale.component.ts
+++ src/app/token-sale/token-sale.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-token-sale-page',
@@ -6,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./token-sale.component.scss'],
 })
 export class TokenSaleComponent implements OnInit {
-  constructor() {}
+  @ViewChild('tokenSaleModal') tokenSale: TemplateRef<any>;
+
+  protected tokenSaleModal: MatDialogRef<any>;
+
+  public tsmodalStep = false;
+  public confirmCheckbox = false;
+  public addressCopy = false;
+
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {}
+
+  public openModal() {
+    this.tokenSaleModal = this.dialog.open(this.tokenSale, {
+      width: '650px',
+      panelClass: 'dialog-ts-container',
+    });
+  }
+
+  public onCopied() {
+    this.addressCopy = true;
+
+    setTimeout(() => {
+      this.addressCopy = false;
+    }, 2500);
+  }
 }
