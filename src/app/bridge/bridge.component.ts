433143836dca60ec8a01b6623316743627280b28
siandrev <andreev.sergey.i@yandex.ru>
2021-01-23 10:42:29 +0300

    Terms and conditions modal added

--- src/app/bridge/bridge.component.ts
+++ src/app/bridge/bridge.component.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
-// @ts-ignore
-import collaborations from '../../assets/content/collaborations/collaborations.json';
+import {AfterContentInit, AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {DisclaimerComponent} from '../components/disclaimer/disclaimer.component';
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'app-bridge',
   templateUrl: './bridge.component.html',
   styleUrls: ['./bridge.component.scss']
 })
-export class BridgeComponent implements OnInit {
+export class BridgeComponent implements OnInit, AfterViewInit{
+  @ViewChild('disclaimerText') disclaimerText;
 
-  collaborations = collaborations;
-
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  private openDisclaimer(): void {
+    this.dialog.open(DisclaimerComponent, {
+      width: '650px',
+      disableClose: true,
+      data: {
+        text: 'DISCLAIMERS.START.TEXT',
+        title: 'DISCLAIMERS.START.TITLE',
+        actions: {}
+      },
+    });
+  }
+
+  ngAfterViewInit() {
+    const link = this.disclaimerText.nativeElement.getElementsByClassName('as-link')[0];
+    if (link) {
+      link.onclick = (event) => {
+        event.preventDefault();
+        this.openDisclaimer();
+        return false;
+      };
+    }
   }
 
 }
