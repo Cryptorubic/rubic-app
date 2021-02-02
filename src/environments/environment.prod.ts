441968992d718375e52d219a5757d73dd1e25c69
siandrev <andreev.sergey.i@yandex.ru>
2021-02-02 15:01:15 +0300

    Instant trades bot integrated.One inch fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://rubic.exchange/api/v1/',
-  botUrl: 'https://devswaps.mywish.io/api/v1/bridge/bot/'
+  bridgeBotUrl: 'https://devswaps.mywish.io/api/v1/bot/bridgeswaps',
+  instantTradesBotUrl: 'https://devswaps.mywish.io/api/v1/bot/instanttrades'
 };
