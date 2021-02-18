534ba190eba4560ddc6cfe0c4aedb984b5376683
siandrev <andreev.sergey.i@yandex.ru>
2021-02-18 15:11:36 +0300

    UniSwap test tokens-eth without approve added

--- src/app/services/instant-trade/uni-swap-service/uni-swap.service.ts
+++ src/app/services/instant-trade/uni-swap-service/uni-swap.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import InstantTradeService from '../InstantTradeService';
 import InstantTrade from '../types/InstantTrade';
-import {InstantTradeToken} from '../types';
-import {ChainId, Fetcher, Route, Token, TokenAmount, Trade, TradeType, WETH} from '@uniswap/sdk';
+import { InstantTradeToken } from '../types';
+import { ChainId, Fetcher, Route, Token, TokenAmount, Trade, TradeType, WETH } from '@uniswap/sdk';
 import BigNumber from 'bignumber.js';
 import { Percent } from '@uniswap/sdk';
-import {Web3ApiService} from '../../web3Api/web3-api.service';
-import {UniSwapContractAbi, UniSwapContractAddress} from './uni-swap-contract';
-import {TransactionReceipt} from 'web3-eth';
+import { Web3ApiService } from '../../web3Api/web3-api.service';
+import { UniSwapContractAbi, UniSwapContractAddress } from './uni-swap-contract';
+import { TransactionReceipt } from 'web3-eth';
 
 interface UniSwapTrade {
   amountIn: string;
@@ -21,7 +21,6 @@ interface UniSwapTrade {
   providedIn: 'root'
 })
 export class UniSwapService extends InstantTradeService {
-
   static slippageTolerance = new Percent('50', '10000'); // 0.50%
   private readonly provider;
   private readonly WETH;
@@ -34,14 +33,14 @@ export class UniSwapService extends InstantTradeService {
   }
 
   public async calculateTrade(
-      fromAmount: BigNumber,
-      fromToken: InstantTradeToken,
-      toToken: InstantTradeToken,
-      chainId?
+    fromAmount: BigNumber,
+    fromToken: InstantTradeToken,
+    toToken: InstantTradeToken,
+    chainId?
   ): Promise<InstantTrade> {
     try {
-      const fromTokenClone = {... fromToken};
-      const toTokenClone = {... toToken};
+      const fromTokenClone = { ...fromToken };
+      const toTokenClone = { ...toToken };
 
       if (this.web3Api.isEtherAddress(fromTokenClone.address)) {
         fromTokenClone.address = this.WETH.address;
@@ -51,12 +50,21 @@ export class UniSwapService extends InstantTradeService {
         toTokenClone.address = this.WETH.address;
       }
 
-      const uniSwapTrade = await this.getUniSwapTrade(fromAmount, fromTokenClone, toTokenClone, chainId);
+      const uniSwapTrade = await this.getUniSwapTrade(
+        fromAmount,
+        fromTokenClone,
+        toTokenClone,
+        chainId
+      );
 
-      const amountIn = new BigNumber(uniSwapTrade.inputAmount
-          .toSignificant(fromTokenClone.decimals)).multipliedBy(10 ** fromTokenClone.decimals);
-      const amountOutMin = new BigNumber(uniSwapTrade.minimumAmountOut(UniSwapService.slippageTolerance)
-          .toSignificant(toTokenClone.decimals)).multipliedBy(10 ** toTokenClone.decimals);
+      const amountIn = new BigNumber(
+        uniSwapTrade.inputAmount.toSignificant(fromTokenClone.decimals)
+      ).multipliedBy(10 ** fromTokenClone.decimals);
+      const amountOutMin = new BigNumber(
+        uniSwapTrade
+          .minimumAmountOut(UniSwapService.slippageTolerance)
+          .toSignificant(toTokenClone.decimals)
+      ).multipliedBy(10 ** toTokenClone.decimals);
       const path = [fromTokenClone.address, toTokenClone.address];
       const to = this.web3Api.address;
       const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
@@ -70,7 +78,9 @@ export class UniSwapService extends InstantTradeService {
   */
       const estimatedGas = new BigNumber(0);
       const gasFee = await this.web3Api.getGasFeeInUSD(estimatedGas);
-      const amountOut = uniSwapTrade.minimumAmountOut(new Percent('0', '1')).toSignificant(toTokenClone.decimals);
+      const amountOut = uniSwapTrade
+        .minimumAmountOut(new Percent('0', '1'))
+        .toSignificant(toTokenClone.decimals);
 
       const trade: InstantTrade = {
         from: {
@@ -83,7 +93,6 @@ export class UniSwapService extends InstantTradeService {
         },
         estimatedGas,
         gasFee
-
       };
       return trade;
     } catch (e) {
@@ -93,24 +102,25 @@ export class UniSwapService extends InstantTradeService {
   }
 
   public async createTrade(
-      trade: InstantTrade,
-      options: {
-        onConfirm?: (hash: string) => void,
-        onApprove?: (hash: string) => void
-      } = { }
-    ): Promise<TransactionReceipt> {
-
+    trade: InstantTrade,
+    options: {
+      onConfirm?: (hash: string) => void;
+      onApprove?: (hash: string) => void;
+    } = {}
+  ): Promise<TransactionReceipt> {
     const amountIn = trade.from.amount.multipliedBy(10 ** trade.from.token.decimals).toFixed(0);
-    const percentSlippage = new BigNumber(UniSwapService.slippageTolerance.toSignificant(10)).div(100);
+    const percentSlippage = new BigNumber(UniSwapService.slippageTolerance.toSignificant(10)).div(
+      100
+    );
     const amountOutMin = trade.to.amount
-        .multipliedBy(new BigNumber(1).minus(percentSlippage))
-        .multipliedBy(10 ** trade.from.token.decimals)
-        .toFixed(0);
+      .multipliedBy(new BigNumber(1).minus(percentSlippage))
+      .multipliedBy(10 ** trade.from.token.decimals)
+      .toFixed(0);
     const path = [trade.from.token.address, trade.to.token.address];
     const to = this.web3Api.address;
     const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
 
-    const uniSwapTrade: UniSwapTrade = {amountIn, amountOutMin, path, to, deadline};
+    const uniSwapTrade: UniSwapTrade = { amountIn, amountOutMin, path, to, deadline };
 
     if (this.web3Api.isEtherAddress(trade.from.token.address)) {
       return this.createEthToTokensTrade(uniSwapTrade, options);
@@ -124,85 +134,101 @@ export class UniSwapService extends InstantTradeService {
   }
 
   private async createEthToTokensTrade(
-      trade: UniSwapTrade,
-      options: {
-        onConfirm?: (hash: string) => void,
-        onApprove?: (hash: string) => void
-      } = { }
+    trade: UniSwapTrade,
+    options: {
+      onConfirm?: (hash: string) => void;
+      onApprove?: (hash: string) => void;
+    } = {}
   ): Promise<TransactionReceipt> {
-
     trade.path[0] = this.WETH.address;
 
     return this.web3Api.executeContractMethod(
-        UniSwapContractAddress,
-        UniSwapContractAbi,
-        'swapExactETHForTokens',
-        [trade.amountOutMin, trade.path, trade.to, trade.deadline],
-        {
-          onTransactionHash: options.onConfirm,
-          value: trade.amountIn
-        }
+      UniSwapContractAddress,
+      UniSwapContractAbi,
+      'swapExactETHForTokens',
+      [trade.amountOutMin, trade.path, trade.to, trade.deadline],
+      {
+        onTransactionHash: options.onConfirm,
+        value: trade.amountIn
+      }
     );
   }
 
   private async createTokensToEthTrade(
-      trade: UniSwapTrade,
-      options: {
-        onConfirm?: (hash: string) => void,
-        onApprove?: (hash: string) => void
-      } = { }
+    trade: UniSwapTrade,
+    options: {
+      onConfirm?: (hash: string) => void;
+      onApprove?: (hash: string) => void;
+    } = {}
   ): Promise<TransactionReceipt> {
-
     trade.path[1] = this.WETH.address;
 
     await this.provideAllowance(trade.path[0], new BigNumber(trade.amountIn), options.onApprove);
 
     return this.web3Api.executeContractMethod(
-        UniSwapContractAddress,
-        UniSwapContractAbi,
-        'swapExactTokensForETH',
-        [trade.amountIn, trade.amountOutMin, trade.path, trade.to, trade.deadline],
-        {
-          onTransactionHash: options.onConfirm
-        }
+      UniSwapContractAddress,
+      UniSwapContractAbi,
+      'swapExactTokensForETH',
+      [trade.amountIn, trade.amountOutMin, trade.path, trade.to, trade.deadline],
+      {
+        onTransactionHash: options.onConfirm
+      }
     );
   }
 
   private async createTokensToTokensTrade(
-      trade: UniSwapTrade,
-      options: {
-        onConfirm?: (hash: string) => void,
-        onApprove?: (hash: string) => void
-      } = { }
+    trade: UniSwapTrade,
+    options: {
+      onConfirm?: (hash: string) => void;
+      onApprove?: (hash: string) => void;
+    } = {}
   ): Promise<TransactionReceipt> {
-
     await this.provideAllowance(trade.path[0], new BigNumber(trade.amountIn), options.onApprove);
 
     return this.web3Api.executeContractMethod(
-        UniSwapContractAddress,
-        UniSwapContractAbi,
-        'swapExactTokensForTokensSupportingFeeOnTransferTokens',
-        [trade.amountIn, trade.amountOutMin, trade.path, trade.to, trade.deadline],
-        { onTransactionHash: options.onConfirm }
+      UniSwapContractAddress,
+      UniSwapContractAbi,
+      'swapExactTokensForTokensSupportingFeeOnTransferTokens',
+      [trade.amountIn, trade.amountOutMin, trade.path, trade.to, trade.deadline],
+      { onTransactionHash: options.onConfirm }
     );
   }
 
-  private async provideAllowance(tokenAddress: string, value: BigNumber, onApprove?: (hash: string) => void): Promise<void> {
+  private async provideAllowance(
+    tokenAddress: string,
+    value: BigNumber,
+    onApprove?: (hash: string) => void
+  ): Promise<void> {
     const allowance = await this.web3Api.getAllowance(tokenAddress, UniSwapContractAddress);
     if (value.gt(allowance)) {
       const uintInfinity = new BigNumber(2).pow(256).minus(1);
-      await this.web3Api.approveTokens(tokenAddress, UniSwapContractAddress, uintInfinity, { onTransactionHash: onApprove });
+      await this.web3Api.approveTokens(tokenAddress, UniSwapContractAddress, uintInfinity, {
+        onTransactionHash: onApprove
+      });
     }
   }
 
-  private async getUniSwapTrade(fromAmount: BigNumber, fromToken: InstantTradeToken, toToken: InstantTradeToken, chainId?): Promise<Trade> {
-    const uniSwapFromToken = new Token(chainId || ChainId.MAINNET, fromToken.address, fromToken.decimals);
+  private async getUniSwapTrade(
+    fromAmount: BigNumber,
+    fromToken: InstantTradeToken,
+    toToken: InstantTradeToken,
+    chainId?
+  ): Promise<Trade> {
+    const uniSwapFromToken = new Token(
+      chainId || ChainId.MAINNET,
+      fromToken.address,
+      fromToken.decimals
+    );
     const uniSwapToToken = new Token(chainId || ChainId.MAINNET, toToken.address, toToken.decimals);
     const pair = await Fetcher.fetchPairData(uniSwapFromToken, uniSwapToToken, this.provider);
     const route = new Route([pair], uniSwapFromToken);
 
     const fullFromAmount = fromAmount.multipliedBy(10 ** fromToken.decimals);
 
-    return  new Trade(route, new TokenAmount(uniSwapFromToken, fullFromAmount.toString()), TradeType.EXACT_INPUT);
+    return new Trade(
+      route,
+      new TokenAmount(uniSwapFromToken, fullFromAmount.toString()),
+      TradeType.EXACT_INPUT
+    );
   }
 }
--- src/app/services/web3Api/web3-api.service.spec.ts
+++ src/app/services/web3Api/web3-api.service.spec.ts
@@ -1,13 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Web3ApiService } from './web3-api.service';
-import {ProviderService} from '../provider/provider.service';
-import Web3 from 'web3';
-import {HttpClient, HttpHandler} from '@angular/common/http';
+import { ProviderService } from '../provider/provider.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import BigNumber from 'bignumber.js';
 import providerServiceStub from '../provider/provider-service-stub';
-import {WEENUS} from '../../../test/tokens/eth-tokens';
-
+import { WEENUS } from '../../../test/tokens/eth-tokens';
 
 describe('Web3ApiService', () => {
   let originalTimeout;
@@ -17,7 +15,11 @@ describe('Web3ApiService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler, { provide: ProviderService, useValue: providerServiceStub() }]
+      providers: [
+        HttpClient,
+        HttpHandler,
+        { provide: ProviderService, useValue: providerServiceStub() }
+      ]
     });
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
@@ -39,59 +41,65 @@ describe('Web3ApiService', () => {
     expect(network.id).toBe(42);
   });
 
-  it('get balance works', async (done) => {
+  it('get balance works', async done => {
     const balance = await service.getBalance();
     expect(balance).not.toBe(undefined);
     expect(balance.gt(0)).toBeTruthy();
     done();
   });
 
-  it('balance of works (tokens)', async (done) => {
+  it('balance of works (tokens)', async done => {
     const balance = await service.getTokenBalance(WEENUS.address);
     expect(balance).not.toBe(undefined);
     expect(balance.gt(0)).toBeTruthy();
     done();
   });
 
-  it ('send transaction', async (done) => {
+  it('send transaction', async done => {
     const amount = new BigNumber(0.001);
-    const bobStartBalance = await service.getBalance({address: bobAddress});
+    const bobStartBalance = await service.getBalance({ address: bobAddress });
     const callbackObject = {
-      onTransactionHash: (hash: string) => { }
+      onTransactionHash: (hash: string) => {}
     };
     spyOn(callbackObject, 'onTransactionHash');
 
-    const receipt = await service.sendTransaction(bobAddress, amount,
-        { onTransactionHash: callbackObject.onTransactionHash.bind(callbackObject)});
+    const receipt = await service.sendTransaction(bobAddress, amount, {
+      onTransactionHash: callbackObject.onTransactionHash.bind(callbackObject)
+    });
 
-    expect(callbackObject.onTransactionHash).toHaveBeenCalledWith(jasmine.stringMatching(/^0x([A-Fa-f0-9]{64})$/));
+    expect(callbackObject.onTransactionHash).toHaveBeenCalledWith(
+      jasmine.stringMatching(/^0x([A-Fa-f0-9]{64})$/)
+    );
     expect(receipt).not.toBe(undefined);
     expect(receipt.blockNumber > 0).toBeTruthy();
-    const bobNewBalance = await service.getBalance({ address: bobAddress} );
+    const bobNewBalance = await service.getBalance({ address: bobAddress });
     expect(bobNewBalance.minus(bobStartBalance).toString()).toBe(amount.toString());
     done();
   });
 
-  it('send tokens', async (done) => {
+  it('send tokens', async done => {
     const amount = new BigNumber(3).multipliedBy(10 ** WEENUS.decimals);
-    const bobStartBalance = await service.getTokenBalance(WEENUS.address, {address: bobAddress});
+    const bobStartBalance = await service.getTokenBalance(WEENUS.address, { address: bobAddress });
     const callbackObject = {
-      onTransactionHash: (hash: string) => { }
+      onTransactionHash: (hash: string) => {}
     };
     spyOn(callbackObject, 'onTransactionHash');
 
-    const receipt = await service.transferTokens(WEENUS.address, bobAddress, amount,
-        { onTransactionHash: callbackObject.onTransactionHash.bind(callbackObject)});
+    const receipt = await service.transferTokens(WEENUS.address, bobAddress, amount, {
+      onTransactionHash: callbackObject.onTransactionHash.bind(callbackObject)
+    });
 
-    expect(callbackObject.onTransactionHash).toHaveBeenCalledWith(jasmine.stringMatching(/^0x([A-Fa-f0-9]{64})$/));
+    expect(callbackObject.onTransactionHash).toHaveBeenCalledWith(
+      jasmine.stringMatching(/^0x([A-Fa-f0-9]{64})$/)
+    );
     expect(receipt).not.toBe(undefined);
     expect(receipt.blockNumber > 0).toBeTruthy();
-    const bobNewBalance = await service.getTokenBalance(WEENUS.address, { address: bobAddress});
+    const bobNewBalance = await service.getTokenBalance(WEENUS.address, { address: bobAddress });
     expect(bobNewBalance.minus(bobStartBalance).toString()).toBe(amount.toString());
     done();
   });
 
-  it('allowance', async (done) => {
+  it('allowance', async done => {
     const allowance = await service.getAllowance(WEENUS.address, bobAddress);
 
     console.log(allowance);
@@ -100,19 +108,22 @@ describe('Web3ApiService', () => {
     done();
   });
 
-  it('approve', async (done) => {
+  it('approve', async done => {
     const amount = new BigNumber(2.39).multipliedBy(10 ** WEENUS.decimals);
     const bobStartAllowance = await service.getAllowance(WEENUS.address, bobAddress);
     console.log(bobStartAllowance.toString());
     const callbackObject = {
-      onTransactionHash: (hash: string) => { }
+      onTransactionHash: (hash: string) => {}
     };
     spyOn(callbackObject, 'onTransactionHash');
 
-    const receipt = await service.approveTokens(WEENUS.address, bobAddress, amount,
-        { onTransactionHash: callbackObject.onTransactionHash.bind(callbackObject)});
+    const receipt = await service.approveTokens(WEENUS.address, bobAddress, amount, {
+      onTransactionHash: callbackObject.onTransactionHash.bind(callbackObject)
+    });
 
-    expect(callbackObject.onTransactionHash).toHaveBeenCalledWith(jasmine.stringMatching(/^0x([A-Fa-f0-9]{64})$/));
+    expect(callbackObject.onTransactionHash).toHaveBeenCalledWith(
+      jasmine.stringMatching(/^0x([A-Fa-f0-9]{64})$/)
+    );
     expect(receipt).not.toBe(undefined);
     expect(receipt.blockNumber > 0).toBeTruthy();
     const bobNewAllowance = await service.getAllowance(WEENUS.address, bobAddress);
@@ -120,7 +131,7 @@ describe('Web3ApiService', () => {
     done();
   });
 
-  it('unApprove', async (done) => {
+  it('unApprove', async done => {
     await service.unApprove(WEENUS.address, bobAddress);
 
     const allowance = await service.getAllowance(WEENUS.address, bobAddress);
