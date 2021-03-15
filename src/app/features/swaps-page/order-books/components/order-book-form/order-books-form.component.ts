db23a8f01e4ac3a9b393b6db1beb9fe18c3afbce
ottebrut <ott.andrey.ao@gmail.com>
2021-03-15 12:08:56 +0300

    order book is transferred

---
+++ src/app/features/swaps-page/order-books/components/order-book-form/order-books-form.component.ts
@@ -0,0 +1,159 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { BLOCKCHAIN_NAME } from 'src/app/shared/models/blockchain/BLOCKCHAIN_NAME';
+import { Token } from 'src/app/shared/models/tokens/Token';
+import { OrderBookToken, OrderBookTokens, TradeInfo } from 'src/app/core/services/order-book/types';
+import { OrderBookService } from 'src/app/core/services/order-book/order-book.service';
+
+@Component({
+  selector: 'app-order-books-form',
+  templateUrl: './order-books-form.component.html',
+  styleUrls: ['./order-books-form.component.scss']
+})
+export class OrderBooksFormComponent {
+  @Input()
+  set blockchain(value: BLOCKCHAIN_NAME) {
+    if (this._blockchain && this._blockchain !== value) {
+      this.resetTokens();
+    }
+    this._blockchain = value;
+    this.tradeInfo = { ...this.tradeInfo, blockchain: this._blockchain };
+
+    setTimeout(() => {
+      this.updateCustomTokenAddresses();
+    });
+  }
+
+  get blockchain(): BLOCKCHAIN_NAME {
+    return this._blockchain;
+  }
+
+  private _blockchain: BLOCKCHAIN_NAME;
+
+  @Input()
+  set tokens(value: OrderBookTokens) {
+    this.tradeInfo.tokens = value;
+  }
+
+  @ViewChild('baseCustomToken') baseCustomToken: NgModel;
+
+  @ViewChild('quoteCustomToken') quoteCustomToken: NgModel;
+
+  get tradeInfo(): TradeInfo {
+    return this._tradeInfo;
+  }
+
+  set tradeInfo(value) {
+    this._tradeInfo = value;
+
+    this.checkIfCreateTradeIsAvailable();
+  }
+
+  private _tradeInfo: TradeInfo;
+
+  public isCreateTradeAvailable: boolean;
+
+  public isCustomTokenSectionOpened = {
+    base: false,
+    quote: false
+  };
+
+  public customTokens = {
+    base: {} as OrderBookToken,
+    quote: {} as OrderBookToken
+  };
+
+  public isAdvancedSectionOpened: boolean = false;
+
+  get areAdvancedOptionsValid(): boolean {
+    return this._areAdvancedOptionsValid;
+  }
+
+  set areAdvancedOptionsValid(value) {
+    this._areAdvancedOptionsValid = value;
+
+    this.checkIfCreateTradeIsAvailable();
+  }
+
+  public _areAdvancedOptionsValid: boolean;
+
+  constructor(private orderBookService: OrderBookService) {
+    this.isCreateTradeAvailable = false;
+
+    this.tradeInfo = {
+      tokens: { base: {} as OrderBookToken, quote: {} as OrderBookToken }
+    } as TradeInfo;
+  }
+
+  private resetTokens(): void {
+    const defaultTokenInfo = {
+      address: '',
+      name: '',
+      symbol: ''
+    };
+    this.tradeInfo.tokens = {
+      base: {
+        ...this.tradeInfo.tokens.base,
+        ...defaultTokenInfo
+      },
+      quote: {
+        ...this.tradeInfo.tokens.quote,
+        ...defaultTokenInfo
+      }
+    };
+  }
+
+  public addCustomToken(tokenPart: string, tokenBody: Token): void {
+    this.customTokens[tokenPart] = { ...this.customTokens[tokenPart], ...tokenBody };
+  }
+
+  private updateCustomTokenAddresses(): void {
+    this.baseCustomToken?.control.updateValueAndValidity();
+    this.quoteCustomToken?.control.updateValueAndValidity();
+  }
+
+  public setCustomToken(tokenPart: string): void {
+    this.tradeInfo = {
+      ...this.tradeInfo,
+      tokens: {
+        ...this.tradeInfo.tokens,
+        [tokenPart]: {
+          ...this.tradeInfo.tokens[tokenPart],
+          ...this.customTokens[tokenPart]
+        }
+      }
+    };
+  }
+
+  public onAmountChanges(tokenPart: string, amount: string): void {
+    this.tradeInfo = {
+      ...this.tradeInfo,
+      tokens: {
+        ...this.tradeInfo.tokens,
+        [tokenPart]: {
+          ...this.tradeInfo.tokens[tokenPart],
+          amount
+        }
+      }
+    };
+  }
+
+  private checkIfCreateTradeIsAvailable(): void {
+    this.isCreateTradeAvailable = !!(
+      this._areAdvancedOptionsValid &&
+      this.tradeInfo.tokens &&
+      this.tradeInfo.tokens.base.address &&
+      this.tradeInfo.tokens.base.amount &&
+      this.tradeInfo.tokens.quote.address &&
+      this.tradeInfo.tokens.quote.amount
+    );
+  }
+
+  public async createTrade(): Promise<void> {
+    try {
+      await this.orderBookService.createOrder(this.tradeInfo);
+    } catch (err) {
+      console.log('err', err);
+    }
+  }
+}
--- src/app/features/swaps-page/order-books/components/order-books/order-books.component.ts
+++ src/app/features/swaps-page/order-books/components/order-books/order-books.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-books.component.scss']
 })
 export class OrderBooksComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
