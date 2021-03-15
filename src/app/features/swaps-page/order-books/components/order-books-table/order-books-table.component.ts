7f08fbd584858477dfa4283e82915140b4b261ec
ottebrut <ott.andrey.ao@gmail.com>
2021-03-15 19:30:44 +0300

    order book table template is prepared

---
+++ src/app/features/swaps-page/order-books/components/order-books-table/order-books-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-books-table',
+  templateUrl: './order-books-table.component.html',
+  styleUrls: ['./order-books-table.component.scss']
+})
+export class OrderBooksTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/features/swaps-page/order-books/order-books.module.ts
+++ src/app/features/swaps-page/order-books/order-books.module.ts
@@ -6,13 +6,19 @@ import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { OrderBooksComponent } from './components/order-books/order-books.component';
 import { TradesModule } from '../trades-module/trades.module';
-import { OrderBooksFormComponent } from './components/order-book-form/order-books-form.component';
-import { OrderBooksFormOptionsComponent } from './components/order-book-form/order-books-form-options/order-books-form-options.component';
+import { OrderBooksFormComponent } from './components/order-books-form/order-books-form.component';
+import { OrderBooksFormOptionsComponent } from './components/order-books-form/order-books-form-options/order-books-form-options.component';
 import { SharedModule } from '../../../shared/shared.module';
-import { OrderBooksFormService } from './components/order-book-form/services/order-books-form.service';
+import { OrderBooksFormService } from './components/order-books-form/services/order-books-form.service';
+import { OrderBooksTableComponent } from './components/order-books-table/order-books-table.component';
 
 @NgModule({
-  declarations: [OrderBooksComponent, OrderBooksFormComponent, OrderBooksFormOptionsComponent],
+  declarations: [
+    OrderBooksComponent,
+    OrderBooksFormComponent,
+    OrderBooksFormOptionsComponent,
+    OrderBooksTableComponent
+  ],
   imports: [
     CommonModule,
     TradesModule,
