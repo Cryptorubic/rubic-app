626e952bc5bdf562030b9f768f30c033fe369d70
ottebrut <ott.andrey.ao@gmail.com>
2021-03-17 13:27:39 +0300

    create-order is separated into another service

--- src/app/core/services/order-book/order-book.service.ts
+++ src/app/core/services/order-book/order-book.service.ts
@@ -5,9 +5,7 @@ import { CONTRACT } from './smart-contract';
 import { OrderBookApiService } from '../backend/order-book-api/order-book-api.service';
 import { Web3Public } from '../blockchain/web3-public-service/Web3Public';
 import { Web3PublicService } from '../blockchain/web3-public-service/web3-public.service';
-import { Web3PrivateService } from '../blockchain/web3-private-service/web3-private.service';
 import { OrderBookDataToken, TokenPart } from './types/tokens';
-import { OrderBookTradeForm } from './types/trade-form';
 import { OrderBookTradeApi } from './types/trade-api';
 import { ORDER_BOOK_TRADE_STATUS, OrderBookTradeData } from './types/trade-page';
 import SwapToken from '../../../shared/models/tokens/SwapToken';
@@ -22,127 +20,19 @@ interface Web3PublicParameters {
   providedIn: 'root'
 })
 export class OrderBookService {
-  private readonly EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
+  static readonly EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
 
   constructor(
     private orderBookApiService: OrderBookApiService,
-    private web3PublicService: Web3PublicService,
-    private web3PrivateService: Web3PrivateService
+    private web3PublicService: Web3PublicService
   ) {}
 
-  private static tokenAmountToWei(token: SwapToken, amount: string | BigNumber): string {
+  static tokenAmountToWei(token: SwapToken, amount: string | BigNumber): string {
     return new BigNumber(amount || '0').times(new BigNumber(10).pow(token.decimals)).toFixed(0);
   }
 
   private static tokenWeiToAmount(token: SwapToken, amount: string): BigNumber {
     return new BigNumber(amount).div(new BigNumber(10).pow(token.decimals));
-  }
-
-  /**
-   * @description creates order book through smart contract and then makes post request to backend
-   * @param tradeInfo information about the trade
-   */
-  public async createOrder(tradeInfo: OrderBookTradeForm): Promise<void> {
-    const web3Public: Web3Public = this.web3PublicService[tradeInfo.blockchain];
-
-    const contractAddress = CONTRACT.ADDRESSES[2][tradeInfo.blockchain];
-    const contractAbi = CONTRACT.ABI[2] as any[];
-
-    const fee: string = await web3Public.callContractMethod(
-      contractAddress,
-      contractAbi,
-      'feeAmount'
-    );
-
-    const tradeInfoApi = this.generateCreateSwapApiObject(tradeInfo);
-    const args = await this.generateCreateOrderArguments(tradeInfoApi);
-    const receipt = await this.web3PrivateService.executeContractMethod(
-      contractAddress,
-      contractAbi,
-      'createOrder',
-      args,
-      {
-        value: fee
-      }
-    );
-    tradeInfoApi.memo_contract = receipt.events.OrderCreated.returnValues.id;
-
-    await this.orderBookApiService.createTrade(tradeInfoApi);
-  }
-
-  private generateCreateSwapApiObject(tradeInfo: OrderBookTradeForm): OrderBookTradeApi {
-    let network;
-    switch (tradeInfo.blockchain) {
-      case BLOCKCHAIN_NAME.ETHEREUM:
-        network = 1;
-        break;
-      case BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN:
-        network = 22;
-        break;
-      case BLOCKCHAIN_NAME.MATIC:
-        network = 24;
-      // no default
-    }
-
-    return {
-      memo_contract: '',
-      contract_address: CONTRACT.ADDRESSES[2][tradeInfo.blockchain],
-      base_address: tradeInfo.token.base.address,
-      quote_address: tradeInfo.token.quote.address,
-      base_limit: OrderBookService.tokenAmountToWei(
-        tradeInfo.token.base,
-        tradeInfo.token.base.amount
-      ),
-      quote_limit: OrderBookService.tokenAmountToWei(
-        tradeInfo.token.quote,
-        tradeInfo.token.quote.amount
-      ),
-      stop_date: tradeInfo.stopDate,
-      public: tradeInfo.isPublic,
-      min_base_wei: OrderBookService.tokenAmountToWei(
-        tradeInfo.token.base,
-        tradeInfo.token.base.minContribution
-      ),
-      min_quote_wei: OrderBookService.tokenAmountToWei(
-        tradeInfo.token.quote,
-        tradeInfo.token.quote.minContribution
-      ),
-      base_amount_contributed: '0',
-      quote_amount_contributed: '0',
-      broker_fee: tradeInfo.isWithBrokerFee,
-      broker_fee_address: tradeInfo.isWithBrokerFee ? tradeInfo.brokerAddress : this.EMPTY_ADDRESS,
-      broker_fee_base: parseInt(tradeInfo.token.base.brokerPercent),
-      broker_fee_quote: parseInt(tradeInfo.token.quote.brokerPercent),
-
-      name: `${tradeInfo.token.base.symbol} <> ${tradeInfo.token.quote.symbol}`,
-      network,
-      state: 'ACTIVE',
-      contract_state: 'ACTIVE',
-      contract_type: 20,
-      notification: false,
-      permanent: false,
-      is_rubic_order: true,
-      rubic_initialized: true
-    };
-  }
-
-  private generateCreateOrderArguments(tradeInfoApi: OrderBookTradeApi): any[] {
-    // eslint-disable-next-line no-magic-numbers
-    const stopDate = Math.round(new Date(tradeInfoApi.stop_date).getTime() / 1000).toString();
-
-    return [
-      tradeInfoApi.base_address,
-      tradeInfoApi.quote_address,
-      tradeInfoApi.base_limit,
-      tradeInfoApi.quote_limit,
-      stopDate,
-      this.EMPTY_ADDRESS, // whitelist_address
-      tradeInfoApi.min_base_wei,
-      tradeInfoApi.min_quote_wei,
-      tradeInfoApi.broker_fee_address,
-      tradeInfoApi.broker_fee_base * 100,
-      tradeInfoApi.broker_fee_base * 100
-    ];
   }
 
   public async getTradeData(uniqueLink: string): Promise<OrderBookTradeData> {
--- src/app/features/swaps-page/order-books/components/order-books-form/services/order-books-form.service.ts
+++ src/app/features/swaps-page/order-books/components/order-books-form/services/order-books-form.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { OrderBookTradeForm } from 'src/app/core/services/order-book/types/trade-form';
 import { OrderBookFormToken } from 'src/app/core/services/order-book/types/tokens';
+import { Web3Public } from 'src/app/core/services/blockchain/web3-public-service/Web3Public';
+import { CONTRACT } from 'src/app/core/services/order-book/smart-contract';
+import { OrderBookTradeApi } from 'src/app/core/services/order-book/types/trade-api';
+import { BLOCKCHAIN_NAME } from 'src/app/shared/models/blockchain/BLOCKCHAIN_NAME';
+import { Web3PublicService } from 'src/app/core/services/blockchain/web3-public-service/web3-public.service';
+import { Web3PrivateService } from 'src/app/core/services/blockchain/web3-private-service/web3-private.service';
+import { OrderBookApiService } from 'src/app/core/services/backend/order-book-api/order-book-api.service';
+import { OrderBookService } from 'src/app/core/services/order-book/order-book.service';
 
 @Injectable()
 export class OrderBooksFormService {
@@ -12,7 +20,12 @@ export class OrderBooksFormService {
     }
   } as OrderBookTradeForm);
 
-  constructor() {}
+  constructor(
+    private orderBookService: OrderBookService,
+    private orderBookApiService: OrderBookApiService,
+    private web3PublicService: Web3PublicService,
+    private web3PrivateService: Web3PrivateService
+  ) {}
 
   public getTradeForm(): Observable<OrderBookTradeForm> {
     return this._tradeForm.asObservable();
@@ -20,5 +33,108 @@ export class OrderBooksFormService {
 
   public setTradeForm(tradeForm: OrderBookTradeForm): void {
     return this._tradeForm.next(tradeForm);
+  }
+
+  /**
+   * @description creates order book through smart contract and then makes post request to backend-api
+   * @param tradeForm information about the trade
+   */
+  public async createOrder(tradeForm: OrderBookTradeForm): Promise<void> {
+    const web3Public: Web3Public = this.web3PublicService[tradeForm.blockchain];
+
+    const contractAddress = CONTRACT.ADDRESSES[2][tradeForm.blockchain];
+    const contractAbi = CONTRACT.ABI[2] as any[];
+
+    const tradeApi = this.generateTradeApiObject(tradeForm);
+
+    const fee: string = await web3Public.callContractMethod(
+      contractAddress,
+      contractAbi,
+      'feeAmount'
+    );
+
+    const createOrderArguments = [
+      tradeApi.base_address,
+      tradeApi.quote_address,
+      tradeApi.base_limit,
+      tradeApi.quote_limit,
+      Math.round(new Date(tradeApi.stop_date).getTime() / 1000).toString(), // stop_date in seconds
+      OrderBookService.EMPTY_ADDRESS, // whitelist_address
+      tradeApi.min_base_wei,
+      tradeApi.min_quote_wei,
+      tradeApi.broker_fee_address,
+      tradeApi.broker_fee_base * 100,
+      tradeApi.broker_fee_base * 100
+    ];
+    const receipt = await this.web3PrivateService.executeContractMethod(
+      contractAddress,
+      contractAbi,
+      'createOrder',
+      createOrderArguments,
+      {
+        value: fee
+      }
+    );
+    tradeApi.memo_contract = receipt.events.OrderCreated.returnValues.id;
+
+    await this.orderBookApiService.createTrade(tradeApi);
+  }
+
+  private generateTradeApiObject(tradeForm: OrderBookTradeForm): OrderBookTradeApi {
+    let network;
+    switch (tradeForm.blockchain) {
+      case BLOCKCHAIN_NAME.ETHEREUM:
+        network = 1;
+        break;
+      case BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN:
+        network = 22;
+        break;
+      case BLOCKCHAIN_NAME.MATIC:
+        network = 24;
+      // no default
+    }
+
+    return {
+      memo_contract: '',
+      contract_address: CONTRACT.ADDRESSES[2][tradeForm.blockchain],
+      base_address: tradeForm.token.base.address,
+      quote_address: tradeForm.token.quote.address,
+      base_limit: OrderBookService.tokenAmountToWei(
+        tradeForm.token.base,
+        tradeForm.token.base.amount
+      ),
+      quote_limit: OrderBookService.tokenAmountToWei(
+        tradeForm.token.quote,
+        tradeForm.token.quote.amount
+      ),
+      stop_date: tradeForm.stopDate,
+      public: tradeForm.isPublic,
+      min_base_wei: OrderBookService.tokenAmountToWei(
+        tradeForm.token.base,
+        tradeForm.token.base.minContribution
+      ),
+      min_quote_wei: OrderBookService.tokenAmountToWei(
+        tradeForm.token.quote,
+        tradeForm.token.quote.minContribution
+      ),
+      base_amount_contributed: '0',
+      quote_amount_contributed: '0',
+      broker_fee: tradeForm.isWithBrokerFee,
+      broker_fee_address: tradeForm.isWithBrokerFee
+        ? tradeForm.brokerAddress
+        : OrderBookService.EMPTY_ADDRESS,
+      broker_fee_base: parseInt(tradeForm.token.base.brokerPercent),
+      broker_fee_quote: parseInt(tradeForm.token.quote.brokerPercent),
+
+      name: `${tradeForm.token.base.symbol} <> ${tradeForm.token.quote.symbol}`,
+      network,
+      state: 'ACTIVE',
+      contract_state: 'ACTIVE',
+      contract_type: 20,
+      notification: false,
+      permanent: false,
+      is_rubic_order: true,
+      rubic_initialized: true
+    };
   }
 }
