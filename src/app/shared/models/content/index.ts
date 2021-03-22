350466b32e0a36f00c20418ac1cccc83570aec2d
siandrev <andreev.sergey.i@yandex.ru>
2021-03-22 17:28:32 +0300

    Content updated

--- src/app/shared/models/content/index.ts
+++ src/app/shared/models/content/index.ts
@@ -1,6 +1,4 @@
 export { default as Content } from './Content';
 export { default as VolumeContent } from './VolumeContent';
 export { default as TeamCardContent } from './TeamCardContent';
-export { default as Collaborations } from './CollaborationsContent';
-
-export class CollaborationsContent {}
+export { default as CollaborationsContent } from './CollaborationsContent';
