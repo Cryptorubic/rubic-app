f4f9cf39fc6d71833ee7a16c013cc7e6735bfe39
vlad3k <vlad3k@bk.ru>
2021-04-14 18:50:10 +0300

    add sections create bridge

---
+++ src/app/features/bridge-page/components/bridge-section/bridge-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bridge-section',
+  templateUrl: './bridge-section.component.html',
+  styleUrls: ['./bridge-section.component.scss']
+})
+export class BridgeSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
