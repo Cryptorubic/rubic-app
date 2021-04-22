238f7aeb72cda013787d5d91f5a32edd52dcaa3d
ottebrut <ott.andrey.ao@gmail.com>
2021-04-22 13:36:58 +0300

    total-supply-overflow-error.component test is fixed

--- src/app/shared/components/errors/total-supply-overflow-error/total-supply-overflow-error.component.spec.ts
+++ src/app/shared/components/errors/total-supply-overflow-error/total-supply-overflow-error.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { TranslateModule } from '@ngx-translate/core';
 import { TotalSupplyOverflowErrorComponent } from './total-supply-overflow-error.component';
+import { TotalSupplyOverflowError } from '../../../models/errors/order-book/TotalSupplyOverflowError';
 
 describe('TotalSupplyOverflowErrorComponent', () => {
   let component: TotalSupplyOverflowErrorComponent;
@@ -8,6 +10,7 @@ describe('TotalSupplyOverflowErrorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
       declarations: [TotalSupplyOverflowErrorComponent]
     }).compileComponents();
   });
@@ -15,6 +18,7 @@ describe('TotalSupplyOverflowErrorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TotalSupplyOverflowErrorComponent);
     component = fixture.componentInstance;
+    component.totalSupplyOverflowError = new TotalSupplyOverflowError('RBC', '124,000,000');
     fixture.detectChanges();
   });
 
