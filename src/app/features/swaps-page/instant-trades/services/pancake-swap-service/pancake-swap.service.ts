908b866d6344a15746c224b402548461c2f6b6cc
siandrev <andreev.sergey.i@yandex.ru>
2021-04-22 17:43:28 +0300

    useTestingMode fixes

--- src/app/features/swaps-page/instant-trades/services/pancake-swap-service/pancake-swap.service.ts
+++ src/app/features/swaps-page/instant-trades/services/pancake-swap-service/pancake-swap.service.ts
@@ -34,5 +34,11 @@ export class PancakeSwapService extends UniswapAbstract {
     this.web3Public = web3Public[BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN];
     this.blockchain = BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN;
     this.shouldCalculateGas = false;
+
+    useTestingModeService.isTestingMode.subscribe(value => {
+      if (value) {
+        this.web3Public = web3Public[BLOCKCHAIN_NAME.ETHEREUM];
+      }
+    });
   }
 }
