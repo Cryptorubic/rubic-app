769261744073b12c33493cc5fea50bb252cf5ad1
vlad3k <vlad3k@bk.ru>
2021-04-26 14:04:12 +0300

    setup new language structure

--- src/app/core/header/models/languages-list.ts
+++ src/app/core/header/models/languages-list.ts
@@ -6,39 +6,19 @@ export const languagesList: LanguageListElement[] = [
     title: 'English'
   },
   {
-    lng: 'en-new',
-    title: 'English new'
-  },
-  {
     lng: 'ko',
     title: '한국어'
-  },
-  {
-    lng: 'ko-new',
-    title: '한국어 new'
   },
   {
     lng: 'zh',
     title: '中国'
   },
   {
-    lng: 'zh-new',
-    title: '中国 new'
-  },
-  {
     lng: 'ru',
     title: 'Русский'
   },
   {
-    lng: 'ru-new',
-    title: 'Русский new'
-  },
-  {
     lng: 'es',
     title: 'Español'
-  },
-  {
-    lng: 'es-new',
-    title: 'Español new'
   }
 ];
