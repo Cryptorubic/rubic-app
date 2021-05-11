5bdaab9c71ffa103d98fc29e35a8dda13782c3ad
axtezy <axtezy@gmail.com>
2021-05-11 18:30:39 +0300

    Iframe warning fix

--- src/app/core/services/use-testing-mode/use-testing-mode.service.ts
+++ src/app/core/services/use-testing-mode/use-testing-mode.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable, NgZone } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -7,11 +7,15 @@ import { BehaviorSubject } from 'rxjs';
 export class UseTestingModeService {
   public isTestingMode = new BehaviorSubject(false);
 
-  constructor() {
+  constructor(private zone: NgZone, private appRef: ApplicationRef) {
     window['useTestingMode'] = () => {
       if (!this.isTestingMode.getValue()) {
         this.isTestingMode.next(true);
       }
+      this.zone.run(() => {
+        setTimeout(() => this.appRef.tick(), 500);
+        setTimeout(() => this.appRef.tick(), 2000);
+      });
     };
   }
 }
