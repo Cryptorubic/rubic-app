689cc19d2cede952324a7ba9c814211430442a70
ottebrut <ott.andrey.ao@gmail.com>
2021-06-02 19:08:47 +0300

    Refresh button animation is smoothed

--- src/app/shared/components/refresh-button/refresh-tool.component.ts
+++ src/app/shared/components/refresh-button/refresh-tool.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 import { REFRESH_STATUS } from 'src/app/shared/models/instant-trade/REFRESH_STATUS';
 import Timeout = NodeJS.Timeout;
 
@@ -7,15 +15,13 @@ import Timeout = NodeJS.Timeout;
   templateUrl: './refresh-tool.component.html',
   styleUrls: ['./refresh-tool.component.scss']
 })
-export class RefreshToolComponent {
+export class RefreshToolComponent implements OnInit {
   /**
    * Timeout before next refreshing in seconds
    */
   @Input() readonly refreshTimeout = 30;
 
   @Input() set refreshStatus(status) {
-    this._refreshStatus = status;
-
     if (status !== REFRESH_STATUS.WAITING) {
       if (this._lastTimeoutId) {
         clearInterval(this._lastTimeoutId);
@@ -23,6 +29,15 @@ export class RefreshToolComponent {
     } else if (this.isWithAutoRefresh) {
       this._lastTimeoutId = setTimeout(() => this.onRefresh.emit(), this.refreshTimeout * 1000);
     }
+
+    if (status === REFRESH_STATUS.REFRESHING) {
+      this.refreshButtonStatus = 'refreshing';
+      this.refreshButton.nativeElement.classList.add('refresh-button_refreshing');
+    } else if (this._refreshStatus === REFRESH_STATUS.REFRESHING) {
+      this.refreshButtonStatus = 'stopped';
+    }
+
+    this._refreshStatus = status;
   }
 
   get refreshStatus(): REFRESH_STATUS {
@@ -30,6 +45,8 @@ export class RefreshToolComponent {
   }
 
   @Output() onRefresh = new EventEmitter<void>();
+
+  @ViewChild('refreshButton', { static: true }) refreshButton: ElementRef;
 
   public REFRESH_STATUS = REFRESH_STATUS;
 
@@ -52,7 +69,17 @@ export class RefreshToolComponent {
 
   private _isWithAutoRefresh = true;
 
+  public refreshButtonStatus: 'refreshing' | 'stopped' = 'stopped';
+
   constructor() {}
+
+  ngOnInit() {
+    this.refreshButton.nativeElement.addEventListener('animationiteration', () => {
+      if (this.refreshButtonStatus === 'stopped') {
+        this.refreshButton.nativeElement.classList.remove('refresh-button_refreshing');
+      }
+    });
+  }
 
   public onTimeoutClick(): void {
     this.isWithAutoRefresh = !this.isWithAutoRefresh;
