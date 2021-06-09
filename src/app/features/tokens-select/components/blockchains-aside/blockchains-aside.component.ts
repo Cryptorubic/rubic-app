44bd86d7e5dae487df1bfa204b8dadd3dcd91c90
siandrev <andreev.sergey.i@yandex.ru>
2021-06-09 17:23:00 +0300

    blockchains layout improved

--- src/app/features/tokens-select/components/blockchains-aside/blockchains-aside.component.ts
+++ src/app/features/tokens-select/components/blockchains-aside/blockchains-aside.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BLOCKCHAIN_NAME } from '../../../../shared/models/blockchain/BLOCKCHAIN_NAME';
+import { BlockchainsInfo } from '../../../../core/services/blockchain/blockchain-info';
 
 @Component({
   selector: 'app-blockchains-aside',
@@ -7,5 +9,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BlockchainsAsideComponent {
+  public blockchains: BLOCKCHAIN_NAME[] = [
+    BLOCKCHAIN_NAME.ETHEREUM,
+    BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN,
+    BLOCKCHAIN_NAME.POLYGON,
+    BLOCKCHAIN_NAME.TRON,
+    BLOCKCHAIN_NAME.XDAI
+  ];
+
+  public blockchainImages = Object.fromEntries(
+    this.blockchains.map(blockchainName => [
+      blockchainName,
+      BlockchainsInfo.getBlockchainByName(blockchainName).imagePath
+    ])
+  );
+
   constructor() {}
 }
