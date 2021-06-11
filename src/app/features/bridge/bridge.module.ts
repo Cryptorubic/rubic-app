abb2841a3b2d8df33f86d424ed18ffd48fa54ec8
siandrev <andreev.sergey.i@yandex.ru>
2021-06-11 18:48:07 +0300

    Swap provider added

--- src/app/features/bridge/bridge.module.ts
+++ src/app/features/bridge/bridge.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BridgeBottomFormComponent } from './components/bridge-bottom-form/bridge-bottom-form.component';
+import { BridgesSwapProviderService } from './services/bridges-swap-provider-service/bridges-swap-provider.service';
 
 @NgModule({
   declarations: [BridgeBottomFormComponent],
   exports: [BridgeBottomFormComponent],
-  imports: [CommonModule, SharedModule]
+  imports: [CommonModule, SharedModule],
+  providers: [BridgesSwapProviderService]
 })
 export class BridgeModule {}
--- src/app/features/instant-trade/instant-trade.module.ts
+++ src/app/features/instant-trade/instant-trade.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { InstantTradeBottomFormComponent } from './components/instant-trade-bottom-form/instant-trade-bottom-form.component';
+import { InstantTradesSwapProviderService } from './services/instant-trades-swap-provider-service/instant-trades-swap-provider.service';
 
 @NgModule({
   declarations: [InstantTradeBottomFormComponent],
   exports: [InstantTradeBottomFormComponent],
-  imports: [CommonModule, SharedModule]
+  imports: [CommonModule, SharedModule],
+  providers: [InstantTradesSwapProviderService]
 })
 export class InstantTradeModule {}
