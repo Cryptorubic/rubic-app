12f5c0b1a756574ede1d4cab04e1ec44b93a66ff
ottebrut <ott.andrey.ao@gmail.com>
2021-06-14 20:09:08 +0300

    Pagination and sorting is added for table

--- src/app/features/my-trades/components/my-trades/my-trades.component.ts
+++ src/app/features/my-trades/components/my-trades/my-trades.component.ts
@@ -3,6 +3,10 @@ import { BLOCKCHAIN_NAME } from 'src/app/shared/models/blockchain/BLOCKCHAIN_NAM
 import { NATIVE_TOKEN_ADDRESS } from 'src/app/shared/constants/blockchain/NATIVE_TOKEN_ADDRESS';
 import { BlockchainToken } from 'src/app/shared/models/tokens/BlockchainToken';
 import { BLOCKCHAINS } from 'src/app/shared/constants/blockchain/BLOCKCHAINS';
+import { BehaviorSubject, combineLatest, Subject, timer } from 'rxjs';
+import { defaultSort, TuiComparator } from '@taiga-ui/addon-table';
+import { debounceTime, filter, map, share, startWith } from 'rxjs/operators';
+import { isPresent } from '@taiga-ui/cdk';
 
 interface TableToken extends BlockchainToken {
   image: string;
@@ -16,7 +20,9 @@ interface TableTrade {
   date: Date;
 }
 
-interface TradeTableRow {
+type TableRowKey = 'Status' | 'From' | 'To' | 'Sent' | 'Expected' | 'Date';
+
+interface TableRow {
   Status: string;
   From: string;
   To: string;
@@ -24,6 +30,28 @@ interface TradeTableRow {
   Expected: number;
   Date: Date;
 }
+
+// example
+const ethToken: TableToken = {
+  blockchain: BLOCKCHAIN_NAME.ETHEREUM,
+  address: NATIVE_TOKEN_ADDRESS,
+  name: 'Ethereum',
+  symbol: 'ETH',
+  decimals: 18,
+  image: 'http://dev-api.rubic.exchange/media/token_images/cg_logo_ETH_ethereum_4jp3DKD.png',
+  amount: 50
+};
+
+const bscToken: TableToken = {
+  blockchain: BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN,
+  address: NATIVE_TOKEN_ADDRESS,
+  name: 'Binance',
+  symbol: 'BNB',
+  decimals: 18,
+  image:
+    'https://dev-api.rubic.exchange/media/token_images/cg_logo_bnb_binance-coin-logo_7bZIPmd.png',
+  amount: 50
+};
 
 @Component({
   selector: 'app-my-trades',
@@ -33,64 +61,133 @@ interface TradeTableRow {
 export class MyTradesComponent implements OnInit {
   public BLOCKCHAINS = BLOCKCHAINS;
 
-  private ethToken: TableToken = {
-    blockchain: BLOCKCHAIN_NAME.ETHEREUM,
-    address: NATIVE_TOKEN_ADDRESS,
-    name: 'Ethereum',
-    symbol: 'ETH',
-    decimals: 18,
-    image: 'http://dev-api.rubic.exchange/media/token_images/cg_logo_ETH_ethereum_4jp3DKD.png',
-    amount: 50
-  };
-
-  private bscToken: TableToken = {
-    blockchain: BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN,
-    address: NATIVE_TOKEN_ADDRESS,
-    name: 'Ethereum',
-    symbol: 'ETH',
-    decimals: 18,
-    image: 'http://dev-api.rubic.exchange/media/token_images/cg_logo_ETH_ethereum_4jp3DKD.png',
-    amount: 50
-  };
-
-  public readonly tableTrades: TableTrade[] = [
+  private readonly tableTrades: TableTrade[] = [
     {
       status: 'Waiting for deposit',
-      fromToken: this.ethToken,
-      toToken: this.bscToken,
+      fromToken: bscToken,
+      toToken: ethToken,
       date: new Date(Date.now())
     },
     {
       status: 'Completed',
-      fromToken: this.ethToken,
-      toToken: this.bscToken,
-      date: new Date(Date.now())
+      fromToken: ethToken,
+      toToken: bscToken,
+      date: new Date(Date.now() - 60000)
     },
     {
       status: 'Cancelled',
-      fromToken: this.ethToken,
-      toToken: this.bscToken,
-      date: new Date(Date.now())
+      fromToken: ethToken,
+      toToken: bscToken,
+      date: new Date(Date.now() - 3600000)
     }
   ];
 
-  public columns: string[];
+  private readonly tableData$ = new Subject<TableRow[]>();
 
-  public data: TradeTableRow[] = [];
+  public columns: string[] = [];
+
+  public readonly sorters: Record<TableRowKey, TuiComparator<TableRow>> = {
+    Status: () => 0,
+    From: () => 0,
+    To: () => 0,
+    Sent: () => 0,
+    Expected: () => 0,
+    Date: () => 0
+  };
+
+  public readonly sorter$ = new BehaviorSubject<TuiComparator<TableRow>>(this.sorters.Date);
+
+  public readonly direction$ = new BehaviorSubject<-1 | 1>(-1);
+
+  public readonly page$ = new Subject<number>();
+
+  public readonly size$ = new Subject<number>();
+
+  private readonly request$ = combineLatest([
+    this.sorter$.pipe(map(sorter => this.getTableRowKey(sorter, this.sorters))),
+    this.direction$,
+    this.page$.pipe(startWith(0)),
+    this.size$.pipe(startWith(10)),
+    this.tableData$
+  ]).pipe(
+    // zero time debounce for a case when both key and direction change
+    debounceTime(0),
+    startWith(null),
+    map(query => query && this.getData(...query)),
+    share()
+  );
+
+  public readonly loading$ = this.request$.pipe(map(value => !value));
+
+  public readonly visibleData$ = this.request$.pipe(
+    filter(isPresent),
+    map(tableRow => tableRow.filter(isPresent)),
+    startWith([])
+  );
+
+  public readonly total$ = this.request$.pipe(
+    filter(isPresent),
+    map(({ length }) => length),
+    startWith(1)
+  );
 
   constructor() {}
 
   ngOnInit(): void {
-    this.tableTrades.forEach(trade => {
-      this.data.push({
-        Status: trade.status,
-        From: trade.fromToken.blockchain,
-        To: trade.toToken.blockchain,
-        Sent: trade.fromToken.amount,
-        Expected: trade.toToken.amount,
-        Date: trade.date
-      });
+    // mock request
+    timer(1500).subscribe(() => {
+      if (this.tableTrades.length) {
+        for (let i = 0; i < 20; ++i) {
+          this.tableTrades.push(this.tableTrades[i % 3]);
+        }
+        const tableData = [];
+        this.tableTrades.forEach(trade => {
+          tableData.push({
+            Status: trade.status,
+            From: trade.fromToken.blockchain,
+            To: trade.toToken.blockchain,
+            Sent: trade.fromToken.amount,
+            Expected: trade.toToken.amount,
+            Date: trade.date
+          });
+        });
+        this.columns = Object.keys(tableData[0]);
+        this.tableData$.next(tableData);
+      } else {
+        this.tableData$.next([]);
+      }
     });
-    this.columns = Object.keys(this.data[0]);
+  }
+
+  private getTableRowKey(
+    sorter: TuiComparator<TableRow>,
+    dictionary: Record<TableRowKey, TuiComparator<TableRow>>
+  ): TableRowKey {
+    const pair = Object.entries(dictionary).find(
+      (item): item is [TableRowKey, TuiComparator<TableRow>] => item[1] === sorter
+    );
+    return pair ? pair[0] : 'Date';
+  }
+
+  private getData(
+    key: TableRowKey,
+    direction: -1 | 1,
+    page: number,
+    size: number,
+    tableData: TableRow[]
+  ): ReadonlyArray<TableRow | null> {
+    const start = page * size;
+    const end = start + size;
+    return [...tableData]
+      .sort(this.sortBy(key, direction))
+      .map((user, index) => (index >= start && index < end ? user : null));
+  }
+
+  private sortBy(key: TableRowKey, direction: -1 | 1): TuiComparator<TableRow> {
+    return (a, b) => direction * defaultSort(a[key], b[key]);
+  }
+
+  public getTableTrade(tableRow: any): TableTrade {
+    return this.tableTrades.find(trade => trade.date === tableRow.Date);
   }
 }
--- src/app/features/my-trades/my-trades.module.ts
+++ src/app/features/my-trades/my-trades.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyTradesRoutingModule } from 'src/app/features/my-trades/my-trades-routing.module';
-import { TuiTableModule } from '@taiga-ui/addon-table';
+import { TuiTableModule, TuiTablePaginationModule } from '@taiga-ui/addon-table';
+import { TuiLoaderModule } from '@taiga-ui/core';
+import { TuiLetModule } from '@taiga-ui/cdk';
+import { InlineSVGModule } from 'ng-inline-svg';
 import { MyTradesComponent } from './components/my-trades/my-trades.component';
 
 @NgModule({
   declarations: [MyTradesComponent],
-  imports: [CommonModule, MyTradesRoutingModule, TuiTableModule]
+  imports: [
+    CommonModule,
+    MyTradesRoutingModule,
+    TuiTableModule,
+    TuiLoaderModule,
+    TuiLetModule,
+    TuiTablePaginationModule,
+    InlineSVGModule
+  ]
 })
 export class MyTradesModule {}
