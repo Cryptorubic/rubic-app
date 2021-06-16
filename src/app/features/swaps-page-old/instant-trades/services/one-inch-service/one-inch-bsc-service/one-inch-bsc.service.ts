925da60096533e41d3ca2a2a1164016179244be5
axtezy <axtezy@gmail.com>
2021-06-16 11:17:49 +0300

    Linter fixes

--- src/app/features/swaps-page-old/instant-trades/services/one-inch-service/one-inch-bsc-service/one-inch-bsc.service.ts
+++ src/app/features/swaps-page-old/instant-trades/services/one-inch-service/one-inch-bsc-service/one-inch-bsc.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Web3PrivateService } from 'src/app/core/services/blockchain/web3-private-service/web3-private.service';
+import { Web3PublicService } from 'src/app/core/services/blockchain/web3-public-service/web3-public.service';
+import { CoingeckoApiService } from 'src/app/core/services/external-api/coingecko-api/coingecko-api.service';
+import { BLOCKCHAIN_NAME } from 'src/app/shared/models/blockchain/BLOCKCHAIN_NAME';
+import { UseTestingModeService } from 'src/app/core/services/use-testing-mode/use-testing-mode.service';
+import { ErrorsOldService } from 'src/app/core/services/errors-old/errors-old.service';
+import { BlockchainsInfo } from 'src/app/core/services/blockchain/blockchain-info';
 import { OneInchService } from '../one-inch-service';
-import { Web3PrivateService } from '../../../../../../core/services/blockchain/web3-private-service/web3-private.service';
-import { Web3PublicService } from '../../../../../../core/services/blockchain/web3-public-service/web3-public.service';
-import { CoingeckoApiService } from '../../../../../../core/services/external-api/coingecko-api/coingecko-api.service';
-import { BLOCKCHAIN_NAME } from '../../../../../../shared/models/blockchain/BLOCKCHAIN_NAME';
-import { UseTestingModeService } from '../../../../../../core/services/use-testing-mode/use-testing-mode.service';
-import { ErrorsOldService } from '../../../../../../core/services/errors-old/errors-old.service';
-import { BlockchainsInfo } from '../../../../../../core/services/blockchain/blockchain-info';
 
 @Injectable()
 export class OneInchBscService extends OneInchService {
