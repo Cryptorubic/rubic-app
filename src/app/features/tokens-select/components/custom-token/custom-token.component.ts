7012a83a41a180fc3f8f2926fd40bd15e2fa6f6b
siandrev <andreev.sergey.i@yandex.ru>
2021-06-17 17:47:22 +0300

    Custom token in bridges blocked

--- src/app/features/tokens-select/components/custom-token/custom-token.component.ts
+++ src/app/features/tokens-select/components/custom-token/custom-token.component.ts
@@ -5,13 +5,15 @@ import {
   Output,
   EventEmitter,
   Inject,
-  Injector
+  Injector,
+  ChangeDetectorRef
 } from '@angular/core';
 import { TuiDialogService } from '@taiga-ui/core';
 import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus';
 import { TokenAmount } from '../../../../shared/models/tokens/TokenAmount';
 import { CustomTokenWarningModalComponent } from '../custom-token-warning-modal/custom-token-warning-modal.component';
 import { AvailableTokenAmount } from '../../../../shared/models/tokens/AvailableTokenAmount';
+import { BLOCKCHAIN_NAME } from '../../../../shared/models/blockchain/BLOCKCHAIN_NAME';
 
 @Component({
   selector: 'app-custom-token',
@@ -20,6 +22,8 @@ import { AvailableTokenAmount } from '../../../../shared/models/tokens/Available
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomTokenComponent {
+  public hintsShown: boolean;
+
   @Input() token: AvailableTokenAmount;
 
   @Output() tokenSelected = new EventEmitter<AvailableTokenAmount>();
@@ -42,5 +46,9 @@ export class CustomTokenComponent {
           this.tokenSelected.emit(this.token);
         }
       });
+  }
+
+  public toggleHint(enable: boolean) {
+    this.hintsShown = enable;
   }
 }
--- src/app/features/tokens-select/services/tokens-select.service.ts
+++ src/app/features/tokens-select/services/tokens-select.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { TokenAmount } from 'src/app/shared/models/tokens/TokenAmount';
 import { TokensSelectComponent } from '../components/tokens-select/tokens-select.component';
 import { AvailableTokenAmount } from '../../../shared/models/tokens/AvailableTokenAmount';
+import { BLOCKCHAIN_NAME } from '../../../shared/models/blockchain/BLOCKCHAIN_NAME';
 
 @Injectable()
 export class TokensSelectService {
@@ -13,13 +14,17 @@ export class TokensSelectService {
     @Inject(Injector) private injector: Injector
   ) {}
 
-  showDialog(tokens: Observable<AvailableTokenAmount[]>): Observable<TokenAmount> {
+  showDialog(
+    tokens: Observable<AvailableTokenAmount[]>,
+    enabledCustomTokenBlockchain: BLOCKCHAIN_NAME
+  ): Observable<TokenAmount> {
     return this.dialogService.open(
       new PolymorpheusComponent(TokensSelectComponent, this.injector),
       {
         size: 's',
         data: {
-          tokens
+          tokens,
+          enabledCustomTokenBlockchain
         }
       }
     );
