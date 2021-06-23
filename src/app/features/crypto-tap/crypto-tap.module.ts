c749f05b295a80ab8e9f06432d8775be2b095f22
siandrev <andreev.sergey.i@yandex.ru>
2021-06-23 17:16:59 +0300

    Crypto-tap route added

---
+++ src/app/features/crypto-tap/crypto-tap.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CryptoTapFormComponent } from './components/crypto-tap-form/crypto-tap-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    CryptoTapFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CryptoTapModule { }
