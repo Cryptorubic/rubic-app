79e3448785934158cd2a79b2cce11f4d41913f2f
ottebrut <ott.andrey.ao@gmail.com>
2021-07-27 19:15:09 +0300

    Auto slippage is fixed

--- src/app/features/swaps/components/settings-it/settings-it.component.ts
+++ src/app/features/swaps/components/settings-it/settings-it.component.ts
@@ -12,7 +12,11 @@ import { AbstractControl, FormControl, FormGroup } from '@ngneat/reactive-forms'
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SettingsItComponent implements OnInit {
+  private defaultSlippageTolerance = 1;
+
   public instantTradeForm: FormGroup<ItSettingsForm>;
+
+  public slippageTolerance: number;
 
   constructor(private readonly settingsService: SettingsService) {}
 
@@ -30,6 +34,7 @@ export class SettingsItComponent implements OnInit {
       rubicOptimisation: new FormControl<boolean>(form.value.rubicOptimisation),
       autoRefresh: new FormControl<boolean>(form.value.autoRefresh)
     });
+    this.slippageTolerance = form.value.slippageTolerance;
     this.setFormChanges(form);
   }
 
@@ -39,12 +44,29 @@ export class SettingsItComponent implements OnInit {
     });
     form.valueChanges.subscribe(settings => {
       this.instantTradeForm.patchValue({ ...settings }, { emitEvent: false });
+      this.slippageTolerance = settings.slippageTolerance;
     });
   }
 
   public setAutoSlippageTolerance(value: boolean): void {
+    if (value) {
+      this.slippageTolerance = this.defaultSlippageTolerance;
+      this.instantTradeForm.patchValue({
+        autoSlippageTolerance: true,
+        slippageTolerance: this.slippageTolerance
+      });
+    } else {
+      this.instantTradeForm.patchValue({
+        autoSlippageTolerance: false
+      });
+    }
+  }
+
+  public onSlippageToleranceChange(slippageTolerance: number): void {
+    this.slippageTolerance = slippageTolerance;
     this.instantTradeForm.patchValue({
-      autoSlippageTolerance: value
+      autoSlippageTolerance: false,
+      slippageTolerance: this.slippageTolerance
     });
   }
 }
