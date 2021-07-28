11a559539c3a14bece48269ce8d68300c61585f3
axtezy <axtezy@gmail.com>
2021-07-28 12:59:24 +0300

    Css clean up

---
+++ src/app/features/swaps/components/swaps-header/swaps-header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SwapsHeaderComponent } from './swaps-header.component';
+
+describe('SwapsHeaderComponent', () => {
+  let component: SwapsHeaderComponent;
+  let fixture: ComponentFixture<SwapsHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SwapsHeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SwapsHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
