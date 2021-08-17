2db1d6f9adc370d96eff3cc2beb0e710996a3f27
axtezy <axtezy@gmail.com>
2021-08-17 12:32:51 +0300

    About images fix

--- src/app/features/features-page-old/features-page.module.ts
+++ src/app/features/features-page-old/features-page.module.ts
@@ -4,9 +4,10 @@ import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutPageComponent } from './components/about/about.component';
 import { FeaturePageRoutingModule } from './features-page-routing.module';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 @NgModule({
   declarations: [AboutPageComponent],
-  imports: [CommonModule, TranslateModule, SharedModule, FeaturePageRoutingModule]
+  imports: [CommonModule, TranslateModule, SharedModule, FeaturePageRoutingModule, InlineSVGModule]
 })
 export class FeaturesPageModule {}
