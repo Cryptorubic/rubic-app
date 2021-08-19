435403fdc0da10905e363b453741b1034ecff754
siandrev <andreev.sergey.i@yandex.ru>
2021-08-19 18:51:23 +0300

    Iframe bridge settings added

--- src/app/features/swaps/components/iframe-settings/iframe-settings.component.ts
+++ src/app/features/swaps/components/iframe-settings/iframe-settings.component.ts
@@ -1,9 +1,21 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup } from '@ngneat/reactive-forms';
 import {
+  BridgeSettingsForm,
   ItSettingsForm,
   SettingsService
 } from 'src/app/features/swaps/services/settings-service/settings.service';
+import { Subscription } from 'rxjs';
+
+export interface IframeSettingsForm {
+  autoSlippageTolerance: boolean;
+  slippageTolerance: number;
+  disableMultihops: boolean;
+  rubicOptimisation: boolean;
+  autoRefresh: boolean;
+
+  tronAddress: string;
+}
 
 @Component({
   selector: 'app-iframe-settings',
@@ -11,52 +23,76 @@ import {
   styleUrls: ['./iframe-settings.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class IframeSettingsComponent implements OnInit {
+export class IframeSettingsComponent implements OnInit, OnDestroy {
   private defaultSlippageTolerance = 1;
 
-  public instantTradeForm: FormGroup<ItSettingsForm>;
+  public iframeSettingsForm: FormGroup<IframeSettingsForm>;
 
   public slippageTolerance: number;
 
+  private $iframeFormSubscription: Subscription;
+
+  private $itFormSubscription: Subscription;
+
+  private $bridgeFormSubscription: Subscription;
+
   constructor(private readonly settingsService: SettingsService) {}
 
-  public ngOnInit(): void {
+  ngOnInit(): void {
     this.setForm();
   }
 
-  private setForm(): void {
-    const form = this.settingsService.settingsForm.controls.INSTANT_TRADE;
-    this.instantTradeForm = new FormGroup<ItSettingsForm>({
-      autoSlippageTolerance: new FormControl<boolean>(form.value.autoSlippageTolerance),
-      slippageTolerance: new FormControl<number>(form.value.slippageTolerance),
-      deadline: new FormControl<number>(form.value.deadline),
-      disableMultihops: new FormControl<boolean>(form.value.disableMultihops),
-      rubicOptimisation: new FormControl<boolean>(form.value.rubicOptimisation),
-      autoRefresh: new FormControl<boolean>(form.value.autoRefresh)
-    });
-    this.slippageTolerance = form.value.slippageTolerance;
-    this.setFormChanges(form);
+  ngOnDestroy(): void {
+    this.$iframeFormSubscription.unsubscribe();
+    this.$itFormSubscription.unsubscribe();
+    this.$itFormSubscription.unsubscribe();
   }
 
-  private setFormChanges(form: AbstractControl<ItSettingsForm>): void {
-    this.instantTradeForm.valueChanges.subscribe(settings => {
-      form.patchValue({ ...settings });
+  private setForm(): void {
+    const itSettingsForm = this.settingsService.settingsForm.controls.INSTANT_TRADE;
+    const bridgeSettingsForm = this.settingsService.settingsForm.controls.BRIDGE;
+
+    this.iframeSettingsForm = new FormGroup<IframeSettingsForm>({
+      autoSlippageTolerance: new FormControl<boolean>(itSettingsForm.value.autoSlippageTolerance),
+      slippageTolerance: new FormControl<number>(itSettingsForm.value.slippageTolerance),
+      disableMultihops: new FormControl<boolean>(itSettingsForm.value.disableMultihops),
+      rubicOptimisation: new FormControl<boolean>(itSettingsForm.value.rubicOptimisation),
+      autoRefresh: new FormControl<boolean>(itSettingsForm.value.autoRefresh),
+      tronAddress: new FormControl<string>(bridgeSettingsForm.value.tronAddress)
     });
-    form.valueChanges.subscribe(settings => {
-      this.instantTradeForm.patchValue({ ...settings }, { emitEvent: false });
+    this.slippageTolerance = itSettingsForm.value.slippageTolerance;
+    this.setFormChanges(itSettingsForm, bridgeSettingsForm);
+  }
+
+  private setFormChanges(
+    itSettingsForm: AbstractControl<ItSettingsForm>,
+    bridgeSettingsForm: AbstractControl<BridgeSettingsForm>
+  ): void {
+    this.$iframeFormSubscription = this.iframeSettingsForm.valueChanges.subscribe(settings => {
+      const { tronAddress, ...itSettings } = settings;
+      itSettingsForm.patchValue({ ...itSettings });
+      bridgeSettingsForm.patchValue({ tronAddress });
+    });
+
+    this.$itFormSubscription = itSettingsForm.valueChanges.subscribe(settings => {
+      this.iframeSettingsForm.patchValue({ ...settings }, { emitEvent: false });
       this.slippageTolerance = settings.slippageTolerance;
+    });
+
+    this.$bridgeFormSubscription = bridgeSettingsForm.valueChanges.subscribe(settings => {
+      this.iframeSettingsForm.patchValue({ ...settings }, { emitEvent: false });
     });
   }
 
   public setAutoSlippageTolerance(value: boolean): void {
     if (value) {
       this.slippageTolerance = this.defaultSlippageTolerance;
-      this.instantTradeForm.patchValue({
+      this.iframeSettingsForm.patchValue({
         autoSlippageTolerance: true,
         slippageTolerance: this.slippageTolerance
       });
     } else {
-      this.instantTradeForm.patchValue({
+      this.iframeSettingsForm.patchValue({
         autoSlippageTolerance: false
       });
     }
@@ -64,7 +100,7 @@ export class IframeSettingsComponent implements OnInit {
 
   public onSlippageToleranceChange(slippageTolerance: number): void {
     this.slippageTolerance = slippageTolerance;
-    this.instantTradeForm.patchValue({
+    this.iframeSettingsForm.patchValue({
       autoSlippageTolerance: false,
       slippageTolerance: this.slippageTolerance
     });
