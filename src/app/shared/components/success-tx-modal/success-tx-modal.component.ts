117ec87e30a2ad0f9946aad4dda9bdb1567860de
siandrev <andreev.sergey.i@yandex.ru>
2021-08-31 15:43:19 +0300

    minor

--- src/app/shared/components/success-tx-modal/success-tx-modal.component.ts
+++ src/app/shared/components/success-tx-modal/success-tx-modal.component.ts
@@ -1,10 +1,6 @@
-import { Component, ChangeDetectionStrategy, Inject, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { TuiDialogContext } from '@taiga-ui/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
-import { Subscription, timer } from 'rxjs';
-import { TuiDestroyService } from '@taiga-ui/cdk';
-import { first, takeUntil } from 'rxjs/operators';
-import { modalConfig } from 'src/app/shared/constants/modals/modal-config';
 
 @Component({
   selector: 'app-success-tx-modal',
