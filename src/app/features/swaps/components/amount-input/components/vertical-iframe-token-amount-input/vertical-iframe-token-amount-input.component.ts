35aedb8bbdba10bbfffcc7f35d0f469e1a387749
vlad3k <vlad3k@bk.ru>
2021-09-09 18:06:45 +0300

    fix max button for widget

--- src/app/features/swaps/components/amount-input/components/vertical-iframe-token-amount-input/vertical-iframe-token-amount-input.component.ts
+++ src/app/features/swaps/components/amount-input/components/vertical-iframe-token-amount-input/vertical-iframe-token-amount-input.component.ts
@@ -15,6 +15,10 @@ import { SettingsService } from 'src/app/features/swaps/services/settings-servic
 import { TuiDestroyService } from '@taiga-ui/cdk';
 import { TokenAmount } from 'src/app/shared/models/tokens/TokenAmount';
 import { FormControl } from '@ngneat/reactive-forms';
+import { NATIVE_TOKEN_ADDRESS } from 'src/app/shared/constants/blockchain/NATIVE_TOKEN_ADDRESS';
+import networks from 'src/app/shared/constants/blockchain/networks';
+import { NotificationsService } from 'src/app/core/services/notifications/notifications.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-vertical-iframe-token-amount-input',
@@ -28,6 +32,10 @@ export class VerticalIframeTokenAmountInputComponent implements OnInit {
   @Input() tokens: AvailableTokenAmount[];
 
   @Input() placeholder = '0.0';
+
+  @Input() maxGasFee: BigNumber = new BigNumber(0);
+
+  @Input() displayMaxButton: boolean;
 
   private get formattedAmount(): string {
     return this.amount.value.split(',').join('');
@@ -50,7 +58,9 @@ export class VerticalIframeTokenAmountInputComponent implements OnInit {
     private readonly swapsService: SwapsService,
     public readonly swapFormService: SwapFormService,
     private readonly settingsService: SettingsService,
-    private readonly destroy$: TuiDestroyService
+    private readonly destroy$: TuiDestroyService,
+    private readonly notificationsService: NotificationsService,
+    private readonly translateService: TranslateService
   ) {}
 
   ngOnInit() {
@@ -107,10 +117,31 @@ export class VerticalIframeTokenAmountInputComponent implements OnInit {
   public onUserBalanceMaxButtonClick(): void {
     const { swapMode } = this.swapsService;
     if (swapMode !== SWAP_PROVIDER_TYPE.CROSS_CHAIN_ROUTING) {
-      this.amount.setValue(this.selectedToken.amount.toFixed());
+      if (NATIVE_TOKEN_ADDRESS === this.selectedToken.address) {
+        this.amount.setValue(this.getMaxAmount().toFixed());
+      } else {
+        this.amount.setValue(this.selectedToken.amount.toFixed());
+      }
     } else {
       this.amount.setValue(this.getMaxAmountInCrossChainRouting());
     }
+  }
+
+  private getMaxAmount(): BigNumber {
+    if (!this.selectedToken?.amount || this.selectedToken.amount.isNaN()) {
+      return null;
+    }
+
+    if (this.selectedToken.amount.lt(this.maxGasFee)) {
+      const nativeTokenSymbol = networks.find(el => el.name === this.selectedToken.blockchain)
+        .nativeCoin.symbol;
+      this.notificationsService.show(
+        this.translateService.instant('notifications.minerFee', { nativeTokenSymbol }),
+        { autoClose: 7000 }
+      );
+    }
+
+    return this.selectedToken.amount.minus(this.maxGasFee);
   }
 
   public onAmountChange(amount: string): void {
