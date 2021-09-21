1d7f140b8d120618c6b4eea1e6d28eeb89095cbc
vlad3k <vlad3k@bk.ru>
2021-09-21 12:31:33 +0300

    counter

--- src/app/core/services/counter-notifications/counter-notifications.service.ts
+++ src/app/core/services/counter-notifications/counter-notifications.service.ts
@@ -5,7 +5,7 @@ import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { MyTradesService } from 'src/app/features/my-trades/services/my-trades.service';
 import { TRANSACTION_STATUS } from 'src/app/shared/models/blockchain/TRANSACTION_STATUS';
 import { StoreService } from 'src/app/core/services/store/store.service';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -29,15 +29,21 @@ export class CounterNotificationsService {
     private readonly myTradesService: MyTradesService,
     private readonly storeService: StoreService
   ) {
+    console.log('counter');
     this.myTradesService.tableTrades$
       .pipe(
+        tap(trades => {
+          console.log(trades);
+        }),
         filter(trades => !!trades),
         map(
           trades =>
             trades.filter(trade => trade.status === TRANSACTION_STATUS.WAITING_FOR_RECEIVING).length
         )
       )
-      .subscribe(this._unreadReceived$);
+      .subscribe(v => {
+        this._unreadReceived$.next(v);
+      });
 
     this.authService
       .getCurrentUser()
