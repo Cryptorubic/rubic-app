05b0761ff3b82900b1d4bd07a3740aa603dff4ab
axtezy <axtezy@gmail.com>
2021-09-27 11:55:05 +0300

    Add USDC in Polygon to path constructor

--- src/app/features/instant-trade/services/instant-trade-service/providers/polygon/quick-swap-service/quick-swap-constants.ts
+++ src/app/features/instant-trade/services/instant-trade-service/providers/polygon/quick-swap-service/quick-swap-constants.ts
@@ -21,6 +21,7 @@ const routingProvidersNetMode: RoutingProvidersNetMode = {
     '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', // WETH
     '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI
     '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', // USDT
+    '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // USDC
     '0x831753DD7087CaC61aB5644b308642cc1c33Dc13' // QUICK
   ],
   testnet: [
