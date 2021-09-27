c24b10752de9345d95e6a713d918c639ede0f068
axtezy <axtezy@gmail.com>
2021-09-27 12:30:18 +0300

    Fix tokens modal bug after selecting token via query address

--- src/app/core/services/tokens/tokens.service.ts
+++ src/app/core/services/tokens/tokens.service.ts
@@ -72,7 +72,7 @@ export class TokensService {
    * Get current tokens list.
    */
   get favoriteTokens$(): Observable<LocalToken[]> {
-    return this.favoriteTokensSubject.asObservable();
+    return this.favoriteTokensSubject.asObservable().pipe(map(el => el || []));
   }
 
   /**
