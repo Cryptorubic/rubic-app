2ef51cc7b74c619ffb0cc3a11c61b54ef07a4271
vlad3k <vlad3k@bk.ru>
2021-09-27 17:20:04 +0300

    minor fixes

--- src/app/features/instant-trade/services/instant-trade-service/providers/avalanche/pangolin-avalanche-service/pangolin-avalanche.service.ts
+++ src/app/features/instant-trade/services/instant-trade-service/providers/avalanche/pangolin-avalanche-service/pangolin-avalanche.service.ts
@@ -4,7 +4,7 @@ import {
   pangolinAvalancheConstants
 } from 'src/app/features/instant-trade/services/instant-trade-service/providers/avalanche/pangolin-avalanche-service/pangolin-avalanche-constants';
 import { CommonUniswapV2Service } from 'src/app/features/instant-trade/services/instant-trade-service/providers/common/uniswap-v2/common-service/common-uniswap-v2.service';
-import AVAX_CONTRACT_ABI from 'src/app/features/instant-trade/services/instant-trade-service/providers/avalanche/models/avaxContractAbi';
+import AVAX_CONTRACT_ABI from 'src/app/features/instant-trade/services/instant-trade-service/providers/avalanche/models/avax-contract-abi';
 
 @Injectable({
   providedIn: 'root'
