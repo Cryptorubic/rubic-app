c274131bb1fa5a2cda0692ae3664f7193120615a
axtezy <axtezy@gmail.com>
2021-09-28 13:31:55 +0300

    Fix notification position

--- src/app/core/header/components/header/header.component.ts
+++ src/app/core/header/components/header/header.component.ts
@@ -96,6 +96,7 @@ export class HeaderComponent implements AfterViewInit {
       this.window.onscroll = () => {
         const scrolled = this.window.pageYOffset || this.document.documentElement.scrollTop;
         this.pageScrolled = scrolled > scrolledHeight;
+        this.setupNotificationPosition();
       };
     }
     this.countNotifications$ = this.counterNotificationsService.unread$;
@@ -107,6 +108,15 @@ export class HeaderComponent implements AfterViewInit {
       .getCurrentUser()
       .pipe(takeUntil(this.destroy$))
       .subscribe(() => this.cdr.detectChanges());
+  }
+
+  /**
+   * Setups notification position based on window scroll and width.
+   */
+  private setupNotificationPosition(): void {
+    const offset = 90;
+    const pixelOffset = `${this.window.scrollY < offset ? offset : 0}px`;
+    this.window.document.documentElement.style.setProperty('--scroll-size', pixelOffset);
   }
 
   private async loadUser(): Promise<void> {
