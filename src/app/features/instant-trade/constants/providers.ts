0844fe1aa6a5c36aa54ee1586e0250b3d38ccb87
vlad3k <vlad3k@bk.ru>
2021-10-04 16:55:42 +0300

    comment sushiswap

--- src/app/features/instant-trade/constants/providers.ts
+++ src/app/features/instant-trade/constants/providers.ts
@@ -109,13 +109,13 @@ export const INSTANT_TRADE_PROVIDERS: Partial<
     }
   ],
   [BLOCKCHAIN_NAME.MOONRIVER]: [
-    {
-      ...defaultState,
-      tradeProviderInfo: {
-        label: 'Sushiswap',
-        value: INSTANT_TRADES_PROVIDER.SUSHISWAP
-      }
-    },
+    // {
+    //   ...defaultState,
+    //   tradeProviderInfo: {
+    //     label: 'Sushiswap',
+    //     value: INSTANT_TRADES_PROVIDER.SUSHISWAP
+    //   }
+    // },
     {
       ...defaultState,
       tradeProviderInfo: {
