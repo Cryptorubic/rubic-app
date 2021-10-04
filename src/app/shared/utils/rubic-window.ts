dbffb6b402dae4cf675505af70861acd2ef73f11
vlad3k <vlad3k@bk.ru>
2021-10-04 17:23:28 +0300

    minor fix

--- src/app/shared/utils/rubic-window.ts
+++ src/app/shared/utils/rubic-window.ts
@@ -3,7 +3,6 @@ import Process = NodeJS.Process; // Included with Angular CLI.
 /**
  * APPLICATION IMPORTS
  */
-/* eslint-disable  @typescript-eslint/no-explicit-any */
 export interface RubicWindow extends Window {
   global?: unknown;
   process?: Process;
