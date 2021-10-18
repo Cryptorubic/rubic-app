2c1f3fabcc2a81b9cde18bd6c7afc2b023220249
ottebrut <ott.andrey.ao@gmail.com>
2021-10-18 14:06:18 +0300

    Crypto tap is deleted

--- src/app/core/services/backend/constants/BOT_URL.ts
+++ src/app/core/services/backend/constants/BOT_URL.ts
@@ -2,6 +2,5 @@ export const BOT_URL = {
   BRIDGES: 'bot/bridge_swap',
   INSTANT_TRADES: 'bot/instant_trade',
   ORDER_BOOKS_CREATE: 'bot/order_book_create',
-  ORDER_BOOKS_CONTRIBUTE: 'bot/order_book_contribute',
-  CRYPTO_TAP: 'bot/crypto_tap'
+  ORDER_BOOKS_CONTRIBUTE: 'bot/order_book_contribute'
 };
--- src/app/features/crypto-tap/models/CryptoTapTrade.ts
+++
@@ -1,7 +0,0 @@
-import BigNumber from 'bignumber.js';
-
-export interface CryptoTapTrade {
-  fromAmount: BigNumber;
-  toAmount: BigNumber;
-  fee: BigNumber;
-}
