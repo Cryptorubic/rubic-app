2b3dbe27b8fcdd944c48a3e31bec6227576ee48c
ottebrut <ott.andrey.ao@gmail.com>
2021-10-18 13:23:42 +0300

    contract addresses are updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   crossChain: {
     apiBaseUrl: 'https://dev-crosschain.rubic.exchange/api',
     contractAddresses: {
-      [BLOCKCHAIN_NAME.ETHEREUM]: undefined as string,
+      [BLOCKCHAIN_NAME.ETHEREUM]: '0xb9a94be803eC1197A234406eF5c0113f503d3178',
       [BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN]: '0x6b8904739059afbaa91311aab99187f5885c6dc0',
       [BLOCKCHAIN_NAME.POLYGON]: '0xb02c0b6ba0e7719de2d44e613fc4ad01ac2f60ad',
       [BLOCKCHAIN_NAME.AVALANCHE]: '0x3df5f6165fe8429744F9488a9C18259E9a93B4C0'
