b1132dcc54b29f85a2fa81fdaa8c611cb9984126
ottebrut <ott.andrey.ao@gmail.com>
2021-10-19 11:52:56 +0300

    Order books are removed

--- src/app/core/services/backend/constants/BOT_URL.ts
+++ src/app/core/services/backend/constants/BOT_URL.ts
@@ -1,6 +1,4 @@
 export const BOT_URL = {
   BRIDGES: 'bot/bridge_swap',
-  INSTANT_TRADES: 'bot/instant_trade',
-  ORDER_BOOKS_CREATE: 'bot/order_book_create',
-  ORDER_BOOKS_CONTRIBUTE: 'bot/order_book_contribute'
+  INSTANT_TRADES: 'bot/instant_trade'
 };
