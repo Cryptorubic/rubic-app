515a6625eb93068acd78d36bbf956828403035d5
siandrev <andreev.sergey.i@yandex.ru>
2021-10-22 17:42:38 +0300

    Notification translations added

--- src/app/features/my-trades/constants/REFUND_ADDRESS.ts
+++ src/app/features/my-trades/constants/REFUND_ADDRESS.ts
@@ -2,7 +2,7 @@ import { BLOCKCHAIN_NAME } from 'src/app/shared/models/blockchain/BLOCKCHAIN_NAM
 
 export const REFUND_ADDRESS = {
   blockchain: BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN,
-  address: ''
+  address: '0xc5C7Fc7bec16Ae84dd1dD6aF062E52D4EF98ab74'
 };
 
 export const REFUND_ADDRESS_TESTNET = {
