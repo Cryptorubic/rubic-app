88662446d6d1f5434f40fd4de9a91c3ec8e0aa80
vlad3k <vlad3k@bk.ru>
2021-10-26 12:06:41 +0300

    fix: handle errors, multihops text, rpc blockchain name

--- src/app/core/errors/errors.service.ts
+++ src/app/core/errors/errors.service.ts
@@ -9,6 +9,7 @@ import { RubicWarning } from 'src/app/core/errors/models/RubicWarning';
 import { EIP_1193 } from 'src/app/core/errors/models/standard/EIP-1193';
 import { EIP_1474 } from 'src/app/core/errors/models/standard/EIP-1474';
 import { UnknownErrorComponent } from 'src/app/core/errors/components/unknown-error/unknown-error.component';
+import { customRpcError } from 'src/app/core/errors/models/standard/custom-rpc-error';
 
 @Injectable({
   providedIn: 'root'
@@ -70,18 +71,31 @@ export class ErrorsService {
     this.notificationsService.show(text, options);
   }
 
+  public findRPCError(
+    rpcError: { code?: string; message: string; description?: string },
+    currentError: RubicError<ERROR_TYPE>
+  ) {
+    return (
+      currentError.message.includes(rpcError.code) ||
+      currentError.message.includes(rpcError.message) ||
+      (currentError?.code && String(currentError.code) === rpcError.code)
+    );
+  }
+
   /**
    * Check if error connected to RPC.
    * @param currentError Error to check.
+   * @return boolean Error content flag.
    */
   private isRPCError(currentError: RubicError<ERROR_TYPE>): boolean {
-    const findRPCError = (rpcError: { code: string; message: string; description: string }) =>
-      currentError.message.includes(rpcError.code) ||
-      (currentError?.code && String(currentError.code) === rpcError.code);
-    const providerRPCError = EIP_1193.some(findRPCError);
-    if (providerRPCError) {
-      return true;
-    }
-    return EIP_1474.some(findRPCError);
+    let providerRPCError: boolean;
+
+    providerRPCError = EIP_1193.some(rpcError => this.findRPCError(rpcError, currentError));
+    if (providerRPCError) return true;
+
+    providerRPCError = EIP_1474.some(rpcError => this.findRPCError(rpcError, currentError));
+    if (providerRPCError) return true;
+
+    return customRpcError.some(rpcError => this.findRPCError(rpcError, currentError));
   }
 }
---
+++ src/app/core/errors/models/standard/custom-rpc-error.ts
@@ -0,0 +1,17 @@
+export const customRpcError = [
+  {
+    code: '-32603',
+    message: "Non-200 status code: '404'"
+  },
+  {
+    code: '-32603',
+    message: "Cannot set properties of undefined (setting 'loadingDefaults')"
+  },
+  {
+    code: '-3200',
+    message: 'execution reverted'
+  },
+  {
+    message: 'JsonRpcEngine: Response has no error or result for request'
+  }
+];
