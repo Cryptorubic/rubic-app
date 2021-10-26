03df83e473b6382f7a09a770242d48db56d2942d
ottebrut <ott.andrey.ao@gmail.com>
2021-10-26 15:34:40 +0300

    bug fix

--- src/app/shared/components/buttons/swap-button-container/swap-button-container.component.ts
+++ src/app/shared/components/buttons/swap-button-container/swap-button-container.component.ts
@@ -10,13 +10,11 @@ import {
 import { Observable, of } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 import BigNumber from 'bignumber.js';
-import { TokensService } from 'src/app/core/services/tokens/tokens.service';
 import { ISwapFormInput } from 'src/app/shared/models/swaps/ISwapForm';
 import { ProviderConnectorService } from 'src/app/core/services/blockchain/providers/provider-connector-service/provider-connector.service';
 import { UseTestingModeService } from 'src/app/core/services/use-testing-mode/use-testing-mode.service';
 import { TranslateService } from '@ngx-translate/core';
 import { BLOCKCHAIN_NAME } from 'src/app/shared/models/blockchain/BLOCKCHAIN_NAME';
-import { BridgeService } from 'src/app/features/bridge/services/bridge-service/bridge.service';
 import { WALLET_NAME } from 'src/app/core/wallets/components/wallets-modal/models/providers';
 import { Web3PublicService } from 'src/app/core/services/blockchain/web3/web3-public-service/web3-public.service';
 import { WithRoundPipe } from 'src/app/shared/pipes/with-round.pipe';
@@ -203,11 +201,6 @@ export class SwapButtonContainerComponent implements OnInit {
         break;
       }
       default:
-        translateParams = {
-          key: 'Unknown Error',
-          interpolateParams: {}
-        };
-        break;
     }
 
     if (!translateParams) {
@@ -219,12 +212,10 @@ export class SwapButtonContainerComponent implements OnInit {
   constructor(
     private readonly cdr: ChangeDetectorRef,
     private readonly authService: AuthService,
-    private readonly tokensService: TokensService,
     private readonly providerConnectorService: ProviderConnectorService,
     private readonly useTestingModeService: UseTestingModeService,
     private readonly walletsModalService: WalletsModalService,
     private readonly translateService: TranslateService,
-    private readonly bridgeService: BridgeService,
     private readonly web3PublicService: Web3PublicService,
     private readonly withRoundPipe: WithRoundPipe,
     private readonly iframeService: IframeService,
