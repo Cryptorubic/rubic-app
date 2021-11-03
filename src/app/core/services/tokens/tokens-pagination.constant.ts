9d61f1d27cafbbbbd61d96de6584d4da2f5c509a
ottebrut <ott.andrey.ao@gmail.com>
2021-11-03 12:00:00 +0300

    Fixed tokens pagination bug

--- src/app/core/services/tokens/tokens-pagination.constant.ts
+++ src/app/core/services/tokens/tokens-pagination.constant.ts
@@ -7,5 +7,5 @@ export const TOKENS_PAGINATION: TokensNetworkState = {
   [BLOCKCHAIN_NAME.POLYGON]: { page: 2, maxPage: 3 },
   [BLOCKCHAIN_NAME.HARMONY]: { page: 2, maxPage: 2 },
   [BLOCKCHAIN_NAME.AVALANCHE]: { page: 1, maxPage: 1 },
-  [BLOCKCHAIN_NAME.MOONRIVER]: { page: 2, maxPage: 2 }
+  [BLOCKCHAIN_NAME.MOONRIVER]: { page: 1, maxPage: 1 }
 };
