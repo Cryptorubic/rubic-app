a173a12ae48242d232502ae1b707894ff57bf8c6
ottebrut <ott.andrey.ao@gmail.com>
2021-11-12 00:59:20 +0300

    Add fantom ccr

--- src/app/features/cross-chain-routing/services/cross-chain-routing-service/cross-chain-routing.service.ts
+++ src/app/features/cross-chain-routing/services/cross-chain-routing-service/cross-chain-routing.service.ts
@@ -50,6 +50,7 @@ import { TokenAmount } from '@shared/models/tokens/TokenAmount';
 import { SolarBeamMoonRiverService } from '@features/instant-trade/services/instant-trade-service/providers/moonriver/solarbeam-moonriver/solarbeam-moonriver.service';
 import { CcrTradeInfo } from '@features/cross-chain-routing/services/cross-chain-routing-service/models/CcrTradeInfo';
 import { PriceImpactService } from '@core/services/price-impact/price-impact.service';
+import { SpookySwapFantomService } from '@features/instant-trade/services/instant-trade-service/providers/fantom/spooky-swap-fantom-service/spooky-swap-fantom.service';
 
 interface PathAndToAmount {
   path: string[];
@@ -94,6 +95,7 @@ export class CrossChainRoutingService {
     private readonly pangolinAvalancheService: PangolinAvalancheService,
     private readonly joeAvalancheService: JoeAvalancheService,
     private readonly solarBeamMoonRiverService: SolarBeamMoonRiverService,
+    private readonly spookySwapFantomService: SpookySwapFantomService,
     private readonly providerConnectorService: ProviderConnectorService,
     private readonly authService: AuthService,
     private readonly settingsService: SettingsService,
@@ -125,7 +127,8 @@ export class CrossChainRoutingService {
       [BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN]: [this.pancakeSwapService],
       [BLOCKCHAIN_NAME.POLYGON]: [this.quickSwapService],
       [BLOCKCHAIN_NAME.AVALANCHE]: [this.pangolinAvalancheService, this.joeAvalancheService],
-      [BLOCKCHAIN_NAME.MOONRIVER]: [this.solarBeamMoonRiverService]
+      [BLOCKCHAIN_NAME.MOONRIVER]: [this.solarBeamMoonRiverService],
+      [BLOCKCHAIN_NAME.FANTOM]: [this.spookySwapFantomService]
     };
   }
 
@@ -135,7 +138,8 @@ export class CrossChainRoutingService {
       [BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN]: [1],
       [BLOCKCHAIN_NAME.POLYGON]: [3],
       [BLOCKCHAIN_NAME.AVALANCHE]: [4, 5],
-      [BLOCKCHAIN_NAME.MOONRIVER]: [6]
+      [BLOCKCHAIN_NAME.MOONRIVER]: [6],
+      [BLOCKCHAIN_NAME.FANTOM]: [7]
     };
   }
 
