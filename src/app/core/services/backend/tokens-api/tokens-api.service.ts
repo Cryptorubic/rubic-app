51efb8c53cb7ff1f8e02b2c2c3cacf2c4c2d0e46
ottebrut <ott.andrey.ao@gmail.com>
2021-11-17 12:52:36 +0300

    minor fix

--- src/app/core/services/backend/tokens-api/tokens-api.service.ts
+++ src/app/core/services/backend/tokens-api/tokens-api.service.ts
@@ -105,7 +105,7 @@ export class TokensApiService {
     );
     return forkJoin(requests$).pipe(
       map(results => {
-        return TokensApiService.prepareTokens(results.flatMap(el => el.results));
+        return TokensApiService.prepareTokens(results.flatMap(el => el.results || []));
       })
     );
   }
