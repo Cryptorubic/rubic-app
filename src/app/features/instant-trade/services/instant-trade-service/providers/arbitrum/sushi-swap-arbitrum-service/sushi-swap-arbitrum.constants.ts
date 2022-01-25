3a921a95dd8032aff7561795c459f6551f62947b
vlad3k <vlad3k@bk.ru>
2022-01-25 16:08:42 +0300

    add cross-chain for arbitrum with empty addresses

--- src/app/features/instant-trade/services/instant-trade-service/providers/arbitrum/sushi-swap-arbitrum-service/sushi-swap-arbitrum.constants.ts
+++ src/app/features/instant-trade/services/instant-trade-service/providers/arbitrum/sushi-swap-arbitrum-service/sushi-swap-arbitrum.constants.ts
@@ -17,10 +17,12 @@ const wethAddressNetMode: ContractAddressNetMode = {
 
 const routingProvidersNetMode: RoutingProvidersNetMode = {
   mainnet: [
-    { address: '0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a', symbol: 'GMX' },
+    { address: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', symbol: 'WETH' },
+    { address: '0xfea7a6a0b346362bf88a9e4a88416b77a57d6c2a', symbol: 'MIM' },
+    { address: '0x6c2c06790b3e3e3c38e12ee22f8183b37a13ee55', symbol: 'DPX' },
     { address: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8', symbol: 'USDC' },
-    { address: '0xda10009cbd5d07dd0cecc66161fc93d7c9000da1', symbol: 'DAI' },
-    { address: '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f', symbol: 'WBTC' }
+    { address: '0x32eb7902d4134bf98a28b963d26de779af92a212', symbol: 'RDPX' },
+    { address: '0x539bde0d7dbd336b79148aa742883198bbf60342', symbol: 'MAGIC' }
   ],
   testnet: [null]
 };
