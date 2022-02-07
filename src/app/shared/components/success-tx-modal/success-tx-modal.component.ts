fedfe61bd4375d815b75074171fa2ce6223da1bb
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-02-07 13:43:24 +0300

    fix from PR

--- src/app/shared/components/success-tx-modal/success-tx-modal.component.ts
+++ src/app/shared/components/success-tx-modal/success-tx-modal.component.ts
@@ -8,7 +8,6 @@ import { MODAL_CONFIG } from 'src/app/shared/constants/modals/modal-config';
 import { takeUntil } from 'rxjs/operators';
 import { SuccessTxModalType } from 'src/app/shared/components/success-trx-notification/models/modal-type';
 import { BLOCKCHAIN_NAME } from '@shared/models/blockchain/blockchain-name';
-import { ScannerLinkPipe } from '@shared/pipes/scanner-link.pipe';
 
 @Component({
   selector: 'polymorpheus-success-tx-modal',
@@ -26,9 +25,7 @@ export class SuccessTxModalComponent {
 
   public readonly ADDRESS_TYPE = ADDRESS_TYPE;
 
-  public blockchain: BLOCKCHAIN_NAME;
-
-  private readonly scannerLinkPipe: ScannerLinkPipe;
+  public readonly blockchain: BLOCKCHAIN_NAME;
 
   constructor(
     private readonly destroy$: TuiDestroyService,
