e6371bd424580f0a75db26baacd30b105692119a
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-02-09 15:49:17 +0300

    add referral banner

---
+++ src/app/core/header/components/header/components/referral-banner/app-referral-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-referral-banner',
+  templateUrl: './app-referral-banner.component.html',
+  styleUrls: ['./app-referral-banner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppReferralBannerComponent {
+  constructor() {}
+}
