687213753daec1007b44c7f599b01d18d45f0e67
kolebayevv <kolebayevv@gmail.com>
2022-02-09 16:17:22 +0300

    rename page component

--- src/app/features/lp-providing/lp-providing.module.ts
+++ src/app/features/lp-providing/lp-providing.module.ts
@@ -5,10 +5,10 @@ import { LpProvidingService } from './services/lp-providing.service';
 import { LpProvidingApiService } from './services/lp-providing-api.service';
 import { StakeButtonComponent } from './components/stake-button/stake-button.component';
 import { DepositComponent } from './components/deposit/deposit.component';
-import { LpProvidingPage } from './components/lp-providing-page/lp-providing-page.component';
+import { LpProvidingPageComponent } from './components/lp-providing-page/lp-providing-page.component';
 
 @NgModule({
-  declarations: [StakeButtonComponent, DepositComponent, LpProvidingPage],
+  declarations: [StakeButtonComponent, DepositComponent, LpProvidingPageComponent],
   imports: [CommonModule, LpProvidingRoutingModule],
   exports: [],
   providers: [LpProvidingService, LpProvidingApiService]
