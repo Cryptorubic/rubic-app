2547f7d2983507f02dc7ff27fead270fc951564b
axtezy <axtezy@gmail.com>
2022-03-25 16:27:28 +0300

    Replace near address by wrap.near in ccr

--- src/app/features/cross-chain-routing/services/cross-chain-routing-service/contracts-data/contract-data/near-contract-data.ts
+++ src/app/features/cross-chain-routing/services/cross-chain-routing-service/contracts-data/contract-data/near-contract-data.ts
@@ -16,6 +16,8 @@ import { EthLikeWeb3Public } from '@core/services/blockchain/blockchain-adapters
 import { EMPTY_ADDRESS } from '@shared/constants/blockchain/empty-address';
 import BigNumber from 'bignumber.js';
 import { BlockchainNumber } from '@features/cross-chain-routing/services/cross-chain-routing-service/contracts-data/contract-data/models/blockchain-number';
+import { NATIVE_NEAR_ADDRESS } from '@shared/constants/blockchain/native-token-address';
+import { WRAP_NEAR_CONTRACT } from '@features/instant-trade/services/instant-trade-service/providers/near/ref-finance-service/constants/ref-fi-constants';
 
 type NearCrossChainContract = Contract & NearCcrViewMethods;
 
@@ -35,13 +37,11 @@ export class NearContractData extends ContractData {
    */
   private static transformLargeAddresses(addresses: string[]): string[][] {
     return addresses.map((tokenAddress, index) => {
-      if (tokenAddress.length > 40) {
-        return [
-          `${index + 1}!${tokenAddress.slice(0, 40)}`,
-          `${index + 1}!${tokenAddress.slice(41)}`
-        ];
+      const address = tokenAddress === NATIVE_NEAR_ADDRESS ? WRAP_NEAR_CONTRACT : tokenAddress;
+      if (address.length > 40) {
+        return [`${index + 1}!${address.slice(0, 40)}`, `${index + 1}!${address.slice(41)}`];
       }
-      return [`${index + 1}!${tokenAddress}`];
+      return [`${index + 1}!${address}`];
     });
   }
 
