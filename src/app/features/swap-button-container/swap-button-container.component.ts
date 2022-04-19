c6068a71cbcfcf5b9d0abefe87ee67226697c96c
ottebrut <ott.andrey.ao@gmail.com>
2022-04-19 16:16:43 +0300

    Remove unnecessary inputs and subscriptions from swap-button-container

--- src/app/features/swap-button-container/swap-button-container.component.ts
+++ src/app/features/swap-button-container/swap-button-container.component.ts
@@ -1,25 +1,14 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  Self
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { AuthService } from '@core/services/auth/auth.service';
 import BigNumber from 'bignumber.js';
 import { WalletConnectorService } from '@core/services/blockchain/wallets/wallet-connector-service/wallet-connector.service';
 import { WALLET_NAME } from '@core/wallets/components/wallets-modal/models/wallet-name';
-import { TuiDestroyService } from '@taiga-ui/cdk';
-import { startWith, takeUntil } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 import { HeaderStore } from '@core/header/services/header.store';
 import { SwapFormService } from '@features/swaps/services/swaps-form-service/swap-form.service';
 import { TRADE_STATUS } from '@shared/models/swaps/trade-status';
 import { SwapButtonContainerErrorsService } from '@features/swap-button-container/services/swap-button-container-errors.service';
 import { ERROR_TYPE } from '@features/swap-button-container/models/error-type';
-import { BlockchainName } from '@shared/models/blockchain/blockchain-name';
 import { SwapButtonContainerService } from '@features/swap-button-container/services/swap-button-container.service';
 import { SwapButtonService } from '@features/swap-button-container/services/swap-button.service';
 
@@ -27,10 +16,9 @@ import { SwapButtonService } from '@features/swap-button-container/services/swap
   selector: 'app-swap-button-container',
   templateUrl: './swap-button-container.component.html',
   styleUrls: ['./swap-button-container.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [TuiDestroyService]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SwapButtonContainerComponent implements OnInit {
+export class SwapButtonContainerComponent {
   @Input() needApprove = false;
 
   @Input() set status(value: TRADE_STATUS) {
@@ -41,8 +29,6 @@ export class SwapButtonContainerComponent implements OnInit {
   get status(): TRADE_STATUS {
     return this._status;
   }
-
-  @Input() formService: SwapFormService;
 
   @Input() set idPrefix(value: string) {
     this.swapButtonContainerService.idPrefix = value || '';
@@ -64,17 +50,15 @@ export class SwapButtonContainerComponent implements OnInit {
     this.swapButtonService.buttonText = value;
   }
 
-  @Output() approveClick = new EventEmitter<void>();
+  @Output() onApproveClick = new EventEmitter<void>();
 
-  @Output() swapClick = new EventEmitter<void>();
+  @Output() onSwapClick = new EventEmitter<void>();
 
   @Output() onUpdateRateClick = new EventEmitter<void>();
 
   public TRADE_STATUS = TRADE_STATUS;
 
   private _status: TRADE_STATUS;
-
-  public fromBlockchain: BlockchainName;
 
   public readonly isMobile$ = this.headerStore.getMobileDisplayStatus();
 
@@ -84,29 +68,20 @@ export class SwapButtonContainerComponent implements OnInit {
 
   public readonly error$ = this.swapButtonContainerErrorsService.error$;
 
+  public readonly fromBlockchain$ = this.swapFormService.inputValueChanges.pipe(
+    startWith(this.swapFormService.inputValue),
+    map(form => form.fromBlockchain)
+  );
+
   constructor(
-    private readonly cdr: ChangeDetectorRef,
     private readonly swapButtonContainerService: SwapButtonContainerService,
     private readonly swapButtonContainerErrorsService: SwapButtonContainerErrorsService,
     private readonly swapButtonService: SwapButtonService,
     private readonly authService: AuthService,
     private readonly walletConnectorService: WalletConnectorService,
     private readonly headerStore: HeaderStore,
-    @Self() private readonly destroy$: TuiDestroyService
+    private readonly swapFormService: SwapFormService
   ) {}
-
-  ngOnInit(): void {
-    this.setupSubscriptions();
-  }
-
-  private setupSubscriptions(): void {
-    this.formService.inputValueChanges
-      .pipe(startWith(this.formService.inputValue), takeUntil(this.destroy$))
-      .subscribe(form => {
-        this.fromBlockchain = form.fromBlockchain;
-        this.cdr.markForCheck();
-      });
-  }
 
   public allowChangeNetwork(err: ERROR_TYPE): boolean {
     if (err !== ERROR_TYPE.WRONG_BLOCKCHAIN) {
@@ -118,7 +93,8 @@ export class SwapButtonContainerComponent implements OnInit {
   public async changeNetwork(): Promise<void> {
     const currentStatus = this.status;
     this.status = TRADE_STATUS.LOADING;
-    const { fromBlockchain } = this.formService.inputValue;
+
+    const { fromBlockchain } = this.swapFormService.inputValue;
     try {
       await this.walletConnectorService.switchChain(fromBlockchain);
     } finally {
