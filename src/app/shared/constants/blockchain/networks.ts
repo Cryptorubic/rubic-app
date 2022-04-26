055201326da0338111cd988ea7907d8aa5cfb03c
kolebayevv <kolebayevv@gmail.com>
2022-04-26 14:33:49 +0300

    add fantom additional rpc link

--- src/app/shared/constants/blockchain/networks.ts
+++ src/app/shared/constants/blockchain/networks.ts
@@ -132,7 +132,8 @@ const networks: ReadonlyArray<Network> = [
     label: 'Fantom',
     scannerUrl: 'https://ftmscan.com',
     rpcLink: 'https://speedy-nodes-nyc.moralis.io/106bebf40377b2e543f51299/fantom/mainnet',
-    additionalRpcLink: '',
+    additionalRpcLink:
+      'https://ftm.getblock.io/mainnet/?api_key=02530958-c8c4-4297-974c-66203e79800d',
     imagePath: 'assets/images/icons/coins/fantom.svg',
     nativeCoin: {
       blockchain: BLOCKCHAIN_NAME.FANTOM,
