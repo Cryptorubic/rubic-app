cccb1e0eb19451c31fa053cb1b31b386e25dbe88
ottebrut <ott.andrey.ao@gmail.com>
2022-05-13 16:06:57 +0300

    Add token rate component

---
+++ src/app/features/swaps/shared/tokens-rate/tokens-rate.component.ts
@@ -0,0 +1,55 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { SwapFormService } from '@features/swaps/features/main-form/services/swap-form-service/swap-form.service';
+import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import BigNumber from 'bignumber.js';
+
+interface TokenRate {
+  amount: BigNumber;
+  symbol: string;
+}
+
+interface TokensRate {
+  from: TokenRate;
+  to: TokenRate;
+}
+
+@Component({
+  selector: 'app-tokens-rate',
+  templateUrl: './tokens-rate.component.html',
+  styleUrls: ['./tokens-rate.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TokensRateComponent {
+  public tokensRate$: Observable<TokensRate> = this.swapFormService.outputValueChanges.pipe(
+    startWith(this.swapFormService.outputValue),
+    map(outputForm => {
+      const { toAmount } = outputForm;
+
+      if (toAmount?.isFinite()) {
+        const { fromAmount, fromToken, toToken } = this.swapFormService.inputValue;
+
+        return {
+          from: {
+            amount: fromAmount.dividedBy(toAmount),
+            symbol: fromToken.symbol
+          },
+          to: {
+            amount: toAmount.dividedBy(fromAmount),
+            symbol: toToken.symbol
+          }
+        };
+      } else {
+        return null;
+      }
+    })
+  );
+
+  public rateDirection: 'from' | 'to' = 'from';
+
+  constructor(private readonly swapFormService: SwapFormService) {}
+
+  public onChangeDirection(): void {
+    this.rateDirection = this.rateDirection === 'from' ? 'to' : 'from';
+  }
+}
