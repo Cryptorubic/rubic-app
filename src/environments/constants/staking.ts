bb6caed5e39b1ae95349f81eed451518d73177c2
kolebayevv <ko1ebayev.worx@gmail.com>
2022-08-03 15:41:53 +0600

    upd env

--- src/environments/constants/staking.ts
+++ src/environments/constants/staking.ts
@@ -1,7 +1,8 @@
 export const STAKING_CONFIG_DEVELOP = {
-  nftContractAddress: '0x345C19973B58F9a57AB768ce2Dd5f527D5Fb82BE',
-  rewardsContractAddress: '0x9835d4A1399fF4A67876605f9Dc06f6ADcf14606',
-  rbcToken: '0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA' //fake rbc
+  nftContractAddress: '0x3b67942461E2B487701748f63c1d24De7C72591E',
+  rewardsContractAddress: '0x3d9aBCdf76bc969a860175E13Fe4Fc791E836D08',
+  // rbcToken: '0xd452d01C6348D3d5B35FA1d5500d23F8Ae65D6eA' //fake rbc
+  rbcToken: '0x8e3bcc334657560253b83f08331d85267316e08a' // real rbc
 };
 
 export const STAKING_CONFIG_PROD = {
