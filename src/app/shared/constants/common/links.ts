ef5efb35d4083d08f915d4aa32be1e88bdcac74d
kolebayevv <ko1ebayev.worx@gmail.com>
2022-08-04 17:22:53 +0600

    upd link

--- src/app/shared/constants/common/links.ts
+++ src/app/shared/constants/common/links.ts
@@ -12,7 +12,7 @@ export enum ROUTE_PATH {
 
 export enum EXTERNAL_LINKS {
   LANDING = 'https://rubic.exchange/',
-  LANDING_STAKING = 'https://rubic.exchange/staking',
+  LANDING_STAKING = 'https://dev.rubic.exchange/staking',
   LANDING_REFERRAL = 'https://rubic.exchange/referral',
   LANDING_LP = 'https://rubic.exchange/lp',
   LANDING_SETUP_WIDGET = 'https://rubic.exchange/widget',
