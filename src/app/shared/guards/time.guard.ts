c09357e39804bfa180241fed80ae39af14275df8
kolebayevv <ko1ebayev.worx@gmail.com>
2022-08-05 15:42:19 +0600

    update time

--- src/app/shared/guards/time.guard.ts
+++ src/app/shared/guards/time.guard.ts
@@ -5,14 +5,13 @@ import { RubicWindow } from '@shared/utils/rubic-window';
 import { EXTERNAL_LINKS } from '@shared/constants/common/links';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 import { ActivationResult } from '@shared/guards/models/types';
 
 @Injectable()
 export class TimeGuard implements CanActivate {
   private readonly redirectUrl = EXTERNAL_LINKS.LANDING_STAKING;
 
-  private readonly expiredDateUTC = Date.UTC(2022, 7, 5, 9, 40);
+  private readonly expiredDateUTC = Date.UTC(2022, 7, 5, 9, 50);
 
   constructor(
     @Inject(WINDOW) private readonly window: RubicWindow,
@@ -24,21 +23,27 @@ export class TimeGuard implements CanActivate {
   }
 
   private redirectIfExpired(): Observable<Boolean> {
-    return this.httpClient
-      .get<{ fulldate: string }>(
-        'https://script.googleusercontent.com/macros/echo?user_content_key=Vhsl1EP7sG2pMrcSNoyf0eAn7o8_UMnbX13lYZadzbU6HWn8kDJHWNHkepkSmqSvn3Rj0G4LNOIISxLzLVZLi8i-_6Bp5L5km5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJ9GRkcRevgjTvo8Dc32iw_BLJPcPfRdVKhJT5HNzQuXEeN3QFwl2n0M6ZmO-h7C6bwVq0tbM60-zM6AQdjHj01JxuldTWgDhQ&lib=MwxUjRcLr2qLlnVOLh12wSNkqcO1Ikdrk'
-      )
-      .pipe(
-        switchMap(({ fulldate }) => {
-          const currentTimestamp = new Date(fulldate).getTime();
+    if (Date.now() < this.expiredDateUTC) {
+      this.window.location.assign(this.redirectUrl);
+      return of(false);
+    }
 
-          if (currentTimestamp < this.expiredDateUTC) {
-            this.window.location.assign(this.redirectUrl);
-            return of(false);
-          }
+    return of(true);
+    // return this.httpClient
+    //   .get<{ fulldate: string }>(
+    //     'https://script.googleusercontent.com/macros/echo?user_content_key=Vhsl1EP7sG2pMrcSNoyf0eAn7o8_UMnbX13lYZadzbU6HWn8kDJHWNHkepkSmqSvn3Rj0G4LNOIISxLzLVZLi8i-_6Bp5L5km5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJ9GRkcRevgjTvo8Dc32iw_BLJPcPfRdVKhJT5HNzQuXEeN3QFwl2n0M6ZmO-h7C6bwVq0tbM60-zM6AQdjHj01JxuldTWgDhQ&lib=MwxUjRcLr2qLlnVOLh12wSNkqcO1Ikdrk'
+    //   )
+    //   .pipe(
+    //     switchMap(({ fulldate }) => {
+    //       const currentTimestamp = new Date(fulldate).getTime();
 
-          return of(true);
-        })
-      );
+    //       if (currentTimestamp < this.expiredDateUTC) {
+    //         this.window.location.assign(this.redirectUrl);
+    //         return of(false);
+    //       }
+
+    //       return of(true);
+    //     })
+    //   );
   }
 }
