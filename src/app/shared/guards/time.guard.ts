2fec763b7cbb539fd5ca7311b39073a8dea18270
kolebayevv <ko1ebayev.worx@gmail.com>
2022-08-05 16:42:38 +0600

    update time

--- src/app/shared/guards/time.guard.ts
+++ src/app/shared/guards/time.guard.ts
@@ -3,19 +3,15 @@ import { CanActivate, CanLoad } from '@angular/router';
 import { WINDOW } from '@ng-web-apis/common';
 import { RubicWindow } from '@shared/utils/rubic-window';
 import { EXTERNAL_LINKS } from '@shared/constants/common/links';
-import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class TimeGuard implements CanActivate, CanLoad {
   private readonly redirectUrl = EXTERNAL_LINKS.LANDING_STAKING;
 
-  private readonly expiredDateUTC = Date.UTC(2022, 7, 5, 10, 30);
+  private readonly expiredDateUTC = Date.UTC(2022, 7, 5, 11, 0);
 
-  constructor(
-    @Inject(WINDOW) private readonly window: RubicWindow,
-    private readonly httpClient: HttpClient
-  ) {}
+  constructor(@Inject(WINDOW) private readonly window: RubicWindow) {}
 
   public canActivate(): Observable<boolean> {
     if (Date.now() < this.expiredDateUTC) {
