3993ffbeff1d7bf4b0dcd00988a3349bda987779
ottebrut <ott.andrey.ao@gmail.com>
2022-08-26 12:46:46 +0300

    Update version

--- src/app/shared/components/via-slippage-warning-modal/via-slippage-warning-modal.component.ts
+++ src/app/shared/components/via-slippage-warning-modal/via-slippage-warning-modal.component.ts
@@ -1,14 +1,12 @@
 import { Component, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { TuiDialogContext } from '@taiga-ui/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
-import { TuiDestroyService } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'polymorpheus-via-slippage-warning-modal',
   templateUrl: './via-slippage-warning-modal.component.html',
   styleUrls: ['./via-slippage-warning-modal.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [TuiDestroyService]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViaSlippageWarningModalComponent {
   constructor(
