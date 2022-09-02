d1c7509927a92f12ce580818ce506e42f849cca3
kolebayevv <ko1ebayev.worx@gmail.com>
2022-09-02 12:59:58 +0300

    remove console

--- src/app/features/swaps/features/cross-chain-routing/services/cross-chain-routing-service/cross-chain-routing.service.ts
+++ src/app/features/swaps/features/cross-chain-routing/services/cross-chain-routing-service/cross-chain-routing.service.ts
@@ -398,7 +398,6 @@ export class CrossChainRoutingService extends TradeService {
   }
 
   public parseCalculationError(error: RubicSdkError): RubicError<ERROR_TYPE> {
-    console.error({ [this.parseCalculationError.name]: error });
     if (error instanceof CrossChainIsUnavailableError) {
       return new CrossChainIsUnavailableWarning();
     }
