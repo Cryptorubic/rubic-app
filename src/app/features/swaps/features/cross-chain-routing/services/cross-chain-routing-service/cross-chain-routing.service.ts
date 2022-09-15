fe2d35194e0e3a3cb76f8e350761ba62b3ac4d99
kolebayevv <ko1ebayev.worx@gmail.com>
2022-09-15 17:14:13 +0300

    update sdk version

--- src/app/features/swaps/features/cross-chain-routing/services/cross-chain-routing-service/cross-chain-routing.service.ts
+++ src/app/features/swaps/features/cross-chain-routing/services/cross-chain-routing-service/cross-chain-routing.service.ts
@@ -137,7 +137,9 @@ export class CrossChainRoutingService extends TradeService {
         toSlippageTolerance: slippageTolerance / 2,
         slippageTolerance,
         timeout: this.defaultTimeout,
-        disabledProviders: isViaDisabled ? [CROSS_CHAIN_TRADE_TYPE.VIA] : []
+        disabledProviders: isViaDisabled
+          ? [CROSS_CHAIN_TRADE_TYPE.VIA]
+          : ['DEBRIDGE', 'CELER', 'RANGO', 'SYMBIOSIS', 'RUBIC']
       };
       return this.sdk.crossChain
         .calculateTradesReactively(fromToken, fromAmount.toString(), toToken, options)
