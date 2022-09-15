a5658117667b260a002ac1fce2f948441815a6a7
kolebayevv <ko1ebayev.worx@gmail.com>
2022-09-15 11:44:58 +0300

    it refresh btn fix

--- src/app/features/swaps/features/instant-trade/components/instant-trade-bottom-form/instant-trade-bottom-form.component.ts
+++ src/app/features/swaps/features/instant-trade/components/instant-trade-bottom-form/instant-trade-bottom-form.component.ts
@@ -220,6 +220,7 @@ export class InstantTradeBottomFormComponent implements OnInit {
       )
       .subscribe(form => {
         this.setupSwapForm(form);
+        this.conditionalCalculate('normal');
       });
 
     this.swapFormService.input.controls.toToken.valueChanges
@@ -259,7 +260,9 @@ export class InstantTradeBottomFormComponent implements OnInit {
         this.conditionalCalculate('normal');
       });
 
-    this.onRefreshTrade.pipe(takeUntil(this.destroy$)).subscribe(() => this.conditionalCalculate());
+    this.onRefreshTrade
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.conditionalCalculate('normal'));
   }
 
   /**
@@ -295,16 +298,14 @@ export class InstantTradeBottomFormComponent implements OnInit {
   /**
    * Makes additional checks and starts `normal` or `hidden` calculation.
    */
-  private conditionalCalculate(type?: 'normal' | 'hidden'): void {
+  private conditionalCalculate(type: 'normal' | 'hidden'): void {
     if (
       this.tradeStatus === TRADE_STATUS.APPROVE_IN_PROGRESS ||
       this.tradeStatus === TRADE_STATUS.SWAP_IN_PROGRESS
     ) {
       return;
     }
-
-    const { autoRefresh } = this.settingsService.instantTradeValue;
-    this.onCalculateTrade$.next(type || (autoRefresh ? 'normal' : 'hidden'));
+    this.onCalculateTrade$.next(type);
   }
 
   private setupNormalTradesCalculation(): void {
