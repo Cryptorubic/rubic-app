5bb32d175f115aade26da408bc03f4fceea3806c
ottebrut <ott.andrey.ao@gmail.com>
2022-09-19 14:14:31 +0300

    remove unused models

--- src/app/core/services/blockchain/models/call.ts
+++
@@ -1,4 +0,0 @@
-export interface Call {
-  target: string;
-  callData: string;
-}
--- src/app/core/services/blockchain/models/connection-link.ts
+++
@@ -1,7 +0,0 @@
-import { BlockchainName } from 'rubic-sdk';
-
-export default interface ConnectionLink {
-  blockchainName: BlockchainName;
-  rpcLink: string;
-  additionalRpcLink: string;
-}
