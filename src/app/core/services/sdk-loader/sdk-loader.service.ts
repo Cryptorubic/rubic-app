44ebca94dec94d1eaad5ca12188ae7af35b69320
kolebayevv <ko1ebayev.worx@gmail.com>
2022-09-30 11:13:27 +0300

    remove qp service

--- src/app/core/services/sdk-loader/sdk-loader.service.ts
+++ src/app/core/services/sdk-loader/sdk-loader.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { RubicSdkService } from '@app/features/swaps/core/services/rubic-sdk-service/rubic-sdk.service';
 import { IframeService } from '@core/services/iframe/iframe.service';
 import { StoreService } from '@core/services/store/store.service';
 import { AuthService } from '@core/services/auth/auth.service';
 import { WalletProvider } from 'rubic-sdk';
 import { WalletConnectorService } from '@core/services/blockchain/wallets/wallet-connector-service/wallet-connector.service';
-import { QueryParamsService } from '../query-params/query-params.service';
+import { WINDOW } from '@ng-web-apis/common';
 
 @Injectable({
   providedIn: 'root'
@@ -17,13 +17,14 @@ export class SdkLoaderService {
     private readonly storeService: StoreService,
     private readonly authService: AuthService,
     private readonly walletConnectorService: WalletConnectorService,
-    private readonly queryParamsService: QueryParamsService
+    @Inject(WINDOW) private readonly window: Window
   ) {}
 
   public async initSdk(): Promise<void> {
-    const providerAddress = this.queryParamsService.getUrlSearchParam('feeTarget');
     await this.loadUser();
-    await this.sdkService.initSDK(providerAddress);
+    await this.sdkService.initSDK(
+      new URLSearchParams(this.window.location.search).get('feeTarget') || undefined
+    );
     await this.updateSdkUser();
   }
 
