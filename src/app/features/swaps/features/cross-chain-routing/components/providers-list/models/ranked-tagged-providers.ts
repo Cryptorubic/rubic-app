80ab2e287f0be9d57cfb97a1f9f25c94856ef426
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-01 17:54:56 +0300

    minor fix

--- src/app/features/swaps/features/cross-chain-routing/components/providers-list/models/ranked-tagged-providers.ts
+++ src/app/features/swaps/features/cross-chain-routing/components/providers-list/models/ranked-tagged-providers.ts
@@ -6,5 +6,6 @@ export type RankedTaggedProviders = WrappedCrossChainTrade & {
     best: boolean;
     minAmountWarning: boolean;
     maxAmountWarning: boolean;
+    deflationTokenWarning?: boolean;
   };
 };
