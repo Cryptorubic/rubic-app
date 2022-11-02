9495039476f58e8f97ed2e8653e53a36d2214220
kolebayevv <ko1ebayev.worx@gmail.com>
2022-11-02 14:26:36 +0300

    fetch imported token balance after confirm

--- src/app/features/swaps/shared/tokens-select/components/custom-token/custom-token.component.ts
+++ src/app/features/swaps/shared/tokens-select/components/custom-token/custom-token.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable rxjs/no-async-subscribe */
 import {
   ChangeDetectionStrategy,
   Component,
@@ -13,6 +14,9 @@ import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus';
 import { TranslateService } from '@ngx-translate/core';
 import { AvailableTokenAmount } from '@shared/models/tokens/available-token-amount';
 import { CustomTokenWarningModalComponent } from '@features/swaps/shared/tokens-select/components/custom-token-warning-modal/custom-token-warning-modal.component';
+import { Web3PublicSupportedBlockchain, Web3Pure } from 'rubic-sdk';
+import { Injector as RubicInjector } from 'rubic-sdk';
+import { WalletConnectorService } from '@app/core/services/wallets/wallet-connector-service/wallet-connector.service';
 
 @Component({
   selector: 'app-custom-token',
@@ -40,7 +44,8 @@ export class CustomTokenComponent {
     @Inject(TuiDialogService) private readonly dialogService: TuiDialogService,
     @Inject(Injector) private readonly injector: Injector,
     private readonly translateService: TranslateService,
-    private readonly cdr: ChangeDetectorRef
+    private readonly cdr: ChangeDetectorRef,
+    private readonly walletConnectorService: WalletConnectorService
   ) {}
 
   /**
@@ -54,9 +59,15 @@ export class CustomTokenComponent {
         label: this.translateService.instant('modals.confirmImportModal.title'),
         size: 's'
       })
-      .subscribe((confirm: boolean) => {
+      .subscribe(async confirm => {
         if (confirm) {
-          this.tokenSelected.emit(this.token);
+          const tokenBalance = await RubicInjector.web3PublicService
+            .getWeb3Public(this.token.blockchain as Web3PublicSupportedBlockchain)
+            .getTokenBalance(this.walletConnectorService.address, this.token.address);
+          this.tokenSelected.emit({
+            ...this.token,
+            amount: Web3Pure.fromWei(tokenBalance, this.token.decimals)
+          });
         }
       });
   }
