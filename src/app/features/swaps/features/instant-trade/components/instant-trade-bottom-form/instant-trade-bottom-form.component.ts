664db9dce1dfb8869809781fc1dcd29d84d6627c
ottebrut <ott.andrey.ao@gmail.com>
2022-11-21 12:29:01 +0400

    fix bug

--- src/app/features/swaps/features/instant-trade/components/instant-trade-bottom-form/instant-trade-bottom-form.component.ts
+++ src/app/features/swaps/features/instant-trade/components/instant-trade-bottom-form/instant-trade-bottom-form.component.ts
@@ -695,7 +695,9 @@ export class InstantTradeBottomFormComponent implements OnInit {
     isSelected?: boolean
   ): void {
     this.tradeStatus = tradeStatus;
-    const trade = this.providersData.find(providerData => providerData.name === providerName).trade;
+    const trade = this.providersData.find(
+      providerData => providerData.name === providerName
+    )?.trade;
     const isProxy = trade instanceof EvmOnChainTrade && trade.useProxy;
 
     this.providersData = this.providersData.map(providerData => {
