5f1f2cce4a0c619a2faa1fb185511a320030967c
kolebayevv <ko1ebayev.worx@gmail.com>
2022-11-21 11:30:42 +0300

    fix bugs

--- src/app/features/swaps/shared/components/target-network-address/services/target-network-address.service.ts
+++ src/app/features/swaps/shared/components/target-network-address/services/target-network-address.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { FormControl } from '@ngneat/reactive-forms';
-import { debounceTime, distinctUntilChanged, filter, skip, startWith } from 'rxjs/operators';
-import { StoreService } from '@core/services/store/store.service';
 import { SwapFormService } from '@features/swaps/core/services/swap-form-service/swap-form.service';
 import { blockchainRequiresAddress } from '@features/swaps/shared/components/target-network-address/services/constants/blockchain-requires-address';
-import { correctAddressValidator } from './utils/correct-address-validator';
-import { isNil } from '@app/shared/utils/utils';
-import { compareAddresses } from 'rubic-sdk';
 
 @Injectable()
 export class TargetNetworkAddressService {
@@ -18,7 +13,7 @@ export class TargetNetworkAddressService {
   public readonly address$ = this._address$.asObservable();
 
   public get address(): string | null {
-    return this._address$.value;
+    return this._address$.getValue();
   }
 
   private readonly _isAddressRequired$ = new BehaviorSubject<boolean>(false);
@@ -29,62 +24,29 @@ export class TargetNetworkAddressService {
 
   public readonly isAddressValid$ = this._isAddressValid$.asObservable();
 
-  constructor(
-    private readonly storeService: StoreService,
-    private readonly swapFormService: SwapFormService
-  ) {
-    this.initSubscriptions();
+  constructor(private readonly swapFormService: SwapFormService) {
+    this.watchIsAddressRequired();
   }
 
-  private initSubscriptions(): void {
-    this.addressForm.valueChanges.pipe(debounceTime(10), distinctUntilChanged()).subscribe(() => {
-      if (this.addressForm.valid) {
-        this.storeService.setItem('targetAddress', this.addressForm.value);
-      }
-
-      this._address$.next(this.addressForm.valid ? this.addressForm.value : null);
-      this._isAddressValid$.next(this.addressForm.valid);
-    });
-
+  private watchIsAddressRequired(): void {
     combineLatest([
-      this.swapFormService.inputControls.fromToken.valueChanges,
-      this.swapFormService.inputControls.toToken.valueChanges
+      this.swapFormService.inputControls.fromBlockchain.valueChanges,
+      this.swapFormService.inputControls.toBlockchain.valueChanges
     ])
-      .pipe(
-        startWith(null),
-        filter(() => !Object.values(this.swapFormService.inputValue).some(value => isNil(value))),
-        skip(1),
-        distinctUntilChanged(([prevFromToken, prevToToken], [currFromToken, currToToken]) => {
-          return (
-            compareAddresses(prevFromToken.address, currFromToken.address) &&
-            compareAddresses(prevToToken.address, currToToken.address) &&
-            prevToToken.blockchain === currToToken.blockchain &&
-            prevFromToken.blockchain === currToToken.blockchain
-          );
-        })
-      )
-      .subscribe(([fromToken, toToken]) => {
-        const { blockchain: fromBlockchain } = fromToken;
-        const { blockchain: toBlockchain } = toToken;
-
-        this.addressForm.patchValue(null);
-        this._address$.next(null);
-
-        this._isAddressRequired$.next(
-          fromBlockchain !== toBlockchain &&
-            blockchainRequiresAddress.some(el => el === fromBlockchain || el === toBlockchain)
-        );
-        this.addressForm.clearValidators();
-        this.addressForm.setValidators(correctAddressValidator(fromBlockchain, toBlockchain));
-
-        this._isAddressValid$.next(this.addressForm.valid);
+      .pipe()
+      .subscribe(([from, to]) => {
+        const isAddressRequired =
+          from !== to &&
+          blockchainRequiresAddress.some(blockchain => blockchain === from || blockchain === to);
+        this._isAddressRequired$.next(isAddressRequired);
       });
   }
 
-  private setupTargetAddress(): void {
-    const targetAddress = this.storeService.getItem('targetAddress');
-    if (typeof targetAddress === 'string') {
-      this.addressForm.patchValue(targetAddress);
-    }
+  public setAddress(value: string): void {
+    this._address$.next(value);
+  }
+
+  public setIsAddressValid(value: boolean): void {
+    this._isAddressValid$.next(value);
   }
 }
--- src/app/features/swaps/shared/components/target-network-address/target-network-address.component.ts
+++ src/app/features/swaps/shared/components/target-network-address/target-network-address.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
-import { BlockchainName } from 'rubic-sdk';
-import { startWith } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, startWith } from 'rxjs/operators';
 import { TuiDestroyService } from '@taiga-ui/cdk';
 import { SwapFormService } from '@features/swaps/core/services/swap-form-service/swap-form.service';
-import { Observable } from 'rxjs';
+import { combineLatest } from 'rxjs';
 import { TargetNetworkAddressService } from '@features/swaps/shared/components/target-network-address/services/target-network-address.service';
 import { WINDOW } from '@ng-web-apis/common';
+import { correctAddressValidator } from './services/utils/correct-address-validator';
+import { FormControl } from '@angular/forms';
+import { compareObjects, isNil } from '@app/shared/utils/utils';
+import { TokenAmount } from '@app/shared/models/tokens/token-amount';
 
 @Component({
   selector: 'app-target-network-address',
@@ -15,9 +18,16 @@ import { WINDOW } from '@ng-web-apis/common';
   providers: [TuiDestroyService]
 })
 export class TargetNetworkAddressComponent implements OnInit {
-  public readonly address = this.targetNetworkAddressService.addressForm;
+  public readonly address = new FormControl<string>(undefined, [
+    correctAddressValidator(
+      this.swapFormService.inputValue.fromBlockchain,
+      this.swapFormService.inputValue.toBlockchain
+    )
+  ]);
 
-  public toBlockchain$: Observable<BlockchainName>;
+  public toBlockchain$ = this.swapFormService.input.controls.toBlockchain.valueChanges.pipe(
+    startWith(this.swapFormService.inputValue.toBlockchain)
+  );
 
   constructor(
     private readonly targetNetworkAddressService: TargetNetworkAddressService,
@@ -26,9 +36,34 @@ export class TargetNetworkAddressComponent implements OnInit {
   ) {}
 
   public ngOnInit(): void {
-    this.toBlockchain$ = this.swapFormService.input.controls.toBlockchain.valueChanges.pipe(
-      startWith(this.swapFormService.inputValue.toBlockchain)
-    );
+    this.subscribeOnTargetAddress();
+    this.subsctibeOnFormValues();
+  }
+
+  private subsctibeOnFormValues(): void {
+    combineLatest([
+      this.swapFormService.inputControls.fromToken.valueChanges,
+      this.swapFormService.inputControls.toToken.valueChanges
+    ])
+      .pipe(
+        filter(form => !form.some(value => isNil(value))),
+        map((form: [TokenAmount, TokenAmount]) => {
+          return form.map(value => {
+            return { address: value.address.toLocaleLowerCase(), blockchain: value.blockchain };
+          });
+        }),
+        distinctUntilChanged((prev, curr) => compareObjects(prev, curr))
+      )
+      .subscribe(() => {
+        this.address.patchValue(null);
+      });
+  }
+
+  private subscribeOnTargetAddress(): void {
+    this.address.valueChanges.pipe(debounceTime(10), distinctUntilChanged()).subscribe(address => {
+      this.targetNetworkAddressService.setIsAddressValid(this.address.valid);
+      this.targetNetworkAddressService.setAddress(this.address.valid ? address : null);
+    });
   }
 
   public async setValueFromClipboard(): Promise<void> {
