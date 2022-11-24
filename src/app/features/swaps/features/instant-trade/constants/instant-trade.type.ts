d85d92fa5d4f72a57b898a321101566c4ccb7698
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-24 13:44:08 +0300

    pr fix

--- src/app/features/swaps/features/instant-trade/constants/instant-trade.type.ts
+++ src/app/features/swaps/features/instant-trade/constants/instant-trade.type.ts
@@ -1,5 +1,11 @@
-import { BlockchainName } from 'rubic-sdk';
+import { BLOCKCHAIN_NAME, BlockchainName } from 'rubic-sdk';
 
-export type NonOnChainNetworks = 'BITGERT' | 'ASTAR' | 'BITCOIN' | 'DEFIKINGDOMS';
+const nonOnChainNetworks = [
+  BLOCKCHAIN_NAME.BITGERT,
+  BLOCKCHAIN_NAME.ASTAR,
+  BLOCKCHAIN_NAME.BITCOIN,
+  BLOCKCHAIN_NAME.DFK
+] as const;
+type NonOnChainNetworks = typeof nonOnChainNetworks[number];
 
 export type SupportedOnChainNetworks = Exclude<BlockchainName, NonOnChainNetworks>;
