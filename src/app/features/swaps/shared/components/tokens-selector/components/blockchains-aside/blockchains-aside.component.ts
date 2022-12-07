c5c57e523c8ec287bd0f9d512b14fba96cfe5527
ottebrut <ott.andrey.ao@gmail.com>
2022-12-07 15:03:06 +0400

    remove clearfix

--- src/app/features/swaps/shared/components/tokens-selector/components/blockchains-aside/blockchains-aside.component.ts
+++ src/app/features/swaps/shared/components/tokens-selector/components/blockchains-aside/blockchains-aside.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core';
 import { BlockchainName } from 'rubic-sdk';
-import { TUI_IS_IOS, TUI_IS_MOBILE } from '@taiga-ui/cdk';
-import { USER_AGENT, WINDOW } from '@ng-web-apis/common';
+import { WINDOW } from '@ng-web-apis/common';
 import { AvailableBlockchain } from '@features/swaps/shared/components/tokens-selector/services/blockchains-list-service/models/available-blockchain';
 import { BlockchainsListService } from '@features/swaps/shared/components/tokens-selector/services/blockchains-list-service/blockchains-list.service';
 import { TokensSelectorService } from '@features/swaps/shared/components/tokens-selector/services/tokens-selector-service/tokens-selector.service';
@@ -43,20 +42,7 @@ export class BlockchainsAsideComponent {
     })
   );
 
-  public get showClearFix(): boolean {
-    const safariDetector: RegExp = /iPhone/i;
-    const chromeDetector: RegExp = /Chrome/i;
-    return (
-      this.isIos &&
-      this.isMobile &&
-      (safariDetector.test(this.userAgent) || chromeDetector.test(this.userAgent))
-    );
-  }
-
   constructor(
-    @Inject(TUI_IS_IOS) private readonly isIos: boolean,
-    @Inject(TUI_IS_MOBILE) private readonly isMobile: boolean,
-    @Inject(USER_AGENT) private readonly userAgent: string,
     private readonly blockchainsListService: BlockchainsListService,
     private readonly tokensSelectorService: TokensSelectorService,
     private readonly windowWidthService: WindowWidthService,
