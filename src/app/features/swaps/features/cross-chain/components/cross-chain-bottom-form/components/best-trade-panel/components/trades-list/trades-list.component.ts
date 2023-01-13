0b9141e68a31c7d7e172ab390be67f9f93cbd09f
axtezy <axtezy@gmail.com>
2023-01-13 15:29:56 +0100

    Small fixes

--- src/app/features/swaps/features/cross-chain/components/cross-chain-bottom-form/components/best-trade-panel/components/trades-list/trades-list.component.ts
+++ src/app/features/swaps/features/cross-chain/components/cross-chain-bottom-form/components/best-trade-panel/components/trades-list/trades-list.component.ts
@@ -35,7 +35,7 @@ export class TradesListComponent {
     const isUsd =
       (error instanceof MinAmountError || error instanceof MinAmountError) &&
       error.tokenSymbol !== taggedTrade.trade.from.symbol &&
-      error.tokenSymbol === 'USDC';
+      error.tokenSymbol.toLowerCase().includes('usd');
 
     if (error instanceof MaxAmountError) {
       return `Max: ${error.maxAmount.toFixed(2)}${isUsd ? '$' : ''}`;
