27217bc105cd14bbb0c19cccb59e20f5fcdb394f
axtezy <axtezy@gmail.com>
2023-01-17 10:03:11 +0100

    Fix token symbol

--- src/app/features/swaps/features/cross-chain/components/cross-chain-bottom-form/components/best-trade-panel/components/trades-list/trades-list.component.ts
+++ src/app/features/swaps/features/cross-chain/components/cross-chain-bottom-form/components/best-trade-panel/components/trades-list/trades-list.component.ts
@@ -37,10 +37,10 @@ export class TradesListComponent {
       error.tokenSymbol.toLowerCase().includes('usd');
 
     if (error instanceof MaxAmountError) {
-      return `Max: ${error.maxAmount.toFixed(2)}${isUsd ? '$' : taggedTrade.trade.from.symbol}`;
+      return `Max: ${error.maxAmount.toFixed(2)}${isUsd ? '$' : ''}`;
     }
     if (error instanceof MinAmountError) {
-      return `Min: ${error.minAmount.toFixed(2)}${isUsd ? '$' : taggedTrade.trade.from.symbol}`;
+      return `Min: ${error.minAmount.toFixed(2)}${isUsd ? '$' : ''}`;
     }
   }
 
