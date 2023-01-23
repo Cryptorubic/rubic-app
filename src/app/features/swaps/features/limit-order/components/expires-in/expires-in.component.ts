c4ba627746edada46a3a1f3be760dcbe7ae32db0
ottebrut <ott.andrey.ao@gmail.com>
2023-01-23 13:39:05 +0400

    add form group in expires in

--- src/app/features/swaps/features/limit-order/components/expires-in/expires-in.component.ts
+++ src/app/features/swaps/features/limit-order/components/expires-in/expires-in.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { TimeFormControls } from '@features/swaps/features/limit-order/models/time-form';
 
 @Component({
   selector: 'app-expires-in',
@@ -6,4 +8,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./expires-in.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ExpiresInComponent {}
+export class ExpiresInComponent {
+  public readonly timeForm = new FormGroup<TimeFormControls>({
+    hours: new FormControl<number>(2),
+    minutes: new FormControl<number>(10)
+  });
+
+  public onSet(): void {}
+}
