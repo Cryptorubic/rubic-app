894a5f17d99248f777d2c9981603aa7ce2756f4d
ottebrut <ott.andrey.ao@gmail.com>
2023-01-24 17:18:50 +0400

    add rate levels

--- src/app/features/swaps/features/limit-order/components/order-rate/order-rate.component.ts
+++ src/app/features/swaps/features/limit-order/components/order-rate/order-rate.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { OrderRateService } from '@features/swaps/features/limit-order/services/order-rate.service';
+import {
+  RateLevel,
+  rateLevelsData
+} from '@features/swaps/features/limit-order/constants/rate-levels';
 
 @Component({
   selector: 'app-order-rate',
@@ -12,6 +16,10 @@ export class OrderRateComponent implements OnInit {
   public rate = new FormControl<string>('');
 
   public percentDiff = 0;
+
+  public iconSrc: string;
+
+  public percentValueClass: string;
 
   private get formattedRate(): string {
     return this.rate.value.split(',').join('');
@@ -30,8 +38,25 @@ export class OrderRateComponent implements OnInit {
         this.rate.setValue(value.toFixed());
       }
       this.percentDiff = percentDiff;
+      this.updateRateLevelData();
       this.cdr.markForCheck();
     });
+  }
+
+  private updateRateLevelData(): void {
+    let level: RateLevel;
+    if (this.percentDiff <= -10) {
+      level = RateLevel.RED;
+    } else if (this.percentDiff <= -5) {
+      level = RateLevel.YELLOW;
+    } else if (this.percentDiff <= 0) {
+      level = RateLevel.NOTHING;
+    } else {
+      level = RateLevel.GREEN;
+    }
+    const levelData = rateLevelsData[level];
+    this.iconSrc = levelData.imgSrc;
+    this.percentValueClass = levelData.class;
   }
 
   public onRateChange(rate: string): void {
---
+++ src/app/features/swaps/features/limit-order/constants/rate-levels.ts
@@ -0,0 +1,32 @@
+export enum RateLevel {
+  NOTHING,
+  GREEN,
+  YELLOW,
+  RED
+}
+
+interface RateLevelData {
+  imgSrc: string;
+  class: string;
+}
+
+const baseImgSrc = 'assets/images/icons/';
+
+export const rateLevelsData: Record<RateLevel, RateLevelData> = {
+  [RateLevel.NOTHING]: {
+    imgSrc: `${baseImgSrc}tick.svg`,
+    class: ''
+  },
+  [RateLevel.GREEN]: {
+    imgSrc: `${baseImgSrc}tick.svg`,
+    class: 'green'
+  },
+  [RateLevel.YELLOW]: {
+    imgSrc: `${baseImgSrc}exclamation-yellow.svg`,
+    class: 'yellow'
+  },
+  [RateLevel.RED]: {
+    imgSrc: `${baseImgSrc}exclamation-red.svg`,
+    class: 'red'
+  }
+};
