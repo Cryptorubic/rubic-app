61ae54eb2343999266d275aa7965f3af556620cd
ottebrut <ott.andrey.ao@gmail.com>
2023-02-02 17:28:01 +0400

    minor fixes

--- src/app/features/swaps/features/limit-order/constants/expiration-options.ts
+++ src/app/features/swaps/features/limit-order/constants/expiration-options.ts
@@ -5,23 +5,23 @@ export interface ExpirationOption {
 
 export const expirationOptions: ExpirationOption[] = [
   {
-    label: '1 Minute',
+    label: '1 minute',
     minutes: 1
   },
   {
-    label: '10 Minutes',
+    label: '10 minutes',
     minutes: 10
   },
   {
-    label: '1 Hour',
+    label: '1 hour',
     minutes: 60
   },
   {
-    label: '1 Day',
+    label: '1 day',
     minutes: 24 * 60
   },
   {
-    label: '7 Days',
+    label: '7 days',
     minutes: 7 * 24 * 60
   }
 ];
