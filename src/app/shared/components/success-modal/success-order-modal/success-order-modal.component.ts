f75fcfc794037cbefe2c305a3dd4c40f7810ca69
ottebrut <ott.andrey.ao@gmail.com>
2023-02-02 17:47:02 +0400

    update orders modal

--- src/app/shared/components/success-modal/success-order-modal/success-order-modal.component.ts
+++ src/app/shared/components/success-modal/success-order-modal/success-order-modal.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
 import { TuiDialogContext } from '@taiga-ui/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success-order-modal',
@@ -9,7 +10,15 @@ import { TuiDialogContext } from '@taiga-ui/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SuccessOrderModalComponent {
-  constructor(@Inject(POLYMORPHEUS_CONTEXT) private readonly context: TuiDialogContext<boolean>) {}
+  constructor(
+    @Inject(POLYMORPHEUS_CONTEXT) private readonly context: TuiDialogContext<boolean>,
+    private readonly router: Router
+  ) {}
+
+  public async navigateToOrders(): Promise<void> {
+    await this.router.navigate(['history/limit-orders'], { queryParamsHandling: 'merge' });
+    this.context.completeWith(true);
+  }
 
   public onConfirm(): void {
     this.context.completeWith(true);
