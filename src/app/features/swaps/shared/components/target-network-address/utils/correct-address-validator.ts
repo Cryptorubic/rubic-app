8e31d9b537bd184d18182ae2182858c993fa9287
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-02-21 12:03:55 +0300

    fix changeNow validator

--- src/app/features/swaps/shared/components/target-network-address/utils/correct-address-validator.ts
+++ src/app/features/swaps/shared/components/target-network-address/utils/correct-address-validator.ts
@@ -2,47 +2,34 @@ import { BlockchainName, BlockchainsInfo, Web3Pure } from 'rubic-sdk';
 import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
 import { AssetType } from '@features/swaps/shared/models/form/asset';
 import { CHAIN_TYPE } from 'rubic-sdk/lib/core/blockchain/models/chain-type';
-import { from, Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 
 export function correctAddressValidator(
   fromAssetType: AssetType,
   toBlockchain: BlockchainName
 ): AsyncValidatorFn {
-  let toChainType: CHAIN_TYPE;
-  try {
-    toChainType = BlockchainsInfo.getChainType(toBlockchain);
-  } catch {}
+  const toChainType: CHAIN_TYPE = BlockchainsInfo.getChainType(toBlockchain);
 
-  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+  return async (control: AbstractControl): Promise<ValidationErrors | null> => {
     const address = control.value;
 
     if (address === '') {
-      return of(null);
+      return null;
     }
 
-    return from(Web3Pure[toChainType].isAddressCorrect(address)).pipe(
-      map(isAddressCorrect => {
-        if (toChainType && !isAddressCorrect) {
-          let fromChainType: CHAIN_TYPE;
-          try {
-            fromChainType = BlockchainsInfo.getChainType(fromAssetType as BlockchainName);
-          } catch {}
-          if (address || fromChainType !== toChainType) {
-            return { wrongAddress: address };
-          }
-        }
+    const isAddressCorrectValue = await Web3Pure[toChainType].isAddressCorrect(address);
 
-        if (!toChainType) {
-          return !address?.length ? { wrongAddress: address } : null;
-        }
-
-        return null;
-      }),
-      catchError(error => {
-        console.log(error);
-        return of(null);
-      })
-    );
+    if (toChainType && !isAddressCorrectValue) {
+      let fromChainType: CHAIN_TYPE;
+      try {
+        fromChainType = BlockchainsInfo.getChainType(fromAssetType as BlockchainName);
+      } catch {}
+      if (address || fromChainType !== toChainType) {
+        return { wrongAddress: address };
+      }
+    }
+    if (!toChainType) {
+      return !address?.length ? { wrongAddress: address } : null;
+    }
+    return null;
   };
 }
