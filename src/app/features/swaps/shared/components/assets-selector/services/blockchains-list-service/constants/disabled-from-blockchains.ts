fb00d53a1204c1d928a21bf34f66fb14b7a34c41
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-02-27 11:06:06 +0300

    Enable swaps from changenow networks

--- src/app/features/swaps/shared/components/assets-selector/services/blockchains-list-service/constants/disabled-from-blockchains.ts
+++ src/app/features/swaps/shared/components/assets-selector/services/blockchains-list-service/constants/disabled-from-blockchains.ts
@@ -1,8 +1,6 @@
 import { BLOCKCHAIN_NAME, BlockchainName } from 'rubic-sdk';
-import { notEvmChangeNowBlockchainsList } from '@features/swaps/shared/components/assets-selector/services/blockchains-list-service/constants/blockchains-list';
 
 export const disabledFromBlockchains: BlockchainName[] = [
-  ...notEvmChangeNowBlockchainsList,
   BLOCKCHAIN_NAME.BITCOIN,
   BLOCKCHAIN_NAME.SOLANA,
   BLOCKCHAIN_NAME.NEAR
