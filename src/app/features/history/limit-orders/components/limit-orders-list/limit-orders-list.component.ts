b8ac00cc40511452391970448b4e1af198ca7362
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-03-21 11:01:49 +0300

    added noLimitTrx text

--- src/app/features/history/limit-orders/components/limit-orders-list/limit-orders-list.component.ts
+++ src/app/features/history/limit-orders/components/limit-orders-list/limit-orders-list.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '@app/core/services/auth/auth.service';
 import { LimitOrdersService } from '@app/core/services/limit-orders/limit-orders.service';
+import { WalletsModalService } from '@core/wallets-modal/services/wallets-modal.service';
+import { Router } from '@angular/router';
+import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
+import { TuiDialogContext } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-limit-orders-list',
@@ -12,12 +16,26 @@ export class LimitOrdersListComponent {
 
   public readonly loading$ = this.limitOrdersService.loading$;
 
+  public readonly user$ = this.authService.currentUser$;
+
   constructor(
     private readonly limitOrdersService: LimitOrdersService,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly walletsModalService: WalletsModalService,
+    private readonly router: Router,
+    @Inject(POLYMORPHEUS_CONTEXT) private readonly context: TuiDialogContext
   ) {
     this.authService.currentUser$.subscribe(() => {
       this.limitOrdersService.updateOrders();
     });
+  }
+
+  public onLogin(): void {
+    this.walletsModalService.open().subscribe();
+  }
+
+  public navigateToLimitOrder(): void {
+    this.context.completeWith(null);
+    this.router.navigate(['/limit-order']);
   }
 }
