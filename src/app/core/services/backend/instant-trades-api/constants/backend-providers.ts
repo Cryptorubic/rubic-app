65078c6344dbdc948d1ed579dbdaba5de17af42f
axtezy <axtezy@gmail.com>
2023-03-29 10:35:59 +0200

    Fix xy, add cronos dexes

--- src/app/core/services/backend/instant-trades-api/constants/backend-providers.ts
+++ src/app/core/services/backend/instant-trades-api/constants/backend-providers.ts
@@ -82,6 +82,7 @@ export const BACKEND_PROVIDERS: Record<OnChainTradeType, string> = {
   [ON_CHAIN_TRADE_TYPE.WAGYU_SWAP]: 'wagyuswap',
   [ON_CHAIN_TRADE_TYPE.ASTRO_SWAP]: 'astroswap',
   [ON_CHAIN_TRADE_TYPE.PEGASYS]: 'pegasys',
+  [ON_CHAIN_TRADE_TYPE.CRO_SWAP]: 'croswap',
 
   // Li-fi dexes
   [ON_CHAIN_TRADE_TYPE.CRONA_SWAP]: 'lifi',
