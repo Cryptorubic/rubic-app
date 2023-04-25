4fca32cdfecf7261fbfde56109aab92b2566fd4e
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-04-25 16:40:38 +0300

    fix form switcher for iframe

--- src/app/features/swaps/features/swap-form/swap-form.component.ts
+++ src/app/features/swaps/features/swap-form/swap-form.component.ts
@@ -33,6 +33,8 @@ import { Subject } from 'rxjs';
 import { RefreshService } from '@features/swaps/core/services/refresh-service/refresh.service';
 import { REFRESH_STATUS } from '@features/swaps/core/services/refresh-service/models/refresh-status';
 import { ModalService } from '@app/core/modals/services/modal.service';
+import { IframeService } from '@core/services/iframe/iframe.service';
+import { notEvmChangeNowBlockchainsList } from '@features/swaps/shared/components/assets-selector/services/blockchains-list-service/constants/blockchains-list';
 
 @Component({
   selector: 'app-swap-form',
@@ -55,6 +57,8 @@ export class SwapFormComponent implements OnInit, OnDestroy {
   public swapType: SWAP_PROVIDER_TYPE;
 
   public currentInstantTradeInfo: InstantTradeInfo;
+
+  public isIframeDstChainNotEvm: boolean = false;
 
   public readonly backgroundColor = this.queryParamsService.backgroundColor;
 
@@ -110,8 +114,18 @@ export class SwapFormComponent implements OnInit, OnDestroy {
     private readonly onramperFormService: OnramperFormService,
     private readonly refreshService: RefreshService,
     private readonly modalService: ModalService,
+    private readonly iframeService: IframeService,
     @Self() private readonly destroy$: TuiDestroyService
-  ) {}
+  ) {
+    this.swapFormService.toBlockchain$.subscribe(blockchainName => {
+      const notEvmChangeNowBlockchains = Object.values(
+        notEvmChangeNowBlockchainsList
+      ) as BlockchainName[];
+
+      this.isIframeDstChainNotEvm =
+        this.iframeService.isIframe && notEvmChangeNowBlockchains.includes(blockchainName);
+    });
+  }
 
   ngOnInit(): void {
     this.swapTypeService.swapMode$
