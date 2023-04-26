1d0c7716801ca08cbd82d916dc57e124d0258927
axtezy <axtezy@gmail.com>
2023-04-26 15:07:57 +0200

    Fix argent wallet

--- src/app/core/services/wallets/wallets-adapters/evm/argent-wallet-adapter.ts
+++ src/app/core/services/wallets/wallets-adapters/evm/argent-wallet-adapter.ts
@@ -2,13 +2,9 @@ import { BehaviorSubject } from 'rxjs';
 import { ErrorsService } from '@core/errors/errors.service';
 import { WALLET_NAME } from '@core/wallets-modal/components/wallets-modal/models/wallet-name';
 import { NgZone } from '@angular/core';
-import { BLOCKCHAIN_NAME, BlockchainName } from 'rubic-sdk';
+import { BlockchainName } from 'rubic-sdk';
 import { RubicWindow } from '@shared/utils/rubic-window';
 import { WalletConnectAbstractAdapter } from '@core/services/wallets/wallets-adapters/evm/common/wallet-connect-abstract';
-import { IWalletConnectProviderOptions } from '@walletconnect/types';
-import { WalletlinkError } from '@core/errors/models/provider/walletlink-error';
-import { getEthereumProvider } from '@argent/login';
-import { rpcList } from '@shared/constants/blockchain/rpc-list';
 
 export class ArgentWalletAdapter extends WalletConnectAbstractAdapter {
   public readonly walletName = WALLET_NAME.ARGENT;
@@ -20,30 +16,32 @@ export class ArgentWalletAdapter extends WalletConnectAbstractAdapter {
     zone: NgZone,
     window: RubicWindow
   ) {
-    const providerConfig: IWalletConnectProviderOptions = {
+    const providerConfig = {
       bridge: 'https://bridge.walletconnect.org',
-      qrcode: false
+      qrCode: true,
+      qrcodeModalOptions: {
+        desktopLinks: ['argent'],
+        mobileLinks: ['argent']
+      }
     };
     super(providerConfig, onAddressChanges$, onNetworkChanges$, errorsService, zone, window);
   }
 
   public async activate(): Promise<void> {
-    try {
-      this.wallet = await getEthereumProvider({
-        chainId: 1,
-        rpcUrl: rpcList[BLOCKCHAIN_NAME.ETHEREUM][0],
-        walletConnect: {
-          metadata: {
-            name: 'Rubic exchange',
-            description: 'Rubic Cross-Chain Tech Aggregator',
-            url: 'https://app.rubic.exchange',
-            icons: [`${this.window.location.origin}/assets/images/rbc.png`]
-          }
-        }
-      });
-      await super.activate();
-    } catch (error) {
-      throw new WalletlinkError();
-    }
+    setTimeout(() => {
+      const walletConnectorWrapper = this.window.document.querySelector('#walletconnect-wrapper');
+      const header = walletConnectorWrapper.querySelector('.walletconnect-modal__header');
+
+      const title = header.querySelector('p');
+      title.innerHTML = 'Connenct Argent Wallet';
+
+      const image = header.querySelector('img') as HTMLImageElement;
+      image.src = `${this.window.origin}/assets/images/icons/wallets/argent.svg`;
+
+      const description = walletConnectorWrapper.querySelector('#walletconnect-qrcode-text');
+      description.innerHTML = 'Scan QR code with a Argent wallet';
+    }, 300);
+
+    await super.activate();
   }
 }
