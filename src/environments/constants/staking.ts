aade30242112dbae6e1fa2020707000f58babd8d
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-05-30 18:36:08 +0300

    minor fix

--- src/environments/constants/staking.ts
+++ src/environments/constants/staking.ts
@@ -1,7 +1,7 @@
 export const STAKING_CONFIG_DEVELOP = {
   // nftContractAddress: '0x902A1dd986d618322a17dae6897bD5C21B7D393B', // минутные периоды
   // nftContractAddress: '0x66b2C1929e617082557F7d05Dec286eAc6e9e352', // дефолтные периоды
-  nftContractAddress: '0x66b2C1929e617082557F7d05Dec286eAc6e9e352',
+  nftContractAddress: '0x902A1dd986d618322a17dae6897bD5C21B7D393B',
   rbcToken: '0x11887ee906de64daa8b905b419bfeb6debafbf34'
 };
 
