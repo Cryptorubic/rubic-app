1ed2537fc48a8b4e96ace77ed1c049140dca6fbf
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-06-07 12:43:12 +0300

    minor fix

--- src/app/shared/guards/time.guard.ts
+++ src/app/shared/guards/time.guard.ts
@@ -1,19 +1,19 @@
 import { Inject, Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad } from '@angular/router';
 import { WINDOW } from '@ng-web-apis/common';
 import { RubicWindow } from '@shared/utils/rubic-window';
-import { catchError, map, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpService } from '@core/services/http/http.service';
 import { LoadResult } from '@shared/guards/models/types';
 
 @Injectable()
-export class TimeGuard implements CanActivate {
+export class TimeGuard implements CanActivate, CanLoad {
   constructor(
     @Inject(WINDOW) private readonly window: RubicWindow,
     private readonly httpService: HttpService
   ) {}
 
-  canLoad(route: ActivatedRouteSnapshot): LoadResult {
+  public canLoad(route: ActivatedRouteSnapshot): LoadResult {
     return this.canActivate(route);
   }
 
@@ -21,23 +21,34 @@ export class TimeGuard implements CanActivate {
     const { redirectPath, expiredDateInSeconds } = route.data;
     const currentDate = Date.now();
 
-    return this.httpService.get<{ current_timestamp: number }>('current_timestamp').pipe(
-      map(response => {
-        if (response.current_timestamp < expiredDateInSeconds) {
-          this.window.location.href = redirectPath;
-          return false;
-        } else {
-          return true;
-        }
-      }),
-      catchError(() => {
-        if (currentDate < expiredDateInSeconds * 1000) {
-          this.window.location.href = redirectPath;
-          return of(false);
-        } else {
-          return of(true);
-        }
-      })
-    );
+    if (currentDate < expiredDateInSeconds * 1000) {
+      this.window.location.href = redirectPath;
+      return of(false);
+    } else {
+      return of(true);
+    }
+
+    // return from(
+    //   firstValueFrom(
+    //     this.httpService.get<{ current_timestamp: number }>('current_timestamp').pipe(
+    //       map(response => {
+    //         if (response.current_timestamp < expiredDateInSeconds) {
+    //           this.window.location.href = redirectPath;
+    //           return false;
+    //         } else {
+    //           return true;
+    //         }
+    //       }),
+    //       catchError(() => {
+    //         if (currentDate < expiredDateInSeconds * 1000) {
+    //           this.window.location.href = redirectPath;
+    //           return of(false);
+    //         } else {
+    //           return of(true);
+    //         }
+    //       })
+    //     )
+    //   )
+    // );
   }
 }
