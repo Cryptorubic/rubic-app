807a327ac41a91e30651f368722ffec22550dccf
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-06-09 18:24:12 +0300

    fix timeGuard

--- src/app/shared/guards/time.guard.ts
+++ src/app/shared/guards/time.guard.ts
@@ -2,14 +2,18 @@ import { Inject, Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad } from '@angular/router';
 import { WINDOW } from '@ng-web-apis/common';
 import { RubicWindow } from '@shared/utils/rubic-window';
-import { Observable, of } from 'rxjs';
+import { catchError, firstValueFrom, from, map, Observable, of } from 'rxjs';
 import { HttpService } from '@core/services/http/http.service';
 import { LoadResult } from '@shared/guards/models/types';
+import { HeaderStore } from '@core/header/services/header.store';
 
 @Injectable()
 export class TimeGuard implements CanActivate, CanLoad {
+  private readonly isMobile = this.headerStore.isMobile;
+
   constructor(
     @Inject(WINDOW) private readonly window: RubicWindow,
+    private readonly headerStore: HeaderStore,
     private readonly httpService: HttpService
   ) {}
 
@@ -17,8 +21,10 @@ export class TimeGuard implements CanActivate, CanLoad {
     return this.canActivate(route);
   }
 
-  public canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
-    const { redirectPath, expiredDateInSeconds } = route.data;
+  private defaultTimeGuard(
+    redirectPath: string,
+    expiredDateInSeconds: number
+  ): Observable<boolean> {
     const currentDate = Date.now();
 
     if (currentDate < expiredDateInSeconds * 1000) {
@@ -27,28 +33,29 @@ export class TimeGuard implements CanActivate, CanLoad {
     } else {
       return of(true);
     }
+  }
 
-    // return from(
-    //   firstValueFrom(
-    //     this.httpService.get<{ current_timestamp: number }>('current_timestamp').pipe(
-    //       map(response => {
-    //         if (response.current_timestamp < expiredDateInSeconds) {
-    //           this.window.location.href = redirectPath;
-    //           return false;
-    //         } else {
-    //           return true;
-    //         }
-    //       }),
-    //       catchError(() => {
-    //         if (currentDate < expiredDateInSeconds * 1000) {
-    //           this.window.location.href = redirectPath;
-    //           return of(false);
-    //         } else {
-    //           return of(true);
-    //         }
-    //       })
-    //     )
-    //   )
-    // );
+  public canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    const { redirectPath, expiredDateInSeconds } = route.data;
+
+    if (this.isMobile) {
+      return this.defaultTimeGuard(redirectPath, expiredDateInSeconds);
+    } else {
+      return from(
+        firstValueFrom(
+          this.httpService.get<{ current_timestamp: number }>('current_timestamp').pipe(
+            map(response => {
+              if (response.current_timestamp < expiredDateInSeconds) {
+                this.window.location.href = redirectPath;
+                return false;
+              } else {
+                return true;
+              }
+            }),
+            catchError(() => this.defaultTimeGuard(redirectPath, expiredDateInSeconds))
+          )
+        )
+      );
+    }
   }
 }
