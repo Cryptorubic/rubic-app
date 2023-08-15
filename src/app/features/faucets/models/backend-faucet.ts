302c5fbe7922d38c21299b5082c05d9eb9c9ae40
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-08-15 11:24:18 +0300

    fix faucets title

--- src/app/features/faucets/models/backend-faucet.ts
+++ src/app/features/faucets/models/backend-faucet.ts
@@ -2,6 +2,7 @@ export interface BackendFaucet {
   readonly address: string;
   readonly symbol: string;
   readonly title: string;
+  readonly faucet_title: string;
   readonly network: string;
   readonly url: string;
   readonly icon_url: string;
