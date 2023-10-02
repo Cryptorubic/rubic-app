6c41a15d90adfe95731b5fd2c234975ea5fe3501
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-10-02 16:28:46 +0300

    upd analytics

--- src/assets/analytics/google-analytics.js
+++ src/assets/analytics/google-analytics.js
@@ -1,11 +1,13 @@
 <!-- Google Tag Manager -->
-(function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s);
-    j.async = true;
-    j.src = `https://www.googletagmanager.com/gtag/js?id=${i}`;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'G-QHYCGJXV8G');
+(function () {
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'G-QHYCGJXV8G');
+})();
 <!-- End Google Tag Manager -->
