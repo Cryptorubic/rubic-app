4533660dea22ecda928cbf70122b855f169c3eae
PseudoElement <bubamainer@mail.ru>
2023-11-16 15:32:09 +0300

    2004, add claim method for arbitrum-bridge-trade

--- src/app/features/history/components/cross-chain-desktop-table/cross-chain-desktop-table.component.ts
+++ src/app/features/history/components/cross-chain-desktop-table/cross-chain-desktop-table.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CrossChainTableService } from '@features/history/services/cross-chain-table-service/cross-chain-table.service';
 import { CrossChainTableData } from '@features/history/models/cross-chain-table-data';
 import { RubicAny } from '@shared/models/utility-types/rubic-any';
+import { CommonTableService } from '../../services/common-table-service/common-table.service';
 
 const crossChainCols = ['from', 'to', 'date', 'statusFrom', 'statusTo', 'provider'] as const;
 
@@ -14,34 +15,37 @@ const crossChainCols = ['from', 'to', 'date', 'statusFrom', 'statusTo', 'provide
 export class CrossChainDesktopTableComponent {
   @Input({ required: true }) device: 'mobile' | 'desktop' | 'tablet';
 
-  public readonly data$ = this.tableService.data$;
+  public readonly data$ = this.crossChainTableSrvice.data$;
 
-  public readonly loading$ = this.tableService.loading$;
+  public readonly loading$ = this.crossChainTableSrvice.loading$;
 
-  public readonly direction$ = this.tableService.direction$;
+  public readonly direction$ = this.crossChainTableSrvice.direction$;
 
-  public readonly sorter$ = this.tableService.sorter$;
+  public readonly sorter$ = this.crossChainTableSrvice.sorter$;
 
   public readonly columns = crossChainCols;
 
-  public readonly page$ = this.tableService.page$;
+  public readonly page$ = this.crossChainTableSrvice.page$;
 
-  public readonly totalPages$ = this.tableService.totalPages$;
+  public readonly totalPages$ = this.crossChainTableSrvice.totalPages$;
 
-  constructor(private readonly tableService: CrossChainTableService) {}
+  constructor(
+    private readonly crossChainTableSrvice: CrossChainTableService,
+    private readonly commonTableService: CommonTableService
+  ) {}
 
   public changeDirection(direction: 1 | -1): void {
-    this.tableService.onDirection(direction);
+    this.crossChainTableSrvice.onDirection(direction);
   }
 
   public changePage(page: number): void {
-    this.tableService.onPage(page);
+    this.crossChainTableSrvice.onPage(page);
   }
 
   public changeSorting(sorting: unknown): void {
     const sort = sorting as (typeof crossChainCols)[number];
     if (sort === 'date') {
-      this.tableService.onSorting('created_at');
+      this.crossChainTableSrvice.onSorting('created_at');
     }
   }
 
@@ -49,5 +53,9 @@ export class CrossChainDesktopTableComponent {
     innerItem: Partial<Record<keyof CrossChainTableData, RubicAny>>
   ): CrossChainTableData {
     return innerItem as unknown as CrossChainTableData;
+  }
+
+  public claimArbitrumTokens(toTxHash: string): void {
+    this.commonTableService.claimArbitrumBridgeTokens(toTxHash);
   }
 }
