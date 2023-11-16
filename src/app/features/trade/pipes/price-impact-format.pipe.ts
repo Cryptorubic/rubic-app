ee3f725493cde6d652d4be2f34cc3260c8d642bf
PseudoElement <bubamainer@mail.ru>
2023-11-16 19:25:59 +0300

    2016, delete log

--- src/app/features/trade/pipes/price-impact-format.pipe.ts
+++ src/app/features/trade/pipes/price-impact-format.pipe.ts
@@ -5,7 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PriceImpactFormatPipe implements PipeTransform {
   transform(value: number | null): string {
-    console.log('pipe', value);
     const isUnknown = isNaN(value) || value === undefined || value === null;
     if (isUnknown) return 'unknown';
     else if (value < 0.01) return '<0.01%';
