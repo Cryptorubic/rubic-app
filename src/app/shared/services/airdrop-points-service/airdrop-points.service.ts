c75db203edbaa7e32a7e0477aaa9545eb1103985
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-11-22 14:34:35 +0300

    Manta-Pacific promo

--- src/app/shared/services/airdrop-points-service/airdrop-points.service.ts
+++ src/app/shared/services/airdrop-points-service/airdrop-points.service.ts
@@ -65,7 +65,8 @@ export class AirdropPointsService {
           finalPoints = points.participant ? 12 : 25;
         }
 
-        return tradeType.to.blockchain === BLOCKCHAIN_NAME.LINEA
+        return tradeType.to.blockchain === BLOCKCHAIN_NAME.LINEA ||
+          tradeType.to.blockchain === BLOCKCHAIN_NAME.MANTA_PACIFIC
           ? Math.trunc(finalPoints / 2)
           : finalPoints;
       })
