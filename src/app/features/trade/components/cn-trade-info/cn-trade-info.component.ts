eb3df38dcf75b6aea8e2f5aedaac53d33eec3583
axtezy <axtezy@gmail.com>
2023-11-25 22:08:58 +0400

    Fix visual bugs

--- src/app/features/trade/components/cn-trade-info/cn-trade-info.component.ts
+++ src/app/features/trade/components/cn-trade-info/cn-trade-info.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { cnInfoText } from '@features/trade/components/cn-trade-info/constants/cn-info-text';
 import ADDRESS_TYPE from '@shared/models/blockchain/address-type';
-import { of } from 'rxjs';
+import { map, of } from 'rxjs';
 import { switchIif } from '@shared/utils/utils';
 import { SwapsStateService } from '@features/trade/services/swaps-state/swaps-state.service';
 import { WalletConnectorService } from '@core/services/wallets/wallet-connector-service/wallet-connector.service';
@@ -20,6 +20,18 @@ export class CnTradeInfoComponent {
   public readonly ADDRESS_TYPE = ADDRESS_TYPE;
 
   public readonly trade$ = this.cnSwapService.cnTrade$;
+
+  public readonly extraField$ = this.trade$.pipe(
+    map(trade =>
+      trade?.extraField
+        ? {
+            name: trade.extraField.name,
+            value: trade.extraField.value,
+            text: `Please don’t forget to specify the ${trade.extraField.name} while sending the ${trade.fromToken.symbol} transaction for the exchange`
+          }
+        : null
+    )
+  );
 
   public readonly walletAddress$ = this.targetAddressService.address$.pipe(
     switchIif(
--- src/app/features/trade/components/cn-trade-info/constants/cn-info-text.ts
+++ src/app/features/trade/components/cn-trade-info/constants/cn-info-text.ts
@@ -3,11 +3,6 @@ export const cnInfoText = {
     label: 'Transaction ID',
     description: 'Transaction swap identifier'
   },
-  memo: {
-    label: 'Memo',
-    description:
-      'Please don’t forget to specify the Memo while sending the ATOM transaction for the exchange'
-  },
   receiver: {
     label: 'Receiver address',
     description: 'Address to send assets'
