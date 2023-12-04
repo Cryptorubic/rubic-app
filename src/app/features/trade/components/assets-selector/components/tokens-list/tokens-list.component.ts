f3251c7841e8fb4efeeec2a02ae91c33974260f5
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-12-04 11:56:45 +0300

    upd token selector#1-5

--- src/app/features/trade/components/assets-selector/components/tokens-list/tokens-list.component.ts
+++ src/app/features/trade/components/assets-selector/components/tokens-list/tokens-list.component.ts
@@ -2,12 +2,17 @@ import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { AvailableTokenAmount } from '@shared/models/tokens/available-token-amount';
 import { QueryParamsService } from '@core/services/query-params/query-params.service';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
-import { of, switchMap } from 'rxjs';
+import { BehaviorSubject, of, switchMap } from 'rxjs';
 import { LIST_ANIMATION } from '@features/trade/components/assets-selector/animations/list-animation';
 import { AssetsSelectorService } from '@features/trade/components/assets-selector/services/assets-selector-service/assets-selector.service';
 import { TokensListService } from '@features/trade/components/assets-selector/services/tokens-list-service/tokens-list.service';
 import { TokensListStoreService } from '@features/trade/components/assets-selector/services/tokens-list-service/tokens-list-store.service';
 import { MobileNativeModalService } from '@core/modals/services/mobile-native-modal.service';
+import { map } from 'rxjs/operators';
+import { SwapsFormService } from '@features/trade/services/swaps-form/swaps-form.service';
+import { BLOCKCHAIN_NAME } from 'rubic-sdk';
+import { TokenAmount } from '@shared/models/tokens/token-amount';
+import { AssetType } from '@features/trade/models/asset';
 
 @Component({
   selector: 'app-tokens-list',
@@ -38,13 +43,92 @@ export class TokensListComponent {
     })
   );
 
+  private readonly _metisText$ = new BehaviorSubject<string>('');
+
+  public readonly metisText$ = this._metisText$.asObservable();
+
+  public readonly newTokensToShow$ = this.tokensToShow$.pipe(
+    map(tokens => {
+      const formType = this.assetsSelectorService.formType;
+      const fromBlockchain = this.swapFormService.inputValue['fromBlockchain'];
+      const fromToken = this.swapFormService.inputValue.fromToken;
+      // const toBlockchain = this.swapFormService.inputValue['toBlockchain'];
+      // const toToken = this.swapFormService.inputValue.toToken;
+      const currentBlockchain = this.assetsSelectorService.assetType;
+
+      // Проверки для целевой сети при исходной METIS
+      if (formType === 'to' && fromBlockchain === BLOCKCHAIN_NAME.METIS) {
+        return this.getAvailableTokensForSwapFromSrcMetis(tokens, currentBlockchain, fromToken);
+      }
+
+      // Проверки для целевой сети METIS при исходной BSC / AVAX
+      if (
+        formType === 'to' &&
+        currentBlockchain === BLOCKCHAIN_NAME.METIS &&
+        (fromBlockchain === BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN ||
+          fromBlockchain === BLOCKCHAIN_NAME.AVALANCHE)
+      ) {
+        return [tokens.find(token => token.symbol.toLowerCase() === 'm.usdt')];
+      }
+
+      // Проверки для целевой сети METIS при исходной ETH
+      if (
+        formType === 'to' &&
+        currentBlockchain === BLOCKCHAIN_NAME.METIS &&
+        fromBlockchain === BLOCKCHAIN_NAME.ETHEREUM
+      ) {
+        return tokens.filter(
+          token => token.symbol.toLowerCase() === 'm.usdt' || token.symbol.toLowerCase() === 'metis'
+        );
+      }
+      return tokens;
+    })
+  );
+
   constructor(
     private readonly queryParamsService: QueryParamsService,
     private readonly assetsSelectorService: AssetsSelectorService,
+    private readonly swapFormService: SwapsFormService,
     private readonly tokensListService: TokensListService,
     private readonly tokensListStoreService: TokensListStoreService,
     private readonly mobileNativeService: MobileNativeModalService
   ) {}
+
+  private getAvailableTokensForSwapFromSrcMetis(
+    tokens: AvailableTokenAmount[],
+    currentBlockchain: AssetType,
+    fromToken: TokenAmount
+  ): AvailableTokenAmount[] {
+    if (
+      currentBlockchain !== BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN &&
+      currentBlockchain !== BLOCKCHAIN_NAME.AVALANCHE &&
+      currentBlockchain !== BLOCKCHAIN_NAME.ETHEREUM &&
+      currentBlockchain !== BLOCKCHAIN_NAME.METIS
+    ) {
+      this._metisText$.next(
+        'From the selected network swaps are available only in BNB, Avalanche and Ethereum'
+      );
+      return [];
+    }
+    if (
+      currentBlockchain === BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN ||
+      currentBlockchain === BLOCKCHAIN_NAME.AVALANCHE
+    ) {
+      return [tokens.find(token => token.symbol.toLowerCase() === 'usdt')];
+    }
+
+    if (currentBlockchain === BLOCKCHAIN_NAME.ETHEREUM) {
+      if (fromToken.symbol.toLowerCase() === 'metis') {
+        return tokens.filter(
+          token => token.symbol.toLowerCase() === 'usdt' || token.symbol.toLowerCase() === 'metis'
+        );
+      } else {
+        return [tokens.find(token => token.symbol.toLowerCase() === 'usdt')];
+      }
+    }
+
+    return tokens;
+  }
 
   /**
    * Function to track list element by unique key: token blockchain and address.
