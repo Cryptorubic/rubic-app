d109ce1f4d2bd3ce625ea99706668f005f9e76be
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-12-18 14:40:22 +0300

    upd layer3 widget

--- src/app/shared/components/layer3-widget/layer3-widget.component.ts
+++ src/app/shared/components/layer3-widget/layer3-widget.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { WINDOW } from '@ng-web-apis/common';
 import { RubicWindow } from '@shared/utils/rubic-window';
-import { WindowWidthService } from '@core/services/widnow-width-service/window-width.service';
+import { HeaderStore } from '@core/header/services/header.store';
 
 @Component({
   selector: 'app-layer3-widget',
@@ -10,20 +10,63 @@ import { WindowWidthService } from '@core/services/widnow-width-service/window-w
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Layer3WidgetComponent {
+  public isMobile = false;
+
   constructor(
-    @Inject(WINDOW) private readonly window: RubicWindow,
-    private readonly windowWidth: WindowWidthService
-  ) {}
+    private readonly headerStore: HeaderStore,
+    @Inject(WINDOW) private readonly window: RubicWindow
+  ) {
+    this.isMobile = headerStore.isMobile;
+  }
 
   public setupIframe(): void {
     const layerWidget = this.window.document.querySelector(
       'iframe#layer3-widget'
     ) as HTMLIFrameElement;
-    console.log(layerWidget);
+
+    if (this.isMobile) {
+      layerWidget.style.width = '100%';
+      layerWidget.style.marginBottom = '60px';
+      layerWidget.style.marginTop = '-95px';
+      layerWidget.style.zIndex = '1000';
+    } else {
+      layerWidget.style.width = '310px';
+      layerWidget.style.position = 'absolute';
+      layerWidget.style.bottom = '25px';
+      layerWidget.style.left = '15px';
+      layerWidget.style.zIndex = '1000';
+    }
 
     this.window.addEventListener('message', event => {
-      if (event.data) {
-        console.log(event.data);
+      if (event.data === 'closeModal') {
+        if (this.isMobile) {
+          layerWidget.style.position = 'static';
+          layerWidget.style.top = 'initial';
+          layerWidget.style.height = 'initial';
+          layerWidget.style.marginTop = '-95px';
+        } else {
+          layerWidget.style.height = 'auto';
+          layerWidget.style.width = '310px';
+          layerWidget.style.left = '15px';
+          layerWidget.style.bottom = '25px';
+          layerWidget.style.transform = 'translate(0, 0)';
+        }
+      }
+
+      if (event.data === 'modalOpened') {
+        if (this.isMobile) {
+          layerWidget.style.position = 'absolute';
+          layerWidget.style.top = '0';
+          layerWidget.style.height = '100vh';
+          layerWidget.style.marginTop = '0';
+        } else {
+          layerWidget.style.height = '656px';
+          layerWidget.style.width = '700px';
+          layerWidget.style.borderRadius = '15px';
+          layerWidget.style.left = '50%';
+          layerWidget.style.bottom = '50%';
+          layerWidget.style.transform = 'translate(-50%, 50%)';
+        }
       }
     });
   }
