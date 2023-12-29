c4d6b6a9bbcc50e6a687e46e11a37cb8f379c753
pseudoelement <bubamainer@mail.ru>
2023-12-29 16:06:21 +0300

    2111, implement async pipes

--- src/app/core/header/components/header/components/user-profile/user-profile.component.ts
+++ src/app/core/header/components/header/components/user-profile/user-profile.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Self } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { UserInterface } from 'src/app/core/services/auth/models/user.interface';
 import { WalletConnectorService } from 'src/app/core/services/wallets/wallet-connector-service/wallet-connector.service';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { HeaderStore } from '../../../../services/header.store';
 import { TuiDestroyService } from '@taiga-ui/cdk';
-import { takeUntil } from 'rxjs/operators';
+import { map, startWith, takeUntil } from 'rxjs/operators';
 import { blockchainIcon } from '@shared/constants/blockchain/blockchain-icon';
 import { ModalService } from '@app/core/modals/services/modal.service';
 import { TradesHistory } from '@core/header/components/header/components/mobile-user-profile/models/tradeHistory';
@@ -54,15 +54,13 @@ export class UserProfileComponent {
       .subscribe(async () => {
         await this.authService.setSpaceIdData();
       });
-
-    this.currentUser$ = this.authService.currentUser$;
   }
 
   public readonly isConfirmModalOpened$: Observable<boolean>;
 
   public readonly isMobile$: Observable<boolean>;
 
-  public currentUser$: Observable<UserInterface>;
+  public currentUser$: Observable<UserInterface> = this.authService.currentUser$;
 
   public currentBlockchainName: BlockchainName;
 
@@ -71,6 +69,30 @@ export class UserProfileComponent {
   public dropdownIsOpened = false;
 
   public spaceIdData$ = this.authService.spaceIdData$;
+
+  public profileText$: Observable<string> = combineLatest([
+    this.authService.spaceIdData$,
+    this.authService.currentUser$
+  ]).pipe(
+    map(([spaceIdData, user]) => {
+      if (spaceIdData?.name) {
+        return spaceIdData.name;
+      }
+
+      return user.address;
+    }),
+    startWith(this.authService.userAddress)
+  );
+
+  public avatar$ = this.authService.spaceIdData$.pipe(
+    map(spaceIdData => {
+      if (spaceIdData?.avatar) {
+        return spaceIdData.avatar;
+      }
+
+      return this.currentBlockchainIcon;
+    })
+  );
 
   public logout(): void {
     this.authService.disconnectWallet();
