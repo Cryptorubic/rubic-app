0d7c26d15c144d55126dac65de3f49de4af6d85d
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-01-10 16:21:34 +0300

    fix name

--- src/app/features/trade/components/mev-bot/mev-bot.component.ts
+++ src/app/features/trade/components/mev-bot/mev-bot.component.ts
@@ -19,7 +19,7 @@ export class MevBotComponent {
 
   public displayMev: boolean = false;
 
-  @Input() set tradePrice(trade: CrossChainTrade | OnChainTrade) {
+  @Input() set trade(trade: CrossChainTrade | OnChainTrade) {
     const minDollarAmountToDisplay = 1;
     const amount = trade?.from.price.multipliedBy(trade?.from.tokenAmount);
 
