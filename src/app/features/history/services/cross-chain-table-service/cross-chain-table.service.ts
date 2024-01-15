0f086a2d52dc1146a6062e13e0982d41780587b0
pseudoelement <bubamainer@mail.ru>
2024-01-15 18:28:27 +0300

    2097, cope chnagenowId instead of txHash

--- src/app/features/history/services/cross-chain-table-service/cross-chain-table.service.ts
+++ src/app/features/history/services/cross-chain-table-service/cross-chain-table.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { combineLatest, combineLatestWith, Observable } from 'rxjs';
 import { TableKey } from '@features/history/models/table-key';
-import { debounceTime, filter, map, share, startWith, switchMap } from 'rxjs/operators';
+import { debounceTime, filter, map, share, startWith, switchMap, tap } from 'rxjs/operators';
 import { tuiControlValue, tuiIsFalsy, tuiIsPresent } from '@taiga-ui/cdk';
 import { CrossChainTableResponse } from '@features/history/models/cross-chain-table-response';
 import { HttpService } from '@core/services/http/http.service';
@@ -59,6 +59,7 @@ export class CrossChainTableService extends TableService<
   public readonly data$: Observable<CrossChainTableData[]> = this.request$.pipe(
     filter(tuiIsPresent),
     map(response => response.data.filter(tuiIsPresent)),
+    tap(i => console.log('DATA$', i)),
     startWith([])
   );
 
@@ -158,7 +159,8 @@ export class CrossChainTableService extends TableService<
           fromTx,
           toTx,
           date: backendData.created_at,
-          provider
+          provider,
+          ...(backendData.changenow_id && { changenowId: backendData.changenow_id })
         };
       }),
       total: response.count
