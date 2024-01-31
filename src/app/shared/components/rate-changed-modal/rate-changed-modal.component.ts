7fa3b7477ba6db9b0b5fd628a23accc74fba477a
pseudoelement <bubamainer@mail.ru>
2024-01-31 12:31:53 +0300

    2183, 4.53.0-alpha.cn.2

--- src/app/shared/components/rate-changed-modal/rate-changed-modal.component.ts
+++ src/app/shared/components/rate-changed-modal/rate-changed-modal.component.ts
@@ -1,17 +1,19 @@
-import { ChangeDetectionStrategy, Component, Inject, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Self } from '@angular/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
 import { TuiDialogContext } from '@taiga-ui/core';
 import BigNumber from 'bignumber.js';
 import { SWAP_PROVIDER_TYPE } from '@features/trade/models/swap-provider-type';
-import { BehaviorSubject, takeUntil, timer } from 'rxjs';
+import { takeUntil, timer } from 'rxjs';
+import { TuiDestroyService } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-rate-changed-modal',
   templateUrl: './rate-changed-modal.component.html',
   styleUrls: ['./rate-changed-modal.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [TuiDestroyService]
 })
-export class RateChangedModalComponent implements OnDestroy {
+export class RateChangedModalComponent {
   public readonly oldAmount: BigNumber;
 
   public readonly newAmount: BigNumber;
@@ -20,14 +22,13 @@ export class RateChangedModalComponent implements OnDestroy {
 
   public readonly tokenSymbol: string;
 
-  private destroyed$ = new BehaviorSubject<boolean>(false);
-
   constructor(
     @Inject(POLYMORPHEUS_CONTEXT)
     private readonly context: TuiDialogContext<
       boolean,
       { oldAmount: BigNumber; newAmount: BigNumber; tokenSymbol: string }
-    >
+    >,
+    @Self() private readonly destroyed$: TuiDestroyService
   ) {
     this.oldAmount = context.data.oldAmount;
     this.newAmount = context.data.newAmount;
@@ -40,10 +41,6 @@ export class RateChangedModalComponent implements OnDestroy {
     timer(15_000)
       .pipe(takeUntil(this.destroyed$))
       .subscribe(() => this.onCancel());
-  }
-
-  ngOnDestroy(): void {
-    this.destroyed$.next(true);
   }
 
   public onConfirm(): void {
