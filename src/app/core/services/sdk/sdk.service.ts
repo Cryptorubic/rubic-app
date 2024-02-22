6885b611d442310c3e16fc2adf95090c693dda80
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-02-22 16:33:06 +0300

    minor fix

--- src/app/core/services/sdk/sdk.service.ts
+++ src/app/core/services/sdk/sdk.service.ts
@@ -84,10 +84,6 @@ export class SdkService {
       crossChain: '0x3fFF9bDEb3147cE13A7FFEf85Dae81874E0AEDbE',
       onChain: '0x3b9Ce17A7bD729A0abc5976bEAb6D7d150fbD0d4'
     };
-    console.log('Provider Addresses: ', {
-      crossChain: params?.crossChainIntegratorAddress || defaultProvidersAddresses.crossChain,
-      onChain: params?.onChainIntegratorAddress || defaultProvidersAddresses.onChain
-    });
     this.currentConfig = {
       ...this.defaultConfig,
       providerAddress: {
