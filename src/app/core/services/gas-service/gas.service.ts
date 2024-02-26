0234c8df421d2bc38f661a32ef74232912c11946
PseudoElement <bubamainer@mail.ru>
2024-02-26 17:30:43 +0300

    2226, fix kroma gasPrice

--- src/app/core/services/gas-service/gas.service.ts
+++ src/app/core/services/gas-service/gas.service.ts
@@ -29,7 +29,8 @@ const supportedBlockchains = [
   BLOCKCHAIN_NAME.MANTLE,
   BLOCKCHAIN_NAME.POLYGON_ZKEVM,
   BLOCKCHAIN_NAME.SCROLL,
-  BLOCKCHAIN_NAME.MANTA_PACIFIC
+  BLOCKCHAIN_NAME.MANTA_PACIFIC,
+  BLOCKCHAIN_NAME.KROMA
 ] as const;
 
 type SupportedBlockchain = (typeof supportedBlockchains)[number];
@@ -85,7 +86,8 @@ export class GasService {
       [BLOCKCHAIN_NAME.MANTLE]: new BehaviorSubject(null),
       [BLOCKCHAIN_NAME.POLYGON_ZKEVM]: new BehaviorSubject(null),
       [BLOCKCHAIN_NAME.SCROLL]: new BehaviorSubject(null),
-      [BLOCKCHAIN_NAME.MANTA_PACIFIC]: new BehaviorSubject(null)
+      [BLOCKCHAIN_NAME.MANTA_PACIFIC]: new BehaviorSubject(null),
+      [BLOCKCHAIN_NAME.KROMA]: new BehaviorSubject(null)
     };
     this.gasPriceFunctions = {
       [BLOCKCHAIN_NAME.ETHEREUM]: this.fetchEthGas.bind(this),
@@ -103,7 +105,8 @@ export class GasService {
       [BLOCKCHAIN_NAME.MANTLE]: this.fetchMantleGas.bind(this),
       [BLOCKCHAIN_NAME.POLYGON_ZKEVM]: this.fetchPolygonZkEvmGas.bind(this),
       [BLOCKCHAIN_NAME.SCROLL]: this.fetchScrollGas.bind(this),
-      [BLOCKCHAIN_NAME.MANTA_PACIFIC]: this.fetchMantaPacificGas.bind(this)
+      [BLOCKCHAIN_NAME.MANTA_PACIFIC]: this.fetchMantaPacificGas.bind(this),
+      [BLOCKCHAIN_NAME.KROMA]: this.fetchKromaGas.bind(this)
     };
 
     this.setIntervalOnGasPriceRefreshing();
@@ -358,6 +361,17 @@ export class GasService {
   })
   private fetchArbitrumGas(): Observable<GasPrice | null> {
     const blockchainAdapter = Injector.web3PublicService.getWeb3Public(BLOCKCHAIN_NAME.ARBITRUM);
+    return from(blockchainAdapter.getPriorityFeeGas()).pipe(
+      map(formatEIP1559Gas),
+      catchError(() => of(null))
+    );
+  }
+
+  @Cacheable({
+    maxAge: GasService.requestInterval
+  })
+  private fetchKromaGas(): Observable<GasPrice> {
+    const blockchainAdapter = Injector.web3PublicService.getWeb3Public(BLOCKCHAIN_NAME.KROMA);
     return from(blockchainAdapter.getPriorityFeeGas()).pipe(
       map(formatEIP1559Gas),
       catchError(() => of(null))
