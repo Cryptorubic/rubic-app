5ed79352113de3c9e5abfcd8431eda44a5ca867d
PseudoElement <bubamainer@mail.ru>
2024-03-06 13:34:44 +0300

    TESTNETS, fix blast_testnet name

--- src/app/shared/constants/blockchain/backend-blockchains.ts
+++ src/app/shared/constants/blockchain/backend-blockchains.ts
@@ -130,7 +130,7 @@ const BLOCKCHAINS_MAPPING: Record<BlockchainName, string> = {
   [BLOCKCHAIN_NAME.MANTA_PACIFIC]: 'manta-pacific',
   [BLOCKCHAIN_NAME.SCROLL]: 'scroll',
   [BLOCKCHAIN_NAME.BERACHAIN]: 'berachain',
-  [BLOCKCHAIN_NAME.BLAST_TESTNET]: 'blast',
+  [BLOCKCHAIN_NAME.BLAST_TESTNET]: 'blast-testnet',
   [BLOCKCHAIN_NAME.BLAST]: 'blast',
   [BLOCKCHAIN_NAME.HOLESKY]: 'holesky'
 } as const;
