78b899234f73baa8d4a5a144e13a956c7fe60a8c
PseudoElement <bubamainer@mail.ru>
2024-04-08 16:25:59 +0300

    2263, handle gtag events firing

--- src/app/features/trade/services/cross-chain/cross-chain.service.ts
+++ src/app/features/trade/services/cross-chain/cross-chain.service.ts
@@ -51,6 +51,8 @@ import { TradeParser } from '@features/trade/utils/trade-parser';
 import { SessionStorageService } from '@core/services/session-storage/session-storage.service';
 import { AirdropPointsService } from '@app/shared/services/airdrop-points-service/airdrop-points.service';
 import { CALCULATION_TIMEOUT_MS } from '../../constants/calculation';
+import { FormsTogglerService } from '../forms-toggler/forms-toggler.service';
+import { MAIN_FORM_TYPE } from '../forms-toggler/models';
 
 @Injectable()
 export class CrossChainService {
@@ -79,7 +81,8 @@ export class CrossChainService {
     private readonly authService: AuthService,
     private readonly gtmService: GoogleTagManagerService,
     private readonly gasService: GasService,
-    private readonly airdropPointsService: AirdropPointsService
+    private readonly airdropPointsService: AirdropPointsService,
+    private readonly formsTogglerService: FormsTogglerService
   ) {}
 
   public calculateTrades(disabledTradeTypes: CrossChainTradeType[]): Observable<TradeContainer> {
@@ -313,6 +316,9 @@ export class CrossChainService {
 
       if (!(parsedError instanceof UserRejectError)) {
         this.gtmService.fireTransactionError(trade.from.name, trade.to.name, error.code);
+        if (this.formsTogglerService.selectedForm === MAIN_FORM_TYPE.GAS_FORM) {
+          this.gtmService.fireGasFormGtm({ isSuccessfullSwap: false });
+        }
       }
 
       throw parsedError;
@@ -456,6 +462,10 @@ export class CrossChainService {
       'crosschain',
       fromAmount.multipliedBy(fromToken.price).gt(1000) ? useMevBotProtection : null
     );
+
+    if (this.formsTogglerService.selectedForm === MAIN_FORM_TYPE.GAS_FORM) {
+      this.gtmService.fireGasFormGtm({ isSuccessfullSwap: true });
+    }
   }
 
   private async conditionalAwait(blockchain: BlockchainName): Promise<void> {
