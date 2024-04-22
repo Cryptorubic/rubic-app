0db486d8e8bf31e9ebd77b8de33e25cb75ca950e
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-04-22 12:50:59 +0300

    added x layer network

--- src/app/features/trade/components/assets-selector/services/blockchains-list-service/constants/blockchains-list.ts
+++ src/app/features/trade/components/assets-selector/services/blockchains-list-service/constants/blockchains-list.ts
@@ -147,6 +147,7 @@ export const blockchainsList: RankedBlockchain[] = [
   { name: BLOCKCHAIN_NAME.SCROLL, rank: 0.5, tags: ['ETH'] },
   { name: BLOCKCHAIN_NAME.ZETACHAIN, rank: 0.5, tags: ['ZETA'] },
   { name: BLOCKCHAIN_NAME.MODE, rank: 0, tags: ['ETH'] },
+  { name: BLOCKCHAIN_NAME.XLAYER, rank: 0, tags: ['OKB'] },
   { name: BLOCKCHAIN_NAME.MERLIN, rank: 0, tags: ['BTC'] },
   { name: BLOCKCHAIN_NAME.ZK_FAIR, rank: 0, tags: ['USDC'] },
   { name: BLOCKCHAIN_NAME.ZK_LINK, rank: 0, tags: ['ETH'] },
