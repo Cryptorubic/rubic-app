a2737a67424ab9a4767f67eb26e59d8aa3142345
axtezy <axtezy@gmail.com>
2024-04-27 14:18:17 +0200

    Fix tests

---
+++ src/test.ts
@@ -0,0 +1,8 @@
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
