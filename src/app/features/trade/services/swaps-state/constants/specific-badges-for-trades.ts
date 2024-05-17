2c22dba8105ac3489dae40b285d872c161d357dd
PseudoElement <bubamainer@mail.ru>
2024-05-17 15:15:04 +0300

    2410, fix meson decimals, add badge

--- src/app/features/trade/services/swaps-state/constants/specific-badges-for-trades.ts
+++ src/app/features/trade/services/swaps-state/constants/specific-badges-for-trades.ts
@@ -2,7 +2,6 @@ import { BadgeInfo } from '@features/trade/models/trade-state';
 import {
   BLOCKCHAIN_NAME,
   BRIDGE_TYPE,
-  CROSS_CHAIN_TRADE_TYPE,
   CrossChainTrade,
   CrossChainTradeType,
   OnChainTrade
@@ -14,16 +13,16 @@ function showNoSlippageLabelArbitrumBridge(trade: CrossChainTrade | OnChainTrade
 
 function showAttentionLabelArbitrumBridge(trade: CrossChainTrade | OnChainTrade): boolean {
   return (
-    trade.type === CROSS_CHAIN_TRADE_TYPE.ARBITRUM &&
     trade.from.blockchain === BLOCKCHAIN_NAME.ARBITRUM &&
     trade.to.blockchain === BLOCKCHAIN_NAME.ETHEREUM
   );
 }
 
-const positiveBadgeColor =
+const POSITIVE_COLOR =
   'linear-gradient(90deg, rgba(0, 255, 117, 0.6) 0%, rgba(224, 255, 32, 0.6) 99.18%)';
-const warningBadgeColor =
+const WARNING_COLOR =
   'linear-gradient(90deg, rgba(204, 141, 23, 0.83) 0%, rgba(213, 185, 5, 0.94) 99.18%)';
+
 export const SYMBIOSIS_REWARD_PRICE: { [key: string]: string } = {
   '1.2': '$50',
   '1.5': '$250',
@@ -35,33 +34,33 @@ export const SYMBIOSIS_REWARD_PRICE: { [key: string]: string } = {
 export const SPECIFIC_BADGES: Partial<Record<CrossChainTradeType, BadgeInfo[]>> = {
   [BRIDGE_TYPE.SYMBIOSIS]: [
     {
-      hint: 'Swap $100+ & get up to 1.3 $MNT!',
       label: '+ 1.3 MNT *',
+      hint: 'Swap $100+ & get up to 1.3 $MNT!',
       href: 'https://twitter.com/symbiosis_fi/status/1785996599564382501',
-      fromSdk: false,
+      fromSdk: true,
       showLabel: () => true
     }
   ],
   [BRIDGE_TYPE.XY]: [
     {
       label: 'Get Blast Points!',
-      fromSdk: true,
-      bgColor: positiveBadgeColor,
       href: 'https://twitter.com/xyfinance/status/1788862005736288497',
+      bgColor: POSITIVE_COLOR,
+      fromSdk: true,
       showLabel: () => true
     }
   ],
   [BRIDGE_TYPE.ARBITRUM]: [
     {
       label: 'NO SLIPPAGE',
-      bgColor: positiveBadgeColor,
+      bgColor: POSITIVE_COLOR,
       fromSdk: false,
       showLabel: showNoSlippageLabelArbitrumBridge
     },
     {
       label: 'ATTENTION',
       hint: 'Waiting funds in target chain for 7 days',
-      bgColor: warningBadgeColor,
+      bgColor: WARNING_COLOR,
       fromSdk: false,
       showLabel: showAttentionLabelArbitrumBridge
     }
