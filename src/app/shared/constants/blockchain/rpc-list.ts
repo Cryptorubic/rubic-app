335cdf8949254d939ed80417e6724c7c780d10ee
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-05-21 13:34:46 +0300

    update rpc

--- src/app/shared/constants/blockchain/rpc-list.ts
+++ src/app/shared/constants/blockchain/rpc-list.ts
@@ -229,8 +229,8 @@ export const rpcList: Record<EvmBlockchainName, string[]> &
   [BLOCKCHAIN_NAME.SCROLL_SEPOLIA]: ['https://sepolia-rpc.scroll.io/'],
   [BLOCKCHAIN_NAME.ARTHERA]: [],
   [BLOCKCHAIN_NAME.ZETACHAIN]: [
-    'https://zetachain-mainnet-archive.allthatnode.com:8545',
-    'https://zetachain-evm.blockpi.network/v1/rpc/public'
+    'https://zetachain-evm.blockpi.network/v1/rpc/public',
+    'https://zetachain-mainnet-archive.allthatnode.com:8545'
   ],
   [BLOCKCHAIN_NAME.TAIKO]: ['https://rpc.katla.taiko.xyz'],
   [BLOCKCHAIN_NAME.SEPOLIA]: ['https://l1rpc.jolnir.taiko.xyz'],
