5b9fc3c125dd173f72109772ccea7f4ddc2d5c60
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-05-27 11:47:10 +0300

    fix approve

--- src/app/features/earn/services/staking.service.ts
+++ src/app/features/earn/services/staking.service.ts
@@ -9,6 +9,7 @@ import {
   catchError,
   combineLatest,
   filter,
+  firstValueFrom,
   forkJoin,
   from,
   map,
@@ -129,7 +130,9 @@ export class StakingService {
     const routerEvents$ = this.router.events.pipe(filter(event => event instanceof NavigationEnd));
     const userBalanceAndAllowance$ = this.user$.pipe(
       filter(user => Boolean(user?.address)),
-      switchMap(() => this.getAllowance()),
+      switchMap(() =>
+        this.getAllowance().pipe(tap((allowance: BigNumber) => this.setAllowance(allowance)))
+      ),
       switchMap(() => this.getRbcTokenBalance())
     );
     combineLatest([routerEvents$, userBalanceAndAllowance$])
@@ -148,10 +151,7 @@ export class StakingService {
         this.walletAddress,
         STAKING_ROUND_THREE.NFT.address
       )
-    ).pipe(
-      map((allowance: BigNumber) => Web3Pure.fromWei(allowance)),
-      tap((allowance: BigNumber) => this.setAllowance(allowance))
-    );
+    ).pipe(map((allowance: BigNumber) => Web3Pure.fromWei(allowance)));
   }
 
   public setAllowance(allowance: BigNumber | 'Infinity'): void {
@@ -213,8 +213,10 @@ export class StakingService {
         );
 
       if (receipt && receipt.status) {
+        const allowance = await firstValueFrom(this.getAllowance());
+
         this.stakingNotificationService.showSuccessApproveNotification();
-        this.setAllowance('Infinity');
+        this.setAllowance(allowance);
       }
 
       return receipt;
