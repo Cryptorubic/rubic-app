2b506b4f5b9be4df45a1aaf8061f1e43826055f7
Andrew <hibebol45@gmail.com>
2024-06-24 17:21:52 +0300

    fix issues

--- src/app/features/trade/components/assets-selector/services/search-query-service/search-query.service.ts
+++ src/app/features/trade/components/assets-selector/services/search-query-service/search-query.service.ts
@@ -20,9 +20,9 @@ export class SearchQueryService {
   }
 
   public set query(value: string) {
-    this._query$.next(value);
+    this._query$.next(value.trim());
     if (this.assetsSelectorService.selectorListType !== 'tokens') {
-      this.gasFormService.updateSearchQuery(value);
+      this.gasFormService.updateSearchQuery(value.trim());
     }
   }
 
