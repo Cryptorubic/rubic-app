851e3539fd377bf1feb3fbf0fb80489d54a55fb3
axtezy <axtezy@gmail.com>
2024-07-16 17:24:40 +0200

    Add symbiosis badge

--- src/app/features/trade/services/swaps-state/constants/specific-badges-for-trades.ts
+++ src/app/features/trade/services/swaps-state/constants/specific-badges-for-trades.ts
@@ -17,19 +17,39 @@ export const SPECIFIC_BADGES_FOR_PROVIDERS: Partial<
 > = {
   [BRIDGE_TYPE.SYMBIOSIS]: [
     {
-      href: 'https://x.com/symbiosis_fi/status/1811374725391888666',
+      getUrl: (trade: CrossChainTrade | OnChainTrade) => {
+        const symbolAmount = trade instanceof CrossChainTrade ? trade.promotions?.[0] : null;
+        const [symbol] = symbolAmount.split('_');
+        if (symbol.toLowerCase() === 'taiko') {
+          return 'https://x.com/symbiosis_fi/status/1811374725391888666';
+        }
+        if (symbol.toLowerCase() === 'arb') {
+          return 'https://symbiosis.finance/blog/earn-arb-arbitrum-incentives-at-symbiosis#how-much-arb-rewards-can-you-earn';
+        }
+        return '';
+      },
       fromSdk: true,
       getLabel: (trade: CrossChainTrade | OnChainTrade) => {
         const symbolAmount = trade instanceof CrossChainTrade ? trade.promotions?.[0] : null;
-        // const [symbol, amount] = symbolAmount.split('_');
-        // return `+ ${amount} ${symbol} *`;
-        return symbolAmount ? '+5 $Taiko' : '';
+        const [symbol] = symbolAmount.split('_');
+        if (symbol.toLowerCase() === 'taiko') {
+          return '+5 $Taiko';
+        }
+        if (symbol.toLowerCase() === 'arb') {
+          return '+ARB';
+        }
+        return '';
       },
       getHint: (trade: CrossChainTrade | OnChainTrade) => {
         const symbolAmount = trade instanceof CrossChainTrade ? trade.promotions?.[0] : null;
-        // const [symbol, amount] = symbolAmount.split('_');
-        // return `Swap ${SYMBIOSIS_REWARD_PRICE[amount]}+ & get ${amount} ${symbol}!`;
-        return symbolAmount ? 'Get up to 5 $Taiko per swap via Symbiosis!' : '';
+        const [symbol] = symbolAmount.split('_');
+        if (symbol.toLowerCase() === 'taiko') {
+          return 'Get up to 5 $Taiko per swap via Symbiosis!';
+        }
+        if (symbol.toLowerCase() === 'arb') {
+          return 'Earn ARB Arbitrum Incentives at Symbiosis';
+        }
+        return '';
       },
       showLabel: () => true
     }
@@ -71,7 +91,7 @@ export const SPECIFIC_BADGES_FOR_PROVIDERS: Partial<
   ],
   [BRIDGE_TYPE.OWL_TO_BRIDGE]: [
     {
-      href: 'https://owlto.finance/',
+      getUrl: () => 'https://owlto.finance/',
       bgColor: INFO_COLOR,
       fromSdk: false,
       getLabel: () => '+Points!',
