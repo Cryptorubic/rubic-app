ab9623101542e7924800e6c8909dd8a0fd9659ea
PseudoElement <bubamainer@mail.ru>
2024-07-24 19:54:41 +0300

    bug-fixes, change error text

--- src/app/core/errors/models/common/not-supported-region-error.ts
+++ src/app/core/errors/models/common/not-supported-region-error.ts
@@ -3,9 +3,7 @@ import { RubicError } from '@core/errors/models/rubic-error';
 
 class NotSupportedRegionRubicError extends RubicError<ERROR_TYPE.TEXT> {
   constructor() {
-    super(
-      "Selected provider doesn't support your contry. Try to use VPN or select another provider."
-    );
+    super('Birders does not provide services for your current country/region.');
     Object.setPrototypeOf(this, NotSupportedRegionRubicError.prototype);
   }
 }
