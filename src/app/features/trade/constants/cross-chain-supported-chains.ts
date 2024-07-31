92ff9630c8d039560bd66fdd04865d7ff866d278
Andrew <hibebol45@gmail.com>
2024-07-31 11:43:29 +0300

    add retro bridge cross-chain provider

--- src/app/features/trade/constants/cross-chain-supported-chains.ts
+++ src/app/features/trade/constants/cross-chain-supported-chains.ts
@@ -21,7 +21,8 @@ import {
   symbiosisCrossChainSupportedBlockchains,
   taikoBridgeSupportedBlockchains,
   xySupportedBlockchains,
-  stargateV2SupportedBlockchains
+  stargateV2SupportedBlockchains,
+  retroBridgeSupportedBlockchain
 } from 'rubic-sdk';
 
 export const CROSS_CHAIN_SUPPORTED_CHAINS_CONFIG: Record<
@@ -48,5 +49,6 @@ export const CROSS_CHAIN_SUPPORTED_CHAINS_CONFIG: Record<
   [CROSS_CHAIN_TRADE_TYPE.MESON]: mesonCrossChainSupportedChains,
   [CROSS_CHAIN_TRADE_TYPE.OWL_TO_BRIDGE]: owlToSupportedBlockchains,
   [CROSS_CHAIN_TRADE_TYPE.EDDY_BRIDGE]: eddyBridgeSupportedChains,
-  [CROSS_CHAIN_TRADE_TYPE.STARGATE_V2]: stargateV2SupportedBlockchains
+  [CROSS_CHAIN_TRADE_TYPE.STARGATE_V2]: stargateV2SupportedBlockchains,
+  [CROSS_CHAIN_TRADE_TYPE.RETRO_BRIDGE]: retroBridgeSupportedBlockchain
 } as const;
