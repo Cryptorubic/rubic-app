53230d11c17b158bedee478666a71e17e10da942
axtezy <axtezy@gmail.com>
2024-08-21 10:52:51 +0200

    Fix gtag

--- src/assets/analytics/google-analytics.js
+++ src/assets/analytics/google-analytics.js
@@ -1,11 +1,11 @@
-<!-- Google Tag Manager -->
-(function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-    var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s);
-    j.async = true;
-    j.src = `https://www.googletagmanager.com/gtag/js?id=G-QHYCGJXV8G`;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PZ8NH4J');
-<!-- End Google Tag Manager -->
+// <!-- Google Tag Manager -->
+// (function (w, d, s, l) {
+//     w[l] = w[l] || [];
+//     w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+//     var f = d.getElementsByTagName('title')[0],
+//     j = d.createElement(s);
+//     j.async = true;
+//     j.src = `https://www.googletagmanager.com/gtag/js?id=G-QHYCGJXV8G`;
+//     f.insertBefore(j, f);
+// })(window, document, 'script', 'dataLayer');
+// <!-- End Google Tag Manager -->
