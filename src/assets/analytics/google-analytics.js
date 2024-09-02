909cc12e557f16d53eccebfc9f17c3eb02e6a039
Andrew <hibebol45@gmail.com>
2024-09-02 11:08:13 +0300

    update ga

--- src/assets/analytics/google-analytics.js
+++ src/assets/analytics/google-analytics.js
@@ -2,10 +2,10 @@
 (function (w, d, s, l, i) {
     w[l] = w[l] || [];
     w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-    var f = d.getElementsByTagName(s)[0],
+    var f = d.getElementsByTagName('head')[0],
     j = d.createElement(s);
     j.async = true;
     j.src = `https://www.googletagmanager.com/gtag/js?id=G-QHYCGJXV8G`;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PZ8NH4J');
+        f.appendChild(j);
+    })(window, document, 'script', 'dataLayer', 'GTM-PZ8NH4J');
 <!-- End Google Tag Manager -->
