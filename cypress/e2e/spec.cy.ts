781112b55e80723aaebd5362558207fc4661dab4
ildar <ildargabi@gmail.com>
2024-09-10 01:26:31 +0300

    refactor selectors after codeReview

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,34 +1,28 @@
-describe('OnChain в сети Ethereum test', () => {
-  it('Visits the initial project page', () => {
+describe('OnChain в сети Ethereum', () => {
+  it('Check main page and onChain swap', () => {
     cy.visit('https://app.rubic.exchange/');
     const copyright = cy.get('.rubic-footer__copyright');
     copyright.should('exist');
-    copyright.should('contain.text', ' © Copyright Rubic 2024, ');
+    copyright.should('contain.text', ' © Copyright Rubic ');
 
     // Проверка основных элементов на гоаыной странице
     cy.get('[class="_transitioned"]').should('exist'); // баннер
-    cy.get('[alt="Rubic logotype"]').should('exist'); // лого
+    cy.get('app-logo').should('exist'); // лого
     cy.get('[class=burger-menu]').should('exist'); // меню
     cy.get('[class="swap-button"]').should('exist').should('have.text', ' Swaps '); // кнопка swap
     cy.get('[class="staking-button"]').should('exist').should('have.text', ' Staking '); // кнопка staking
     cy.get('[class*=lang]').should('exist'); // смена языка
     cy.get('[class="history-button"]').should('exist'); // кнопка истории транзакций
-    cy.get('[data-appearance="primary"] > [class="t-wrapper"]')
-      .eq(0)
-      .should('exist')
-      .should('have.text', ' Подключить кошелек\n'); // кнопка подключения кошелька
-    const swapCalc = cy.get('[class*=window]');
-    swapCalc.should('contain.text', 'Swap'); // калькулятор свапа
+    cy.get('[class*=profile]').should('exist'); // кнопка подключения кошелька
+
+    const swapForm = cy.get('[class*=window]'); // форма свапа
+    swapForm.should('contain.text', 'Swap');
 
     // Выбор исходящего токена в сети Ethereum (ETH)
-    swapCalc.get('[alt="Select token"]').eq(0).click();
+    swapForm.get('app-asset-selector').eq(0).click();
     cy.get('[class="asset-types-aside"]').should('exist'); // список сетей
     cy.get('[class*=content-wrapper]').should('exist'); // список токенов
     cy.get('[class="back-button__text"]').should('have.text', 'Select Chain and Token');
-
-    /* 1. Вынести в отдельную функцию выбор сети токена (название сети, название токена, адрес токена, from/to)
-       2. Вынести в отдельный сценарий (
-      */
 
     // Проверка текста во вкладках типа сетей
     const chainTypeList = cy.get('[class=blockchains-filter-list] button');
@@ -40,28 +34,27 @@ describe('OnChain в сети Ethereum test', () => {
 
       expect(buttonText).to.equal(expectedText);
     });
+
     // Проверка налиячия плашки Promo у списка сетей
     cy.get('[class="asset-types-aside"]').should('contain.text', 'PROMO');
-    cy.get('[class*=asset-types] button').eq(0).should('have.text', 'EthereumPROMO');
+
+    // Выбор токена ETH
     cy.get('[class="t-content"] li').eq(0).should('contain.text', 'ETHEthereum').click();
 
-    // Проверка отображения выбранной сети и токена на калькуляторе
+    // Проверка отображения выбранной сети и токена на форме
     cy.get('[class=selector]').eq(0).should('have.text', 'EthereumETH ');
 
     // Выбор целевого токена в сети Ethereum (USDT)
-    swapCalc.get('[alt="Select token"]').eq(1).click();
+    swapForm.get('app-asset-selector').eq(1).click();
     cy.get('[class*=asset-types] ul').should('exist');
-    cy.get('[class*=asset-types] button').eq(0).should('have.text', 'EthereumPROMO').click();
+    cy.get('[class*=asset-types] button').eq(0).click();
     cy.get('[class="t-content"] li').eq(2).should('contain.text', 'Tether USD').click();
 
-    //  Ввод амаунта
-    swapCalc.get('[placeholder="Enter an Amount"]').click().type('1');
+    //  Ввод амаунта =1
+    cy.get('[inputmode="input"]').click().type('1');
 
     // Проверка калькуляции провайдеров
-    cy.get('[data-appearance="primary"] > [class="t-wrapper"]')
-      .eq(1)
-      .should('exist')
-      .should('have.text', ' Connect wallet ');
+    cy.get('[appearance="primary"]').should('exist').should('have.text', ' Connect wallet '); // кнопка подклчения кошелька на форме свапа
     cy.get('app-providers-list-general h5').should('have.text', 'Providers list');
     const providerList = cy.get('app-provider-element');
     const firstProviderCard = providerList.eq(0);
@@ -71,7 +64,7 @@ describe('OnChain в сети Ethereum test', () => {
     firstProviderCard.get('[class="element__token-image"]').should('exist'); // логотип провайдера
     firstProviderCard.get('[class="element__token-symbol"]').should('exist'); // наименование провайдера
     firstProviderCard.get('[class*="element__amount-price"]').should('contain', '$');
-    firstProviderCard.get('[alt="Fee"]').should('contain', ' ~ $');
-    firstProviderCard.get('[alt*="Time"]').should('contain', 'M');
+    firstProviderCard.get('[class*=inserted]').should('contain', ' ~ $');
+    firstProviderCard.get('[class*=time]').should('contain', 'M');
   });
 });
