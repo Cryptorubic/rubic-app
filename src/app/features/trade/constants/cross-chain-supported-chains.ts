896bd643f3d089935563cff9477e77cf1b1c01d2
Andrew <hibebol45@gmail.com>
2024-10-03 18:08:56 +0300

    add across cross-chain provider

--- src/app/features/trade/constants/cross-chain-supported-chains.ts
+++ src/app/features/trade/constants/cross-chain-supported-chains.ts
@@ -22,7 +22,8 @@ import {
   taikoBridgeSupportedBlockchains,
   xySupportedBlockchains,
   stargateV2SupportedBlockchains,
-  routerCrossChainSupportedChains
+  routerCrossChainSupportedChains,
+  acrossCcrSupportedChains
 } from 'rubic-sdk';
 
 export const CROSS_CHAIN_SUPPORTED_CHAINS_CONFIG: Record<
@@ -50,5 +51,6 @@ export const CROSS_CHAIN_SUPPORTED_CHAINS_CONFIG: Record<
   [CROSS_CHAIN_TRADE_TYPE.OWL_TO_BRIDGE]: owlToSupportedBlockchains,
   [CROSS_CHAIN_TRADE_TYPE.EDDY_BRIDGE]: eddyBridgeSupportedChains,
   [CROSS_CHAIN_TRADE_TYPE.STARGATE_V2]: stargateV2SupportedBlockchains,
-  [CROSS_CHAIN_TRADE_TYPE.ROUTER]: routerCrossChainSupportedChains
+  [CROSS_CHAIN_TRADE_TYPE.ROUTER]: routerCrossChainSupportedChains,
+  [CROSS_CHAIN_TRADE_TYPE.ACROSS]: acrossCcrSupportedChains
 } as const;
