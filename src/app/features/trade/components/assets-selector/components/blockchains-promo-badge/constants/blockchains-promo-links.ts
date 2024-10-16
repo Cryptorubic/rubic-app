723c19af80fda38818ac046de010a55f8003bcdc
Andrew <hibebol45@gmail.com>
2024-10-16 16:30:25 +0300

    remove old promo badges

--- src/app/features/trade/components/assets-selector/components/blockchains-promo-badge/constants/blockchains-promo-links.ts
+++ src/app/features/trade/components/assets-selector/components/blockchains-promo-badge/constants/blockchains-promo-links.ts
@@ -6,16 +6,8 @@ export const blockchainsPromoLinks: BlockchainsPromoLinks = {
   [BLOCKCHAIN_NAME.MODE]: 'https://app.layer3.xyz/quests/rubic-on-mode?slug=rubic-on-mode',
   [BLOCKCHAIN_NAME.BLAST]:
     'https://app.layer3.xyz/quests/blast-through-rubic-exchange?slug=blast-through-rubic-exchange',
-  [BLOCKCHAIN_NAME.METIS]: 'https://app.layer3.xyz/quests/bridging-the-gap',
   [BLOCKCHAIN_NAME.TAIKO]:
     'https://rubic.exchange/blog/how-to-bridge-to-and-from-taiko-and-get-taiko-airdrop/',
-  [BLOCKCHAIN_NAME.MERLIN]: 'https://app.galxe.com/quest/Rubic/GC6FstdPpK',
-  [BLOCKCHAIN_NAME.ZK_LINK]:
-    'https://rubic.exchange/blog/bridge-to-zklink-network-and-get-extra-nova-points/',
-  [BLOCKCHAIN_NAME.HORIZEN_EON]: 'https://app.galxe.com/quest/horizen/GCJZxtzTvN',
-  [BLOCKCHAIN_NAME.XLAYER]: 'https://app.layer3.xyz/quests/x-layer-exploration-rubic',
-  [BLOCKCHAIN_NAME.ROOTSTOCK]:
-    'https://www.intract.io/quest/667d6f70010a0189758ca622?utm_source=dashboard',
   [BLOCKCHAIN_NAME.SCROLL]: 'https://rubic.exchange/birthday4'
 };
 type BlockchainsPromoLinks = Partial<Record<BlockchainName, string>>;
