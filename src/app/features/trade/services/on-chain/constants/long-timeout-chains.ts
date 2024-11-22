1734febe8dfddfc0263e24db758e57a897c9af8c
axtezy <axtezy@gmail.com>
2024-11-22 19:32:41 +0100

    Fix pools

--- src/app/features/trade/services/on-chain/constants/long-timeout-chains.ts
+++ src/app/features/trade/services/on-chain/constants/long-timeout-chains.ts
@@ -1,5 +1,6 @@
 import { BLOCKCHAIN_NAME, BlockchainName } from 'rubic-sdk';
 
 export const ON_CHAIN_LONG_TIMEOUT_CHAINS: Readonly<BlockchainName[]> = [
-  BLOCKCHAIN_NAME.MERLIN
+  BLOCKCHAIN_NAME.MERLIN,
+  BLOCKCHAIN_NAME.FLARE
 ] as const;
