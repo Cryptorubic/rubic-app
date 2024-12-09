981335613d857f092ec65354f638928444060b95
pseudoelement <bubamainer@mail.ru>
2024-12-09 15:55:24 +0300

    disable-gas-quote, remove logs

--- src/app/features/trade/services/trade-info-manager/trade-info-manager.service.ts
+++ src/app/features/trade/services/trade-info-manager/trade-info-manager.service.ts
@@ -91,13 +91,6 @@ export class TradeInfoManager {
 
     if (!gasFeeWei) return null;
 
-    console.log(`%c${trade.type}`, 'color: red; font-size: 20px;', {
-      gasPriceWei: gasData.gasPrice?.toFixed(),
-      gasLimitWei: gasData.gasLimit?.toFixed(),
-      maxFeePerGas: gasData.maxFeePerGas?.toFixed(),
-      totalGas: gasData.totalGas?.toFixed()
-    });
-
     return Web3Pure.fromWei(gasFeeWei, nativeToken.decimals);
   }
 
@@ -126,13 +119,6 @@ export class TradeInfoManager {
     } else if ('maxFeePerGas' in gasData && gasData.maxFeePerGas.gt(0)) {
       gasFeeWei = gasData.gasLimit.multipliedBy(gasData.maxFeePerGas);
     }
-
-    console.log(`%c${trade.type}`, 'color: yellow; font-size: 20px;', {
-      gasPriceWei: gasData.gasPrice?.toFixed(),
-      gasLimitWei: gasData.gasLimit?.toFixed(),
-      maxFeePerGas: gasData.maxFeePerGas?.toFixed(),
-      totalGas: gasData.totalGas?.toFixed()
-    });
 
     return Web3Pure.fromWei(gasFeeWei, nativeToken.decimals);
   }
