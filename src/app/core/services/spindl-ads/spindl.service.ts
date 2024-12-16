8f88b058b1154a5863a505690adf82e692f9cd9e
pseudoelement <bubamainer@mail.ru>
2024-12-16 11:40:43 +0300

    730, send events if Russian IP

--- src/app/core/services/spindl-ads/spindl.service.ts
+++ src/app/core/services/spindl-ads/spindl.service.ts
@@ -85,21 +85,19 @@ export class SpindlService {
     const isForbiddenIP = await this.isForbiddenIP();
     this._showSpindl = !isForbiddenIP;
 
-    if (this._showSpindl) {
-      spindl.configure({
-        sdkKey: '5c8549dc-9be6-49ee-bc3f-8192870f4553',
-        debugMode: !ENVIRONMENT.production,
-        maxRetries: 3
+    spindl.configure({
+      sdkKey: '5c8549dc-9be6-49ee-bc3f-8192870f4553',
+      debugMode: !ENVIRONMENT.production,
+      maxRetries: 3
+    });
+
+    spindl.enableAutoPageViews();
+
+    this.authService.currentUser$
+      .pipe(distinctUntilChanged((prev, curr) => prev?.address === curr?.address))
+      .subscribe(user => {
+        if (user.address) spindl.attribute(user.address);
       });
-
-      spindl.enableAutoPageViews();
-
-      this.authService.currentUser$
-        .pipe(distinctUntilChanged((prev, curr) => prev?.address === curr?.address))
-        .subscribe(user => {
-          if (user.address) spindl.attribute(user.address);
-        });
-    }
   }
 
   public sendSwapEvent(txHash: string): void {
