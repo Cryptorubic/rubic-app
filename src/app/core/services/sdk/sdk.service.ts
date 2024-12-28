2dfd4e48d46ab43e63f5049283e806bb92871800
axtezy <axtezy@gmail.com>
2024-12-28 12:49:25 +0100

    Change to dev2

--- src/app/core/services/sdk/sdk.service.ts
+++ src/app/core/services/sdk/sdk.service.ts
@@ -89,7 +89,7 @@ export class SdkService {
     return {
       ...rubicSdkDefaultConfig,
       httpClient: new SdkHttpClient(this.angularHttpClient),
-      envType: 'dev',
+      envType: 'dev2',
       providerAddress: {
         [CHAIN_TYPE.EVM]: {
           crossChain: params?.crossChainIntegratorAddress || defaultProvidersAddresses.crossChain,
