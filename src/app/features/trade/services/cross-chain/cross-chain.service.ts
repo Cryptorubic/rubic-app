c385489344e75a918896f4657fee1ad2f66ffaa8
pseudoelement <bubamainer@mail.ru>
2025-01-23 13:16:16 +0300

    994, fix rateUpdated in solana

--- src/app/features/trade/services/cross-chain/cross-chain.service.ts
+++ src/app/features/trade/services/cross-chain/cross-chain.service.ts
@@ -263,7 +263,10 @@ export class CrossChainService {
   public async swapTrade(
     trade: CrossChainTrade<unknown>,
     callbackOnHash?: (hash: string) => void,
-    useCacheData?: boolean
+    params: { useCacheData: boolean; skipAmountCheck: boolean } = {
+      useCacheData: false,
+      skipAmountCheck: false
+    }
   ): Promise<string | null> {
     if (!this.isSlippageCorrect(trade)) {
       return null;
@@ -310,7 +313,8 @@ export class CrossChainService {
       ...(shouldCalculateGasPrice && { gasPriceOptions }),
       ...(this.queryParamsService.testMode && { testMode: true }),
       ...(referrer && { referrer }),
-      useCacheData: useCacheData || false
+      useCacheData: params.useCacheData,
+      skipAmountCheck: params.skipAmountCheck
     };
 
     try {
--- src/app/features/trade/services/swaps-controller/swaps-controller.service.ts
+++ src/app/features/trade/services/swaps-controller/swaps-controller.service.ts
@@ -327,9 +327,15 @@ export class SwapsControllerService {
         if (allowSwap) {
           try {
             if (trade instanceof CrossChainTrade) {
-              txHash = await this.crossChainService.swapTrade(trade, callback.onHash, true);
+              txHash = await this.crossChainService.swapTrade(trade, callback.onHash, {
+                skipAmountCheck: true,
+                useCacheData: true
+              });
             } else {
-              txHash = await this.onChainService.swapTrade(trade, callback.onHash, true);
+              txHash = await this.onChainService.swapTrade(trade, callback.onHash, {
+                skipAmountCheck: true,
+                useCacheData: true
+              });
             }
           } catch (innerErr) {
             this.catchSwapError(innerErr, tradeState, callback?.onError);
