358fed5958506717a715826b8aba0d270f08eab2
Andrew <hibebol45@gmail.com>
2025-03-19 12:58:21 +0300

    add percent provider address for monad

--- src/app/features/trade/utils/handle-integrator-address.ts
+++ src/app/features/trade/utils/handle-integrator-address.ts
@@ -1,50 +1,15 @@
 import { BLOCKCHAIN_NAME, BlockchainName, CrossChainManagerCalculationOptions } from 'rubic-sdk';
-import {
-  MERLIN_INTEGRATOR_ADDRESS,
-  TAIKO_INTEGRATOR_ADDRESS_CROSS_CHAIN,
-  TAIKO_INTEGRATOR_ADDRESS_ON_CHAIN,
-  XLAYER_INTEGRATOR_ADDRESS_CROSS_CHAIN,
-  XLAYER_INTEGRATOR_ADDRESS_ON_CHAIN
-} from '../constants/calculation';
+import { MONAD_INTEGRATOR_ADDRESS } from '../constants/calculation';
 
 export function handleIntegratorAddress(
   options: Pick<CrossChainManagerCalculationOptions, 'providerAddress'>,
   fromBlockchain: BlockchainName,
   toBlockchain: BlockchainName
 ): void {
-  const urlParams = new URLSearchParams(window.location.search);
-  const commonIntegrator = urlParams.get('feeTarget') || urlParams.get('providerAddress');
-  const crossChainIntegrator = urlParams.get('crossChainIntegratorAddress') || commonIntegrator;
-  const onChainIntegrator = urlParams.get('onChainIntegratorAddress') || commonIntegrator;
-
-  const useTaikoIntegratorOnChain =
-    fromBlockchain === toBlockchain &&
-    fromBlockchain === BLOCKCHAIN_NAME.TAIKO &&
-    !onChainIntegrator;
-  const useTaikoIntegratorCcr =
-    (fromBlockchain === BLOCKCHAIN_NAME.TAIKO || toBlockchain === BLOCKCHAIN_NAME.TAIKO) &&
-    !crossChainIntegrator;
-  const useMerlinIntegrator =
-    (fromBlockchain === BLOCKCHAIN_NAME.MERLIN || toBlockchain === BLOCKCHAIN_NAME.MERLIN) &&
-    !crossChainIntegrator &&
-    !onChainIntegrator;
-  const useXLayerIntegratorOnChain =
-    fromBlockchain === toBlockchain &&
-    fromBlockchain === BLOCKCHAIN_NAME.XLAYER &&
-    !onChainIntegrator;
-  const useXLayerIntegratorCcr =
-    (fromBlockchain === BLOCKCHAIN_NAME.XLAYER || toBlockchain === BLOCKCHAIN_NAME.XLAYER) &&
-    !crossChainIntegrator;
-
-  if (useTaikoIntegratorOnChain) {
-    options.providerAddress = TAIKO_INTEGRATOR_ADDRESS_ON_CHAIN;
-  } else if (useTaikoIntegratorCcr) {
-    options.providerAddress = TAIKO_INTEGRATOR_ADDRESS_CROSS_CHAIN;
-  } else if (useMerlinIntegrator) {
-    options.providerAddress = MERLIN_INTEGRATOR_ADDRESS;
-  } else if (useXLayerIntegratorOnChain) {
-    options.providerAddress = XLAYER_INTEGRATOR_ADDRESS_ON_CHAIN;
-  } else if (useXLayerIntegratorCcr) {
-    options.providerAddress = XLAYER_INTEGRATOR_ADDRESS_CROSS_CHAIN;
+  if (
+    fromBlockchain === BLOCKCHAIN_NAME.MONAD_TESTNET &&
+    toBlockchain === BLOCKCHAIN_NAME.MONAD_TESTNET
+  ) {
+    options.providerAddress = MONAD_INTEGRATOR_ADDRESS;
   }
 }
