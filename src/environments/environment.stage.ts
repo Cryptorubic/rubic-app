ea5a39d5290232ca49bccf4504bbb3e29b4340ba
Andrew <hibebol45@gmail.com>
2025-03-24 09:53:19 +0300

    change dev tokens endpoint for stage

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -4,13 +4,13 @@ export const ENVIRONMENT = {
   environmentName: 'stage',
   production: true,
 
-  apiBaseUrl: '//stage-api.rubic.exchange/api',
-  apiTokenUrl: 'https://stage-api.rubic.exchange/api',
+  apiBaseUrl: '//api.rubic.exchange/api',
+  apiTokenUrl: 'https://api.rubic.exchange/api',
   websocketBaseUrl: 'wss://api.rubic.exchange/ws',
 
   staking: STAKING_CONFIG_DEVELOP,
 
-  zrxAffiliateAddress: undefined as string,
+  zrxAffiliateAddress: '0x19eBB148836B5f8A6320e42666912978B20D0Dbb',
   onramperApiKey: 'pk_prod_01GYYB6KDFQCKYG23MHZ8QK5GC',
   lifiApiKey: '0a1eec2c-b1bd-4dc1-81cf-c988f099c929.f5950d26-5955-4e21-9db2-77ad984ea575'
 };
