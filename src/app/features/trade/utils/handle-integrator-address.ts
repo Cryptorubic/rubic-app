a699a876298e27ef5d66c1050db0875a3c1a79b5
Andrew <hibebol45@gmail.com>
2025-04-01 12:25:46 +0300

    add zero percent integrator address for megaeth

--- src/app/features/trade/utils/handle-integrator-address.ts
+++ src/app/features/trade/utils/handle-integrator-address.ts
@@ -1,5 +1,5 @@
 import { BLOCKCHAIN_NAME, BlockchainName, CrossChainManagerCalculationOptions } from 'rubic-sdk';
-import { MONAD_INTEGRATOR_ADDRESS } from '../constants/calculation';
+import { MONAD_INTEGRATOR_ADDRESS, ZERO_PERCENT_ADDRESS } from '../constants/calculation';
 
 export function handleIntegratorAddress(
   options: Pick<CrossChainManagerCalculationOptions, 'providerAddress'>,
@@ -11,5 +11,10 @@ export function handleIntegratorAddress(
     toBlockchain === BLOCKCHAIN_NAME.MONAD_TESTNET
   ) {
     options.providerAddress = MONAD_INTEGRATOR_ADDRESS;
+  } else if (
+    fromBlockchain === BLOCKCHAIN_NAME.MEGAETH_TESTNET &&
+    toBlockchain === BLOCKCHAIN_NAME.MEGAETH_TESTNET
+  ) {
+    options.providerAddress = ZERO_PERCENT_ADDRESS;
   }
 }
