52f4e684c4c6b1df451698b10e3d8a57d0575eb0
axtezy <axtezy@gmail.com>
2025-06-16 18:19:13 +0200

    Add berachella iframe styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -79,6 +79,7 @@ import { DepositRateChangedModalComponent } from './components/deposit-rate-upda
 import { ShowPriceChangePipe } from './pipes/show-price-change.pipe';
 import { ShortenTextPipe } from './pipes/shorten-text.pipe';
 import { MathAbsPipe } from './pipes/math-abs.pipe';
+import { NoBerachellaDirective } from '@shared/directives/no-berachella/no-berachella.directive';
 
 @NgModule({
   declarations: [
@@ -132,7 +133,8 @@ import { MathAbsPipe } from './pipes/math-abs.pipe';
     DepositRateChangedModalComponent,
     ShowPriceChangePipe,
     ShortenTextPipe,
-    MathAbsPipe
+    MathAbsPipe,
+    NoBerachellaDirective
   ],
   imports: [
     CommonModule,
@@ -212,7 +214,8 @@ import { MathAbsPipe } from './pipes/math-abs.pipe';
     SpindleBannerComponent,
     ShowPriceChangePipe,
     ShortenTextPipe,
-    MathAbsPipe
+    MathAbsPipe,
+    NoBerachellaDirective
   ],
   providers: [ScannerLinkPipe, WithRoundPipe, BigNumberFormatPipe, TimeGuard, SafeSanitizerPipe]
 })
