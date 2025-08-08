734d7c6a1acad673af969b17a9a8ed7006e9eadd
pseudoelement <bubamainer@mail.ru>
2025-08-08 20:30:34 +0300

    rub-282, Chart TradingView

---
+++ src/app/features/trade/components/crypto-rate-chart/crypto-rate-chart.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChartService } from '../../services/chart-service/chart.service';
+
+@Component({
+  selector: 'app-crypto-rate-chart',
+  templateUrl: './crypto-rate-chart.component.html',
+  styleUrls: ['./crypto-rate-chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CryptoRateChartComponent {
+  public readonly chartSize$ = this.chartService.chartSize$;
+
+  constructor(private readonly chartService: ChartService) {}
+}
