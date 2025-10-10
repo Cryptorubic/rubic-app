5f0c4e77eb11922f8851881b7c8f8b5fa9c3552d
Andrew <hibebol45@gmail.com>
2025-10-10 12:49:41 +0300

    update bitcoin/tron position in selector

--- src/app/features/trade/components/assets-selector/services/blockchains-list-service/constants/blockchains-list.ts
+++ src/app/features/trade/components/assets-selector/services/blockchains-list-service/constants/blockchains-list.ts
@@ -15,7 +15,7 @@ function setRankToNonEvmBlockchain(blockchain: BlockchainName): number {
     return 0.7;
   }
   if (blockchain === BLOCKCHAIN_NAME.BITCOIN) {
-    return 0.6;
+    return 0.74;
   }
   if (blockchain === BLOCKCHAIN_NAME.TON) {
     return 0.73;
@@ -216,6 +216,11 @@ export const blockchainsList: RankedBlockchain[] = [
     tags: ['ETH', BlockchainTags.POPULAR, BlockchainTags.EVM, BlockchainTags.LAYER_2]
   },
   {
+    name: BLOCKCHAIN_NAME.TRON,
+    rank: 0.74,
+    tags: ['TRX', BlockchainTags.NON_EVM, BlockchainTags.POPULAR]
+  },
+  {
     name: BLOCKCHAIN_NAME.BERACHAIN,
     rank: 0.73,
     tags: ['ETH', BlockchainTags.POPULAR, BlockchainTags.EVM, BlockchainTags.LAYER_2]
@@ -313,11 +318,6 @@ export const blockchainsList: RankedBlockchain[] = [
     name: BLOCKCHAIN_NAME.PULSECHAIN,
     rank: 0.5,
     tags: ['PLS', BlockchainTags.POPULAR, BlockchainTags.EVM]
-  },
-  {
-    name: BLOCKCHAIN_NAME.TRON,
-    rank: 0.5,
-    tags: ['TRX', BlockchainTags.NON_EVM, BlockchainTags.POPULAR]
   },
   {
     name: BLOCKCHAIN_NAME.TAIKO,
