e3e6f7dd0cee62faf71e1d2a989756370ee49e78
DE2112 <DE2112@bitbucket.org>
2025-11-07 12:43:30 +0300

    rub-988, review fix

--- sentry-webpack.config.ts
+++ sentry-webpack.config.ts
@@ -11,9 +11,10 @@ export default (
   config.devtool = 'source-map';
   config.plugins.push(
     sentryWebpackPlugin({
-      org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT,
-      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: 'rubic', //process.env.SENTRY_ORG,
+      project: 'internal', //process.env.SENTRY_PROJECT,
+      authToken:
+        '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', //process.env.SENTRY_AUTH_TOKEN,
       bundleSizeOptimizations: {
         excludeReplayIframe: true,
         excludeReplayShadowDom: true,
