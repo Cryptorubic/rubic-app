a4c3b9d2d0716e2fd15d6956b0676bf20a998ff8
DE2112 <DE2112@bitbucket.org>
2025-11-19 12:47:00 +0300

    rub-1107, sentry init & webpack configs edit

--- sentry-webpack.config.ts
+++ sentry-webpack.config.ts
@@ -12,7 +12,7 @@ export default (
   config.plugins.push(
     sentryWebpackPlugin({
       org: 'rubic', //process.env.SENTRY_ORG,
-      project: 'internal', //process.env.SENTRY_PROJECT,
+      project: 'rubic-app-prod', //process.env.SENTRY_PROJECT,
       authToken:
         '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', //process.env.SENTRY_AUTH_TOKEN,
       bundleSizeOptimizations: {
