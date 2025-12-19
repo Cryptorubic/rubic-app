de88ef496e6efa3be5953f3097ccb2b8b27975e1
Andrew <hibebol45@gmail.com>
2025-12-19 16:46:26 +0300

    rub-1069 fix stellar balance after swap

--- src/app/features/trade/services/on-chain/on-chain.service.ts
+++ src/app/features/trade/services/on-chain/on-chain.service.ts
@@ -362,7 +362,7 @@ export class OnChainService {
   }
 
   private async conditionalAwait(blockchain: BlockchainName): Promise<void> {
-    if (blockchain === BLOCKCHAIN_NAME.SOLANA) {
+    if (blockchain === BLOCKCHAIN_NAME.SOLANA || blockchain === BLOCKCHAIN_NAME.STELLAR) {
       const waitTime = 3_000;
       await firstValueFrom(timer(waitTime));
     }
