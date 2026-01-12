2e49bdef59507fa0fe25af2e70de8c5070775ac3
pseudoelement <bubamainer@mail.ru>
2026-01-12 18:50:04 +0300

    rub-1426, Houdini Provider

--- src/app/core/services/sdk/sdk-legacy/features/cross-chain/calculation-manager/providers/common/cross-chain-transfer-trade/constans/transfer-trade-supported-providers.ts
+++ src/app/core/services/sdk/sdk-legacy/features/cross-chain/calculation-manager/providers/common/cross-chain-transfer-trade/constans/transfer-trade-supported-providers.ts
@@ -6,7 +6,8 @@ export const transferTradeSupportedProviders = [
   CROSS_CHAIN_TRADE_TYPE.CHANGELLY,
   CROSS_CHAIN_TRADE_TYPE.EXOLIX,
   CROSS_CHAIN_TRADE_TYPE.NEAR_INTENTS,
-  CROSS_CHAIN_TRADE_TYPE.QUICKEX
+  CROSS_CHAIN_TRADE_TYPE.QUICKEX,
+  CROSS_CHAIN_TRADE_TYPE.HOUDINI
 ] as const;
 
 export type TransferTradeType = (typeof transferTradeSupportedProviders)[number];
