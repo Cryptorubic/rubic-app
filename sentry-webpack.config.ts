d6017874d784da7f88d43d8a37ef60273ca5c290
pseudoelement <bubamainer@mail.ru>
2026-02-19 16:56:15 +0300

    rub-1393, fix sentry

--- sentry-webpack.config.ts
+++ sentry-webpack.config.ts
@@ -1,29 +1,23 @@
-import { CustomWebpackBrowserSchema, TargetOptions } from '@angular-builders/custom-webpack';
+import { CustomWebpackBrowserSchema } from '@angular-builders/custom-webpack';
 import { sentryWebpackPlugin } from '@sentry/webpack-plugin';
 
 import * as webpack from 'webpack';
 
-export default (
-  config: webpack.Configuration,
-  _: CustomWebpackBrowserSchema,
-  targetOptions: TargetOptions
-) => {
-  if (targetOptions.configuration === 'production') {
-    config.devtool = 'source-map';
-    config.plugins.push(
-      sentryWebpackPlugin({
-        org: 'rubic',
-        project: 'rubic-app',
-        authToken:
-          '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', //process.env.SENTRY_AUTH_TOKEN,
-        bundleSizeOptimizations: {
-          excludeReplayIframe: true,
-          excludeReplayShadowDom: true,
-          excludeReplayWorker: true
-        }
-      })
-    );
-  }
+export default (config: webpack.Configuration, _: CustomWebpackBrowserSchema) => {
+  config.devtool = 'source-map';
+  config.plugins.push(
+    sentryWebpackPlugin({
+      org: 'rubic',
+      project: 'rubic-app',
+      authToken:
+        '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', //process.env.SENTRY_AUTH_TOKEN,
+      bundleSizeOptimizations: {
+        excludeReplayIframe: true,
+        excludeReplayShadowDom: true,
+        excludeReplayWorker: true
+      }
+    })
+  );
 
   return config;
 };
