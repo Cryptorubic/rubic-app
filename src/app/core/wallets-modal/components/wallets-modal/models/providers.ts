f5476753fa835bbac7800e1dc28856689142b591
andrey-ott <ott.andrey@rubic.finance>
2026-03-30 18:42:49 +0400

    turn on tronlink in mobile

--- src/app/core/wallets-modal/components/wallets-modal/models/providers.ts
+++ src/app/core/wallets-modal/components/wallets-modal/models/providers.ts
@@ -16,7 +16,7 @@ export const PROVIDERS_LIST: ReadonlyArray<WalletProvider> = [
     value: WALLET_NAME.TRON_LINK,
     img: './assets/images/icons/wallets/tronlink.webp',
     supportsDesktop: true,
-    supportsMobile: false,
+    supportsMobile: true,
     display: true,
     disabled: false
   },
