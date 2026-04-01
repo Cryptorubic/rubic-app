1effae6ff782c77e47f2d3e061ca4415285746a3
andrey-ott <ott.andrey@rubic.finance>
2026-04-01 13:09:19 +0400

    disable tronlink on mobile

--- src/app/core/services/wallets/wallets-adapters/tron/tron-link-adapter.ts
+++ src/app/core/services/wallets/wallets-adapters/tron/tron-link-adapter.ts
@@ -51,9 +51,6 @@ export class TronLinkAdapter extends CommonWalletAdapter {
   }
 
   private async connectToWallet(): Promise<void> {
-    window.location.href = `tronlink://dapp?url=${encodeURIComponent(
-      window.location.href.split('?')[0]
-    )}`;
     const provider = this.window.tronLink;
     if (provider?.isBitKeepChrome) {
       throw new RubicError('Please, check you unlocked TronLink.');
