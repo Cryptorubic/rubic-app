64bb8da69e4e37b0c1d861132007418b27884ec2
axtezy <axtezy@gmail.com>
2026-04-10 18:53:02 +0200

    Fix tokens select

--- src/app/features/trade/components/token-selector-page/token-selector-page.component.ts
+++ src/app/features/trade/components/token-selector-page/token-selector-page.component.ts
@@ -12,6 +12,10 @@ import { Asset } from '@features/trade/models/asset';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
 import { TuiDialogContext } from '@taiga-ui/core';
 import { HeaderStore } from '@core/header/services/header.store';
+import { BalanceToken } from '@shared/models/tokens/balance-token';
+import { SwapsFormService } from '@features/trade/services/swaps-form/swaps-form.service';
+import { DOCUMENT } from '@angular/common';
+import { TradePageService } from '@features/trade/services/trade-page/trade-page.service';
 
 @Component({
   selector: 'app-token-selector-page',
@@ -30,12 +34,33 @@ export class TokenSelectorPageComponent {
     @Optional()
     @Inject(POLYMORPHEUS_CONTEXT)
     private readonly context: TuiDialogContext<void, { formType: FormType }>,
-    private readonly headerStore: HeaderStore
+    private readonly headerStore: HeaderStore,
+    @Inject(DOCUMENT) private readonly document: Document,
+    private readonly swapFormService: SwapsFormService,
+    private readonly tradePageService: TradePageService
   ) {
     this.formType = this.context?.data?.formType;
   }
 
   public handleTokenSelect(asset: Asset): void {
-    this.tokenSelected.emit(asset);
+    if (this.headerStore.isMobile) {
+      const token = asset as BalanceToken;
+      if (token) {
+        if (this.formType === 'from') {
+          this.swapFormService.inputControl.patchValue({
+            fromBlockchain: token.blockchain,
+            fromToken: token
+          });
+        } else {
+          this.swapFormService.inputControl.patchValue({
+            toToken: token,
+            toBlockchain: token.blockchain
+          });
+        }
+      }
+      this.tradePageService.setState('form');
+    } else {
+      this.tokenSelected.emit(asset);
+    }
   }
 }
