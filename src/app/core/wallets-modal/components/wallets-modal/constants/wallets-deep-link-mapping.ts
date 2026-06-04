64f0405b912cac0eac7c0bcd5ab8ba48de3dcd6c
Andrew <hibebol45@gmail.com>
2026-06-04 14:43:24 +0300

    restore metamask solana deep link

--- src/app/core/wallets-modal/components/wallets-modal/constants/wallets-deep-link-mapping.ts
+++ src/app/core/wallets-modal/components/wallets-modal/constants/wallets-deep-link-mapping.ts
@@ -5,6 +5,8 @@ export const WALLETS_DEEP_LINK_MAPPING: Partial<
   Record<WALLET_NAME, (window: RubicWindow) => string>
 > = {
   [WALLET_NAME.COIN_BASE]: _window => 'https://go.cb-w.com/cDgO1V5aDlb',
+  [WALLET_NAME.METAMASK_SOLANA]: window =>
+    `https://metamask.app.link/dapp/${window.location.host}${window.location.search}`,
   [WALLET_NAME.BACKPACK]: window =>
     `https://backpack.app/ul/v1/browse/${encodeURIComponent(window.location.href)}?ref=${
       window.location.href
