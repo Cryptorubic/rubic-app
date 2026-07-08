7870bfbc6f8ed3a491b4492fe5dc382634ca08dd
pseudoelement <bubamainer@mail.ru>
2026-07-08 16:22:13 +0300

    rub-2695, change .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  '**/*.ts': (files) => {
-    const patterns = files.map((f) => `--lint-file-patterns="${f}"`).join(' ');
-    return `node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng lint rubic --fix ${patterns}`;
-  },
+  // '**/*.ts': (files) => {
+  //   const patterns = files.map((f) => `--lint-file-patterns="${f}"`).join(' ');
+  //   return `node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng lint rubic --fix ${patterns}`;
+  // },
+  '**/*.ts': 'node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng lint --fix',
   '**/*.css': 'stylelint --fix',
   '**/*.scss': 'stylelint --syntax=scss --fix',
   '**/*.json': 'jsonlint -q',
