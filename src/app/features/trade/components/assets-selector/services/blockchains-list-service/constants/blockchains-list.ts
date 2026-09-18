62010ef9ada50321af8c01372e5530583890bba7
andrey-ott <ott.andrey@rubic.finance>
2026-09-18 16:47:45 +0400

    remove tag new from robinhood

--- src/app/features/trade/components/assets-selector/services/blockchains-list-service/constants/blockchains-list.ts
+++ src/app/features/trade/components/assets-selector/services/blockchains-list-service/constants/blockchains-list.ts
@@ -209,7 +209,7 @@ export const blockchainsList: RankedBlockchain[] = [
   {
     name: BLOCKCHAIN_NAME.ROBINHOOD,
     rank: 0.73,
-    tags: ['ETH', BLOCKCHAIN_TAG.EVM, BLOCKCHAIN_TAG.LAYER_2, BLOCKCHAIN_TAG.NEW]
+    tags: ['ETH', BLOCKCHAIN_TAG.EVM, BLOCKCHAIN_TAG.LAYER_2]
   },
   {
     name: BLOCKCHAIN_NAME.POLYGON,
